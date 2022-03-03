fac3ba2eec3a0d51661a7beecca6aaf2b9564a61
matheril <sauravvm237@gmail.com>
2022-03-03 22:18:16 +0800

    Add Message class
    Message holds hardcoded strings such as error codes.

---
+++ src/main/java/seedu/duke/ui/Message.java
@@ -0,0 +1,9 @@
+package seedu.duke.ui;
+
+/**
+ * Contains hardcoded strings such as error codes.
+ * @author Saurav
+ */
+public class Message {
+    public static String ERRORCODE_EXAMPLE = "";
+}
