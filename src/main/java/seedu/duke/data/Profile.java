1895b926b98ab7e4dba8eaccbcbca03398c312d0
Roy Tang <roy_tql@hotmail.com>
2022-03-03 21:23:56 +0800

    Add new getter and setter methods to fit to backend requirements

--- src/main/java/seedu/duke/data/Profile.java
+++ src/main/java/seedu/duke/data/Profile.java
@@ -6,7 +6,7 @@ public class Profile {
     // To be completed when Warren pushes
     private ArrayList<Session> sessionList;
     private int sessionIdTracker;
-    private int activityCount;
+    private int activityIdTracker;
 
     public Profile() {
         this.sessionList = new ArrayList<>();
@@ -18,12 +18,21 @@ public class Profile {
         return sessionList;
     }
 
+    public int getNewSessionId() {
+        return sessionIdTracker + 1;
     }
 
+    public void updateSessionIdTracker() {
+        this.sessionIdTracker += 1;
     }
 
+    public int getNewActivityIdTracker() {
+        return activityIdTracker + 1;
     }
 
+    public void updateActivityIdTracker() {
+        this.activityIdTracker += 1;
     }
+
 
 }
