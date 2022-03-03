5327bcd0b3c33f53482e5008b8e69e4ee1b3ba39
Roy Tang <roy_tql@hotmail.com>
2022-03-03 22:35:18 +0800

    Add driver functions for main class

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -6,10 +6,29 @@ public class Duke {
     Profile profile;
 
     public static void main(String[] args) {
+        new Duke().run();
     }
 
     public Duke() {
         ui = new TextUI();
         profile = new Profile();
+    }
+    private void run() {
+        showWelcomeMessage();
+        runProcessLoop();
+        showFarewellMessage();
+    }
+    private void showWelcomeMessage() {
+        ui.printWelcome();
+    }
+    private void showFarewellMessage() {
+        ui.printFarewell();
+        System.exit(0);
+    }
+    private void runProcessLoop() {
+        String userInput;
+        do {
+            userInput = ui.readNextLine();
+        } while (userInput.contains("Bye"));
     }
 }
