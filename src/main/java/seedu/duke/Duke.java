017d12bce72059b7d15e3f6ef9ab74c166576c77
Roy Tang <roy_tql@hotmail.com>
2022-03-03 22:34:24 +0800

    Add ui and profile attributes to be initialized by main class

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -2,6 +2,14 @@ package seedu.duke;
 
 
 public class Duke {
+    TextUI ui;
+    Profile profile;
+
     public static void main(String[] args) {
+    }
+
+    public Duke() {
+        ui = new TextUI();
+        profile = new Profile();
     }
 }
