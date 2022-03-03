231a5ea16fd64ab2c121642de32b21ec71ada9f8
Roy Tang <roy_tql@hotmail.com>
2022-03-03 22:35:35 +0800

    Add javadocs for main class

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,6 +1,12 @@
 package seedu.duke;
 
+import seedu.duke.data.Profile;
+import seedu.duke.ui.TextUI;
 
+/**
+ * Entry point of the SplitLah application.
+ * Initializes SplitLah and starts interacting with the user.
+ */
 public class Duke {
     TextUI ui;
     Profile profile;
@@ -9,22 +15,31 @@ public class Duke {
         new Duke().run();
     }
 
+    /** Sets up the required objects */
     public Duke() {
         ui = new TextUI();
         profile = new Profile();
     }
+
+    /** Runs the program until it terminates.  */
     private void run() {
         showWelcomeMessage();
         runProcessLoop();
         showFarewellMessage();
     }
+
+    /** Prints welcome message. */
     private void showWelcomeMessage() {
         ui.printWelcome();
     }
+
+    /** Shows farewell message and exits the program. */
     private void showFarewellMessage() {
         ui.printFarewell();
         System.exit(0);
     }
+
+    /** Reads the user input until the user enters the bye command.  */
     private void runProcessLoop() {
         String userInput;
         do {
