949ce90cb0a042eaba30e944f561064c794d338c
Roy Tang <roy_tql@hotmail.com>
2022-03-04 23:44:25 +0800

    Add attributes, constructor and getter function for SessionDeleteCommand class

--- src/main/java/seedu/duke/command/SessionDeleteCommand.java
+++ src/main/java/seedu/duke/command/SessionDeleteCommand.java
@@ -5,5 +5,19 @@ import seedu.duke.data.Profile;
 import seedu.duke.ui.TextUI;
 public class SessionDeleteCommand extends Command {
 
+    public static final String COMMAND_TEXT = "session /delete";
+
+    private static final String COMMAND_FORMAT = "session /delete /sid <SESSIONID>";
+
+    private int sessionId;
+    public SessionDeleteCommand(int sessionId) {
+        this.sessionId = sessionId;
+    }
+    public int getSessionId() {
+        return sessionId;
+    }
+    @Override
+    public void run(TextUI ui, Profile profile) {
+
     }
 }
