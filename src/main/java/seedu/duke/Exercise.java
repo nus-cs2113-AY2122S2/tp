e9b906b4284b5ebcef3ed3a60cd370ab1076673f
alanlowzies <30099983+alanlowzies@users.noreply.github.com>
2022-03-04 18:27:58 +0800

    Exercise.java: Create class
    Placeholder class. Actual implementation is left to another team member

---
+++ src/main/java/seedu/duke/Exercise.java
@@ -0,0 +1,17 @@
+package seedu.duke;
+
+/**
+ * Placeholder class for Exercise. Will be implemented by Hao Feng.
+ */
+public class Exercise {
+    private String exerciseName;
+
+    // Accessor methods
+    public String getExerciseName() {
+        return this.exerciseName;
+    }
+
+    public void setExerciseName(String exerciseName) {
+        this.exerciseName = exerciseName;
+    }
+}
