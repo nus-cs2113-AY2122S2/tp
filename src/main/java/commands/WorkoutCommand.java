9c57d484620802da023db32be26ab0c75775f060
alanlowzies <30099983+alanlowzies@users.noreply.github.com>
2022-03-05 00:47:46 +0800

    Create WorkoutCommand.java class

---
+++ src/main/java/commands/WorkoutCommand.java
@@ -0,0 +1,18 @@
+package commands;
+
+/**
+ * A class that will handle
+ */
+public class WorkoutCommand extends Command {
+    public static final String BASE_KEYWORD = "workout";
+    public static final String[] ACTION_KEYWORDS = {
+            "/new",
+            "/list",
+            "/delete",
+            "/update"
+    };
+
+    public void execute(String userInput) {
+
+    }
+}
