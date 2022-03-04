c23184aedfa49a51a25aa876a183ef3d9919406e
warrencxw <cxw.warren@gmail.com>
2022-03-04 12:32:11 +0800

    Add abstract run method to run any Command objects

--- src/main/java/seedu/duke/command/Command.java
+++ src/main/java/seedu/duke/command/Command.java
@@ -1,4 +1,8 @@
 package seedu.duke.command;
 
+import seedu.duke.data.Profile;
+import seedu.duke.ui.TextUI;
+
 public abstract class Command {
+    public abstract void run(TextUI ui, Profile profile);
 }
