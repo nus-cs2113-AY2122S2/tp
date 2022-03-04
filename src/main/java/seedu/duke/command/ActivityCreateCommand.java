47b44eeb0d392d1d6d5ac1868013edf331028bdd
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-04 21:58:18 +0800

    Add attributes and constructor to ActivityCreateCommand class

--- src/main/java/seedu/duke/command/ActivityCreateCommand.java
+++ src/main/java/seedu/duke/command/ActivityCreateCommand.java
@@ -1,4 +1,41 @@
 package seedu.duke.command;
 
-public class ActivityCreateCommand {
+import seedu.duke.data.Profile;
+import seedu.duke.ui.TextUI;
+
+public class ActivityCreateCommand extends Command {
+
+    public static final String COMMAND_TEXT = "activity /create";
+    private static final String COMMAND_FORMAT = "There are 3 different types of formats:\n"
+            + "activity /create /sid <SESSIONID> /n <ACTIVITYNAME> /p <PAYER> /i <NAME1 NAME2…> /c <OVERALLCOST> "
+            + "[<OPTIONAL ARGS>]\n"
+            + "activity /create /sid <SESSIONID> /n <ACTIVITYNAME> /p <PAYER> /i <NAME1 NAME2…> /c <COST1 COST2…> "
+            + "[<OPTIONAL ARGS>]\n"
+            + "activity /create /sid <SESSIONID> /n <ACTIVITYNAME> /p <PAYER> /c <OVERALLCOST> [<OPTIONAL ARGS>]";
+
+    private int sessionId;
+    private String activityName;
+    private double cost;
+    private String payer;
+    private String[] involvedList;
+    private double[] costList;
+    private int gst;
+    private int serviceCharge;
+
+    public ActivityCreateCommand(int sessionId, String activityName, double cost, String payer, String[] involvedList,
+                                 double[] costList, int gst, int serviceCharge) {
+        this.sessionId = sessionId;
+        this.activityName = activityName;
+        this.cost = cost;
+        this.payer = payer;
+        this.involvedList = involvedList;
+        this.costList = costList;
+        this.gst = gst;
+        this.serviceCharge = serviceCharge;
+    }
+
+    @Override
+    public void run(TextUI ui, Profile profile) {
+
+    }
 }
