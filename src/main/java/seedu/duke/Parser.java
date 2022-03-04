6c680e41d3d5cf994a3a7b5cf1917fde5b4f9183
alanlowzies <30099983+alanlowzies@users.noreply.github.com>
2022-03-05 00:51:09 +0800

    Create Parser.java class

---
+++ src/main/java/seedu/duke/Parser.java
@@ -0,0 +1,9 @@
+package seedu.duke;
+
+/**
+ * This class will parse the input that the user enters into the WerkIt application into data
+ * that can be further processed by other classes in this application.
+ */
+public class Parser {
+    
+}
