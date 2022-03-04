19596b99873afdecb76500bd8a4f35417fe2847b
warrencxw <cxw.warren@gmail.com>
2022-03-04 12:34:24 +0800

    Add JavaDocs comment to run method in Command class

--- src/main/java/seedu/duke/command/Command.java
+++ src/main/java/seedu/duke/command/Command.java
@@ -9,5 +9,12 @@ import seedu.duke.ui.TextUI;
  * @author Warren
  */
 public abstract class Command {
+    /**
+     * Runs the command with the information parsed from the user input, using the specified
+     * Profile and TextUI objects.
+     * @param ui      A user interface to which the command will read its input from and print its output to.
+     * @param profile A Profile object from which Session, Activity and other objects are used to run
+     *                the command.
+     */
     public abstract void run(TextUI ui, Profile profile);
 }
