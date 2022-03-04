6c8ae08417b58a3ad1ac43c28af8ecbc1497c3b1
Roy Tang <roy_tql@hotmail.com>
2022-03-04 14:59:09 +0800

    Add attributes for SessionCreateCommand class

--- src/main/java/seedu/duke/command/SessionCreateCommand.java
+++ src/main/java/seedu/duke/command/SessionCreateCommand.java
@@ -1,2 +1,19 @@
 package seedu.duke.command;public class SessionCreateCommand {
+package seedu.duke.command;
+
+import seedu.duke.data.Profile;
+import seedu.duke.ui.TextUI;
+
+import java.time.LocalDate;
+
+public class SessionCreateCommand extends Command {
+
+    private String sessionName;
+    private String[] personNames;
+    private LocalDate sessionDate;
+
+    @Override
+    public void run(TextUI ui, Profile profile) {
+
+    }
 }
