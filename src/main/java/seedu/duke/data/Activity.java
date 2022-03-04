6862b7148095e8cf38488632cbaa2e933e767240
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-04 21:33:14 +0800

    Add attributes to Activity class

--- src/main/java/seedu/duke/data/Activity.java
+++ src/main/java/seedu/duke/data/Activity.java
@@ -1,9 +1,14 @@
 package seedu.duke.data;
 
+import java.util.ArrayList;
+
 public class Activity {
 
-    public Activity() {
-
-    }
+    private int sessionId;
+    private int activityId;
+    private String activityName;
+    private double cost;
+    //private Person personPaying;
+    //private ArrayList<Person> involvedPersonList;
 
 }
