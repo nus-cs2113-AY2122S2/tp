f10484cfb379b020f91186e4bc5b0c588301571a
Roy Tang <roy_tql@hotmail.com>
2022-03-04 23:43:22 +0800

    Add SessionDeleteCommand class

---
+++ src/main/java/seedu/duke/command/SessionDeleteCommand.java
@@ -0,0 +1,9 @@
+package seedu.duke.command;public class SessionDeleteCommand {
+package seedu.duke.command;
+
+import seedu.duke.data.Profile;
+import seedu.duke.ui.TextUI;
+public class SessionDeleteCommand extends Command {
+
+    }
+}
