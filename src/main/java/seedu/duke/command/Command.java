7792b1b1d5b8bdebcb808ea9f0624f4ca5a58a88
warrencxw <cxw.warren@gmail.com>
2022-03-04 12:34:03 +0800

    Add JavaDocs comment to Command class

--- src/main/java/seedu/duke/command/Command.java
+++ src/main/java/seedu/duke/command/Command.java
@@ -3,6 +3,11 @@ package seedu.duke.command;
 import seedu.duke.data.Profile;
 import seedu.duke.ui.TextUI;
 
+/**
+ * Represents a generic command that the user has entered into the application
+ * that can be run to produce a change or outcome.
+ * @author Warren
+ */
 public abstract class Command {
     public abstract void run(TextUI ui, Profile profile);
 }
