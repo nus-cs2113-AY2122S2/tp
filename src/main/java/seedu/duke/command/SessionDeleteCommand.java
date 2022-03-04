3ec9dc2a7c71372e630ffef5d9454245c2c042c9
Roy Tang <roy_tql@hotmail.com>
2022-03-04 23:44:47 +0800

    Add javadocs for SessionDeleteCommand class

--- src/main/java/seedu/duke/command/SessionDeleteCommand.java
+++ src/main/java/seedu/duke/command/SessionDeleteCommand.java
@@ -1,8 +1,13 @@
-package seedu.duke.command;public class SessionDeleteCommand {
 package seedu.duke.command;
 
 import seedu.duke.data.Profile;
 import seedu.duke.ui.TextUI;
+
+/**
+ * Represents a command that deletes a session indicated by the user input.
+ *
+ * @author Roy
+ */
 public class SessionDeleteCommand extends Command {
 
     public static final String COMMAND_TEXT = "session /delete";
@@ -10,12 +15,28 @@ public class SessionDeleteCommand extends Command {
     private static final String COMMAND_FORMAT = "session /delete /sid <SESSIONID>";
 
     private int sessionId;
+
+    // Javadocs to be completed when implementing command.
     public SessionDeleteCommand(int sessionId) {
         this.sessionId = sessionId;
     }
+
+    /**
+     * Returns the session id for session deletion.
+     *
+     * @return A session id as int.
+     */
     public int getSessionId() {
         return sessionId;
     }
+
+    /**
+     * Runs the command to delete a session.
+     *
+     * @param ui      A user interface to which the command will read its input from and print its output to.
+     * @param profile A Profile object from which Session, Activity and other objects are used to run
+     *                the command.
+     */
     @Override
     public void run(TextUI ui, Profile profile) {
 
