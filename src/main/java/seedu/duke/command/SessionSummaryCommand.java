07c6a7b818b973f8bb7c7ff3cc06d68c887da5fd
warrencxw <cxw.warren@gmail.com>
2022-03-05 01:25:33 +0800

    Add SessionSummaryCommand class
    Add class member attributes and constructors
    Add JavaDoc comments to the class and methods

---
+++ src/main/java/seedu/duke/command/SessionSummaryCommand.java
@@ -0,0 +1,38 @@
+package seedu.duke.command;
+
+import seedu.duke.data.Profile;
+import seedu.duke.ui.TextUI;
+
+/**
+ * Represents a command object that when run, produces a summary of expenditure for that session.
+ * 
+ * @author Warren
+ */
+public class SessionSummaryCommand extends Command {
+    public static final String COMMAND_TEXT = "session /summary";
+    
+    public static final String COMMAND_FORMAT = "Syntax: session /summary /sid <SESSIONID>";
+    
+    private int sessionId;
+
+    /**
+     * Runs the command with the session identifier as provided by the user input and prints a
+     * summary of expenditure for the session specified by the session identifier.
+     *      
+     * @param ui      A user interface to which the command will read its input from and print its output to.
+     * @param profile A Profile object from which Session, Activity and other objects are used to run
+     */
+    @Override
+    public void run(TextUI ui, Profile profile) {
+        
+    }
+
+    /**
+     * Default constructor, sets sessionId as specified by the provided value.
+     * 
+     * @param sessionId The session identifier number that uniquely identifies a session.
+     */
+    public SessionSummaryCommand(int sessionId) {
+        this.sessionId = sessionId;
+    }
+}
