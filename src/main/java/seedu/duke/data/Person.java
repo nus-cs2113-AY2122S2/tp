6d74dfaec0a0b2bc0248558ad456bc519d8ccbd7
matheril <sauravvm237@gmail.com>
2022-03-04 22:26:29 +0800

    Add getters and setters and documentation to Person

--- src/main/java/seedu/duke/data/Person.java
+++ src/main/java/seedu/duke/data/Person.java
@@ -4,8 +4,13 @@ import seedu.duke.exceptions.InvalidDataException;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a person.
+ * Stores its name, total cost and a list of activityCosts.
+ * @author Saurav
+ */
 public class Person {
-    private String name;
+    private final String name;
     private double totalCost;
     private ArrayList<ActivityCost> activityCostList;
 
@@ -16,10 +21,28 @@ public class Person {
         this.name = name;
     }
 
+    /**
+     * Constructs an activityCost and adds it to the list of activityCosts.
+     * @param activityId the activityId.
+     * @param cost the cost of the activity.
+     * @throws InvalidDataException if the activityCost cannot be created from the given parameters.
+     */
     public void addActivityCost(int activityId, double cost) throws InvalidDataException {
         ActivityCost activityCost = new ActivityCost(activityId, cost);
         activityCostList.add(activityCost);
         totalCost += activityCost.getCost();
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<ActivityCost> getActivityCostList() {
+        return activityCostList;
     }
 
 
