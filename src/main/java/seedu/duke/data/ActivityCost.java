c927053805d6195ea787f0da1d7dd8c48e5e54f6
matheril <sauravvm237@gmail.com>
2022-03-04 15:47:04 +0800

    Add ActivityCost class
    Represents an activity and its cost.
    Currently stores activityId and cost of the activity.
    Currently contains constructor only, accepting activityId and cost.

---
+++ src/main/java/seedu/duke/data/ActivityCost.java
@@ -0,0 +1,25 @@
+package seedu.duke.data;
+
+import seedu.duke.exceptions.InvalidDataException;
+
+/**
+ * Represents an Activity and its cost.
+ * Stores activityId and cost of the activity.
+ * @author Saurav
+ */
+public class ActivityCost {
+    private int activityId;
+    private double cost;
+
+    public ActivityCost(int activityId, double cost) throws InvalidDataException {
+        if (activityId < 0) {
+            throw new InvalidDataException("Activity ID cannot be negative.");
+        }
+        if (cost < 0) {
+            throw new InvalidDataException("Cost cannot be negative.");
+        }
+        this.activityId = activityId;
+        this.cost = cost;
+    }
+
+}
