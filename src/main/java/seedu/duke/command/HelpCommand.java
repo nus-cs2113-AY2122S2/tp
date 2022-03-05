09501c29b7037dffea9473806c506837c8fd8211
warrencxw <cxw.warren@gmail.com>
2022-03-06 01:33:19 +0800

    Implement functionality of HelpCommand class
    Add run method
    Add member attributes
    Add printHelpMenu() method to TextUI class
    Add prototype help message to Message class

--- src/main/java/seedu/duke/command/HelpCommand.java
+++ src/main/java/seedu/duke/command/HelpCommand.java
@@ -1,4 +1,15 @@
 package seedu.duke.command;
 
-public class HelpCommand {
+import seedu.duke.data.Profile;
+import seedu.duke.ui.TextUI;
+
+public class HelpCommand extends Command {
+    public static final String COMMAND_TEXT = "help";
+
+    private static final String COMMAND_FORMAT = "Syntax: help";
+
+    @Override
+    public void run(TextUI ui, Profile profile) {
+        ui.printHelpMenu();
+    }
 }
