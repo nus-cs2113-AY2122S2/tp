0a244c16386044cf6007e07f38f60046eb4b13e2
alanlowzies <30099983+alanlowzies@users.noreply.github.com>
2022-03-05 10:42:23 +0800

    Repackage and update Workout.java

--- src/main/java/data/workouts/Workout.java
+++ src/main/java/data/workouts/Workout.java
@@ -1,19 +1,23 @@
-package seedu.duke;
+package data.workouts;
 
 /**
  * This class represents a Workout, which is an exercise with a specified number of repetitions.
  */
 public class Workout {
-    private Exercise exercise;
+    private String exercise;
     private int repetitions;
 
-    // Accessor methods
-    public Exercise getExercise() {
+    public Workout(String exerciseName, int repetitions) {
+        this.exercise = exerciseName;
+        this.repetitions = repetitions;
+    }
+
+    public String getExercise() {
         return exercise;
     }
 
-    public void setExercise(Exercise exercise) {
-        this.exercise = exercise;
+    public void setExercise(String string) {
+        this.exercise = string;
     }
 
     public int getRepetitions() {
