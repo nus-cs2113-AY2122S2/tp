ec50dbc3e2e4d53582ae3f1db3bf81b629c10f3e
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-05 13:31:40 +0800

    Add Javadoc for constructor of Activity class

--- src/main/java/seedu/duke/data/Activity.java
+++ src/main/java/seedu/duke/data/Activity.java
@@ -11,16 +11,26 @@ public class Activity {
     private int activityId;
     private String activityName;
     private double cost;
-    private Person personPaying;
+    private Person personPaid;
     private ArrayList<Person> involvedPersonList;
 
-    public Activity(int sessionId, int activityId, String activityName, double cost, Person personPaying,
+    /**
+     * Constructs an Activity object that stores the relevant information required for an Activity.
+     *
+     * @param sessionId The id of the session.
+     * @param activityId The id of the activity
+     * @param activityName The name of the activity.
+     * @param cost The total cost of the activity.
+     * @param personPaid The person who paid for the activity.
+     * @param involvedPersonList The persons involved in the activity.
+     */
+    public Activity(int sessionId, int activityId, String activityName, double cost, Person personPaid,
                     ArrayList<Person> involvedPersonList) {
         this .sessionId = sessionId;
         this.activityId = activityId;
         this.activityName = activityName;
         this.cost = cost;
-        this.personPaying = personPaying;
+        this.personPaid = personPaid;
         this.involvedPersonList = involvedPersonList;
     }
 
