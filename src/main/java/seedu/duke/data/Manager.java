9dc6533db033d2ea469d66f4db0c3b80661efe0c
Roy Tang <roy_tql@hotmail.com>
2022-03-06 00:09:38 +0800

    Add javadocs for Manager class

--- src/main/java/seedu/duke/data/Manager.java
+++ src/main/java/seedu/duke/data/Manager.java
@@ -1,18 +1,34 @@
-package seedu.duke.data;public class Manager {
 package seedu.duke.data;
 
 import seedu.duke.ui.TextUI;
 
+/**
+ * Represents a manager that manages the UI and Profile objects of the application.
+ */
 public class Manager {
     private TextUI ui;
     private Profile profile;
+
+    /**
+     * Constructor to create a Manager object.
+     */
     public Manager() {
         ui = new TextUI();
         profile = new Profile();
     }
+
+    /**
+     * Returns a TextUI object for user inputs and outputs.
+     * @return A TextUI object.
+     */
     public TextUI getUi() {
         return ui;
     }
+
+    /**
+     * Returns a Profile object that stores user created sessions.
+     * @return
+     */
     public Profile getProfile() {
         return profile;
     }
