46a1472ad001ac8875f2c09af56eefe83b230179
Roy Tang <roy_tql@hotmail.com>
2022-03-05 21:24:56 +0800

    Add command_text and command_format for ExitCommand class

--- src/main/java/seedu/duke/command/ExitCommand.java
+++ src/main/java/seedu/duke/command/ExitCommand.java
@@ -5,4 +5,11 @@ import seedu.duke.ui.TextUI;
 
 public class ExitCommand extends Command {
 
+    public static final String COMMAND_TEXT = "exit";
+
+    private static final String COMMAND_FORMAT = "Syntax: exit";
+    @Override
+    public void run(TextUI ui, Profile profile) {
+
+    }
 }
