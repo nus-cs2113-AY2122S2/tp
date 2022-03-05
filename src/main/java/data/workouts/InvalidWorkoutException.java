2eea5a81aa93698680a74de5aa7e4a847d11403d
alanlowzies <30099983+alanlowzies@users.noreply.github.com>
2022-03-05 17:20:47 +0800

    Rename InvalidWorkoutsException.java to InvalidWorkoutException.java

--- src/main/java/data/workouts/InvalidWorkoutException.java
+++ src/main/java/data/workouts/InvalidWorkoutException.java
@@ -1,11 +1,11 @@
 package data.workouts;
 
-public class InvalidWorkoutsException extends Exception {
+public class InvalidWorkoutException extends Exception {
     public static final String INVALID_REPS_VALUE = "The number of reps specified is invalid.";
 
     private String throwingClass;
 
-    public InvalidWorkoutsException(String throwingClass, String errorMessage) {
+    public InvalidWorkoutException(String throwingClass, String errorMessage) {
         super(errorMessage);
         this.throwingClass = throwingClass;
     }
