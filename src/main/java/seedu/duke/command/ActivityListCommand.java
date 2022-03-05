ed6f33a032756f2eaa603a4902850f24b98eafe9
RenTianle <lele.rer@outlook.com>
2022-03-05 16:28:01 +0800

    Remove new line between the Javadoc for the class and the start of the class. Change from "Run" to "Runs"

--- src/main/java/seedu/duke/command/ActivityListCommand.java
+++ src/main/java/seedu/duke/command/ActivityListCommand.java
@@ -4,9 +4,8 @@ import seedu.duke.data.Profile;
 import seedu.duke.ui.TextUI;
 
 /**
- * Represents an ActivityListCommand which display activity ids, name, cost and payer within session".
+ * Represents an ActivityListCommand which display activity ids, name, cost and payer within session.
  */
-
 public class ActivityListCommand extends Command {
 
     public static final String COMMAND_TEXT = "activity /list";
@@ -21,8 +20,9 @@ public class ActivityListCommand extends Command {
         this.sessionId = sessionId;
     }
 
+
     /**
-     * Run the command.
+     * Runs the command.
      * @param ui      A user interface to which the command will read its input from and print its output to.
      * @param profile A Profile object from which Session, Activity and other objects are used to run.
      */
