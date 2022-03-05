ad6cafa2481f46fe737cd981c7d91cb7cf124204
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-05 13:51:15 +0800

    Remove sessionId as an attribute from Activity class

--- src/main/java/seedu/duke/data/Activity.java
+++ src/main/java/seedu/duke/data/Activity.java
@@ -6,8 +6,7 @@ import java.util.ArrayList;
  * Represents an activity that was done during a group outing session.
  */
 public class Activity {
-
-    private int sessionId;
+    
     private int activityId;
     private String activityName;
     private double cost;
@@ -17,16 +16,14 @@ public class Activity {
     /**
      * Constructs an Activity object that stores the relevant information required for an Activity.
      *
-     * @param sessionId The id of the session.
      * @param activityId The id of the activity
      * @param activityName The name of the activity.
      * @param cost The total cost of the activity.
      * @param personPaid The person who paid for the activity.
      * @param involvedPersonList The persons involved in the activity.
      */
-    public Activity(int sessionId, int activityId, String activityName, double cost, Person personPaid,
+    public Activity(int activityId, String activityName, double cost, Person personPaid,
                     ArrayList<Person> involvedPersonList) {
-        this.sessionId = sessionId;
         this.activityId = activityId;
         this.activityName = activityName;
         this.cost = cost;
