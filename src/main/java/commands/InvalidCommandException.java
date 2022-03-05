3d91f857af5be0eb6c3608ac776cc8cb789e3d0d
alanlowzies <30099983+alanlowzies@users.noreply.github.com>
2022-03-05 10:40:47 +0800

    Create InvalidCommandException.java

---
+++ src/main/java/commands/InvalidCommandException.java
@@ -0,0 +1,17 @@
+package commands;
+
+public class InvalidCommandException extends Exception {
+    public static final String INVALID_COMMAND_ERROR_MSG = "The command entered is unrecognised.";
+    public static final String INVALID_ACTION_ERROR_MSG = "An invalid user action was specified.";
+
+    private String throwingClass;
+
+    public InvalidCommandException (String throwingClass, String errorMessage) {
+        super(errorMessage);
+        this.throwingClass = throwingClass;
+    }
+
+    public String getThrowingClass() {
+        return this.throwingClass;
+    }
+}
