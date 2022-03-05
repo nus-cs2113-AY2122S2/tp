3ae37cd0d05e554fd010f6bd983c43ac285ddd14
alanlowzies <30099983+alanlowzies@users.noreply.github.com>
2022-03-05 18:33:21 +0800

    Shift main method to Main.java class

--- src/main/java/werkIt/WerkIt.java
+++ src/main/java/werkIt/WerkIt.java
@@ -1,26 +1,77 @@
 package werkIt;
 
 import commands.Command;
+import commands.ExitCommand;
 import commands.InvalidCommandException;
+import data.exercises.ExerciseList;
+import data.workouts.WorkoutList;
+
+import java.io.IOException;
 
 public class WerkIt {
+    private UI ui;
+    private Parser parser;
+    private ExerciseList exerciseList;
+    private WorkoutList workoutList;
+
     /**
-     * Main entry-point for the java.duke.Duke application.
+     * Initialises the components of the WerkIt! application.
      */
-    public static void main(String[] args) {
+    public WerkIt() {
         // Initialise Components
-        UI ui = new UI();
-        
+        ui = new UI();
+        parser = new Parser();
+        exerciseList = new ExerciseList();
+        workoutList = new WorkoutList();
 
+        ui.printGreetings();
 
-        String userInput = "workout /wut russian twist /reps 1";
-        Parser parser = new Parser();
+        // Do file imports
+        System.out.println("(WIP) File import messages will go here");
+
         try {
-            Command newCommand = parser.parseUserInput(userInput);
-            newCommand.execute();
-        } catch (InvalidCommandException e) {
+            exerciseList.loadExercises();
+        } catch (IOException e) {
             System.out.println(e.getMessage());
-            System.out.println(e.getThrowingClass());
         }
+    }
+
+    public void initialiseComponents() {
+        ui = new UI();
+        parser = new Parser();
+        exerciseList = new ExerciseList();
+        workoutList = new WorkoutList();
+    }
+
+    /**
+     * Method adapted from Team Member Alan Low's iP codebase.
+     * Link: https://github.com/alanlowzies/ip/blob/8556dd6a5106d190f5ac0458c6d2c34f98737a91/src/main/java/sora/Sora.java
+     */
+    public void startContinuousUserPrompt() {
+        boolean userWantsToExit = false;
+        boolean isFirstPrompt = true;
+
+            do {
+                try {
+                    ui.printUserInputPrompt(isFirstPrompt);
+                    isFirstPrompt = false;
+                    String userInput = ui.getUserInput();
+                    Command newCommand = parser.parseUserInput(userInput);
+
+                    if (newCommand instanceof ExitCommand) {
+                        userWantsToExit = true;
+                        continue;
+                    }
+
+                    newCommand.execute();
+
+                } catch (InvalidCommandException e) {
+                    System.out.println(e.getMessage());
+                    System.out.println("Please try again.");
+                }
+            } while (!userWantsToExit);
+
+        // User is exiting the program
+        ui.printGoodbye();
     }
 }
