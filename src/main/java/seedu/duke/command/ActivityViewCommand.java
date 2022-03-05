4228e9bd996cc89584950f76c2f256bf0b502c06
RenTianle <lele.rer@outlook.com>
2022-03-05 16:56:19 +0800

    Add Javadoc

--- src/main/java/seedu/duke/command/ActivityViewCommand.java
+++ src/main/java/seedu/duke/command/ActivityViewCommand.java
@@ -21,6 +21,11 @@ public class ActivityViewCommand extends Command {
         this.activityId = activityId;
     }
 
+    /**
+     * Runs the command.
+     * @param ui      A user interface to which the command will read its input from and print its output to.
+     * @param profile A Profile object from which Session, Activity and other objects are used to.
+     */
     @Override
     public void run(TextUI ui, Profile profile) {
 
