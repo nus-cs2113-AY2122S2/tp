b068157cf6d3c94f63cef2ab9d5053a36208f805
alanlowzies <30099983+alanlowzies@users.noreply.github.com>
2022-03-05 19:57:44 +0800

    InvalidWorkoutException.java: Update code and inserted JavaDocs

--- src/main/java/data/workouts/InvalidWorkoutException.java
+++ src/main/java/data/workouts/InvalidWorkoutException.java
@@ -1,10 +1,22 @@
 package data.workouts;
 
+/**
+ * Thrown to indicate that the Workout being created has some invalid parameters.
+ */
 public class InvalidWorkoutException extends Exception {
-    public static final String INVALID_REPS_VALUE = "The number of reps specified is invalid.";
+    // Pre-defined error messages
+    public static final String INVALID_REPS_VALUE = "The number of reps specified is invalid.\n" +
+            "(Needs to be a value of at least 1.)";
 
     private String throwingClass;
 
+    /**
+     * Constructs an InvalidWorkoutException with the class name that this exception instance was
+     * thrown from and the accompanying error message.
+     *
+     * @param throwingClass The class name that this exception instance was thrown from.
+     * @param errorMessage The error message given by the throwing method.
+     */
     public InvalidWorkoutException(String throwingClass, String errorMessage) {
         super(errorMessage);
         this.throwingClass = throwingClass;
