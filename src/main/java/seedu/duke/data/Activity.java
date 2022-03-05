e79123e83eebe8d5dea395472e07829bfddc0dab
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-05 13:59:10 +0800

    Update cost to totalCost for better clarity in Activity class

--- src/main/java/seedu/duke/data/Activity.java
+++ src/main/java/seedu/duke/data/Activity.java
@@ -6,10 +6,10 @@ import java.util.ArrayList;
  * Represents an activity that was done during a group outing session.
  */
 public class Activity {
-    
+
     private int activityId;
     private String activityName;
-    private double cost;
+    private double totalCost;
     private Person personPaid;
     private ArrayList<Person> involvedPersonList;
 
@@ -18,15 +18,15 @@ public class Activity {
      *
      * @param activityId The id of the activity
      * @param activityName The name of the activity.
-     * @param cost The total cost of the activity.
+     * @param totalCost The total cost of the activity.
      * @param personPaid The person who paid for the activity.
      * @param involvedPersonList The persons involved in the activity.
      */
-    public Activity(int activityId, String activityName, double cost, Person personPaid,
+    public Activity(int activityId, String activityName, double totalCost, Person personPaid,
                     ArrayList<Person> involvedPersonList) {
         this.activityId = activityId;
         this.activityName = activityName;
-        this.cost = cost;
+        this.totalCost = totalCost;
         this.personPaid = personPaid;
         this.involvedPersonList = involvedPersonList;
     }
