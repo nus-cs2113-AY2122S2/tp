ad605abba0e2c6e995928d707a973be01c14f6b5
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-06 20:04:14 +0800

    Add attributes to ActivityDeleteCommand class

--- src/main/java/seedu/duke/command/ActivityDeleteCommand.java
+++ src/main/java/seedu/duke/command/ActivityDeleteCommand.java
@@ -1,4 +1,17 @@
 package seedu.duke.command;
 
-public class ActivityDeleteCommand {
+import seedu.duke.data.Manager;
+
+public class ActivityDeleteCommand extends Command {
+
+    public static final String COMMAND_TEXT = "activity /delete";
+    private static final String COMMAND_FORMAT = "activity /delete /sid <SESSIONID> /aid <ACTIVITYID>";
+
+    private int sessionId;
+    private int activityId;
+
+    @Override
+    public void run(Manager manager) {
+
+    }
 }
