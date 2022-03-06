486270217a7ad25176be18c562f8c2086d2dd0c3
Roy Tang <roy_tql@hotmail.com>
2022-03-06 12:02:56 +0800

    Add command_text and command_format for SessionListCommand

--- src/main/java/seedu/duke/command/SessionListCommand.java
+++ src/main/java/seedu/duke/command/SessionListCommand.java
@@ -1,6 +1,11 @@
 package seedu.duke.command;public class SessionListCommand {
 package seedu.duke.command;
 public class SessionListCommand extends Command {
+
+    public static final String COMMAND_TEXT = "session /list";
+
+    private static final String COMMAND_FORMAT = "Syntax: session /list";
+
     @Override
     public void run(Manager manager) {
 
