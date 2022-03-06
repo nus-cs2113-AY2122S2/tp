46e4f70a7420ca664759c11d7df54001d05f1c0b
2211743035 <e0589902@u.nus.edu>
2022-03-06 17:27:54 +0800

    Create TextUiTest.java
    Add JUnit for Ui

---
+++ src/test/java/seedu/duke/ui/TextUiTest.java
@@ -0,0 +1,11 @@
+package seedu.duke.ui;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TextUiTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
