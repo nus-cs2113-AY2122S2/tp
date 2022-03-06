5bbe764d9b85434ee1141ec5f6244ff70aae0755
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-06 20:05:07 +0800

    Add constructor in ActivityDeleteCommand class

--- src/main/java/seedu/duke/command/ActivityDeleteCommand.java
+++ src/main/java/seedu/duke/command/ActivityDeleteCommand.java
@@ -5,10 +5,15 @@ import seedu.duke.data.Manager;
 public class ActivityDeleteCommand extends Command {
 
     public static final String COMMAND_TEXT = "activity /delete";
-    private static final String COMMAND_FORMAT = "activity /delete /sid <SESSIONID> /aid <ACTIVITYID>";
+    private static final String COMMAND_FORMAT = "Syntax: activity /delete /sid <SESSIONID> /aid <ACTIVITYID>";
 
     private int sessionId;
     private int activityId;
+
+    public ActivityDeleteCommand(int sessionId, int activityId) {
+        this.sessionId = sessionId;
+        this.activityId = activityId;
+    }
 
     @Override
     public void run(Manager manager) {
