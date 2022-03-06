8630f9550c329bbc25c0cfd5ace7e939aa25f1ee
Roy Tang <roy_tql@hotmail.com>
2022-03-06 12:02:33 +0800

    Add SessionListCommand class

---
+++ src/main/java/seedu/duke/command/SessionListCommand.java
@@ -0,0 +1,8 @@
+package seedu.duke.command;public class SessionListCommand {
+package seedu.duke.command;
+public class SessionListCommand extends Command {
+    @Override
+    public void run(Manager manager) {
+
+    }
+}
