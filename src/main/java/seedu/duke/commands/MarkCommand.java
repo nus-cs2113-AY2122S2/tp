3d786754e45d285e4c090ed165c0630430665ebd
Yzkkk <yangzikun0511@gmail.com>
2022-03-08 01:25:37 +0800

    fixed typo

--- src/main/java/seedu/duke/commands/MarkCommand.java
+++ src/main/java/seedu/duke/commands/MarkCommand.java
@@ -2,6 +2,7 @@ package seedu.duke.commands;
 
 import seedu.duke.exceptions.ModHappyException;
 import seedu.duke.parsers.AddParser;
+import seedu.duke.parsers.MarkParser;
 
 import java.util.HashMap;
 
@@ -12,16 +13,18 @@ public class MarkCommand extends Command{
     private static final String UNCOMPLETED_FLAG = "/u";
     private static final String MARK_MESSAGE_TOP = "Nice! I have marked this task as completed!\n";
     private static final String UNMARK_MESSAGE_TOP = "Ok! I have marked this task for you as uncompleted!\n";
+    private static int taskNumber = 0;
 
     public MarkCommand (String arg) throws ModHappyException {
         try {
             commandName = MARK_COMMAND_WORD;
-            AddParser addParser = new AddParser();
-            HashMap<String, String> parsedArg = addParser.parseString(arg);
+            MarkParser markParser = new MarkParser();
+            HashMap<String, String> parsedArg = markParser.parseString(arg);
 
             switch (parsedArg.get(FLAG)) {
             case COMPLETED_FLAG:
-                //
+                //mark as completed
+                taskNumber = Integer.parseInt(parsedArg.get("taskNumber"));
 
                 break;
             case UNCOMPLETED_FLAG:
