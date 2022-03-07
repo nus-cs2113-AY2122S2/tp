b540169721b90d690671571714f3e5046d9bd531
Roy Tang <roy_tql@hotmail.com>
2022-03-07 10:51:50 +0800

    Rename package from duke to splitlah

---
+++ src/main/java/seedu/splitlah/command/ExitCommand.java
@@ -0,0 +1,23 @@
+package seedu.splitlah.command;
+
+import seedu.splitlah.data.Manager;
+
+/**
+ * Represents a command that exits the application.
+ */
+public class ExitCommand extends Command {
+
+    public static final String COMMAND_TEXT = "exit";
+
+    private static final String COMMAND_FORMAT = "Syntax: exit";
+
+    /**
+     * Prints the farewell message.
+     *
+     * @param manager A manager object that gets the UI object to print output.
+     */
+    @Override
+    public void run(Manager manager) {
+        manager.getUi().printFarewell();
+    }
+}
---
+++ src/test/java/seedu/splitlah/SplitLahTest.java
@@ -0,0 +1,12 @@
+package seedu.splitlah;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class SplitLahTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
