a752cfa65d48ed3ff8350bf88eb6b5c9573812cf
yuhaochua <yuhaochua99@gmail.com>
2022-03-07 13:35:15 +0800

    Add ListCommand to handle listing of equipment
    Fixes #26

---
+++ src/main/java/seedu/command/ListCommand.java
@@ -0,0 +1,64 @@
+package seedu.command;
+
+import java.util.ArrayList;
+
+/**
+ * Subclass of Command. Handles listing of all equipment or listing of equipment of specified type.
+ */
+public class ListCommand extends Command {
+    private final ArrayList<String> COMMAND_STRINGS;
+    public final String COMMAND_WORD = "list";
+
+    /**
+     * constructor for ListCommand with NO specified type. Initialises successMessage and usageReminder from Command
+     * @param commandStrings parsed user input which contains details of equipment to be viewed
+     */
+    public ListCommand(){
+        COMMAND_STRINGS = null;
+        successMessage = "TOTAL QUANTITY OF EQUIPMENT: %1$d\n";
+        usageReminder = COMMAND_WORD + ": print a list of all equipment in the inventory. "
+                + "Parameters: NIL"
+                + "Example: "
+                + "list\n"
+                + COMMAND_WORD + " t/Type: print a list of all equipment in the inventory of the specified type"
+                + "Parameters: t/Type"
+                + "Example: "
+                + "list Microphone";
+    }
+
+    /**
+     * constructor for ListCommand with specified type. Initialises successMessage and usageReminder from Command
+     * @param commandStrings parsed user input which contains details of equipment to be viewed
+     */
+    public ListCommand(ArrayList<String> commandStrings){
+        COMMAND_STRINGS = commandStrings;
+        successMessage = "TOTAL QUANTITY OF %1$s: %2$d\n";
+        usageReminder = COMMAND_WORD + ": print a list of all equipment in the inventory. "
+                + "Parameters: NIL"
+                + "Example: "
+                + "list\n"
+                + COMMAND_WORD + " t/Type: print a list of all equipment in the inventory of the specified type"
+                + "Parameters: t/Type"
+                + "Example: "
+                + "list Microphone";
+    }
+
+    /**
+     * List all equipment or list equipment of specified type
+     * @return CommandResult with message from execution of this command
+     */
+    public CommandResult execute(){
+        int listSize;
+        ArrayList<Equipment> equipmentArrayList;
+        if(COMMAND_STRINGS == null){
+            equipmentArrayList = equipmentInventory.listEquipment();
+            listSize = equipmentArrayList.size();
+            return new CommandResult(String.format(successMessage, listSize), equipmentArrayList);
+        }
+
+        String typeToList = COMMAND_STRINGS.get(0);
+        equipmentArrayList = equipmentInventory.listEquipment(typeToList);
+        listSize = equipmentArrayList.size();
+        return new CommandResult(String.format(successMessage, typeToList, listSize), equipmentArrayList);
+    }
+}
