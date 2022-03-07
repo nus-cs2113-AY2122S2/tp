9ad5cd67b3e7ef3903c1be8ab0ae6bdcb1a41519
wli-linda <wli62442@gmail.com>
2022-03-07 18:05:25 +0800

    Implement naive listContacts() and Contact class

--- src/main/java/seedu/duke/contacts/ContactsManager.java
+++ src/main/java/seedu/duke/contacts/ContactsManager.java
@@ -3,16 +3,30 @@ package seedu.duke.contacts;
 
 import seedu.duke.ui.TextUi;
 
+import java.util.ArrayList;
+
 public class ContactsManager {
 
     private static final String CONTACTS_WELCOME_MESSAGE = "Welcome to Contacts Manager";
+    private static final int CONTACTS_LIST_MAX_SIZE = 100;
+    private static final ArrayList<Contact> contactsList = new ArrayList<>(CONTACTS_LIST_MAX_SIZE);
 
     private static void contactsWelcome() {
         System.out.println(CONTACTS_WELCOME_MESSAGE);
     }
 
     private static void listContacts() {
-        System.out.println("list");
+        if (contactsList.size() == 0) {
+            System.out.println("You haven't added any contacts to your list yet!");
+            return;
+        }
+
+        String listAsString = "";
+        for (int i = 0; i < contactsList.size(); i++) {
+            Contact curr = contactsList.get(i);
+            listAsString = listAsString.concat(String.format(" %d. %s\n", i + 1, curr));
+        }
+        System.out.println("Here are the contacts in your list:\n" + listAsString);
     }
 
     private static void deleteContact() {
