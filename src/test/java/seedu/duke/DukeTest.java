fe969420297c8fdaf772c9814ba99a8fd5ee44dc
Toh Yan Jie <yanjie20009@gmail.com>
2022-03-09 02:38:42 +0800

    Revert "Refactor Duke to MeetingJio"
    This reverts commit 6d13608556041d2e6ca6e5dcb0af2ef9e4f275a6.

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -1,10 +1,10 @@
-package seedu.meetingjio;
+package seedu.duke;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-class MeetingJioTest {
+class DukeTest {
     @Test
     public void sampleTest() {
         assertTrue(true);
