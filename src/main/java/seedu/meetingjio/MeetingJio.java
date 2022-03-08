71a6fa2f8b6742b067681826feddcd5b146ee225
Toh Yan Jie <yanjie20009@gmail.com>
2022-03-09 02:15:41 +0800

    Add Parser.java

--- src/main/java/seedu/meetingjio/MeetingJio.java
+++ src/main/java/seedu/meetingjio/MeetingJio.java
@@ -3,6 +3,7 @@ package seedu.meetingjio;
 import java.util.Scanner;
 
 public class MeetingJio {
+    private static Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Hello from MeetingJio\n");
@@ -10,5 +11,12 @@ public class MeetingJio {
 
         Scanner in = new Scanner(System.in);
         System.out.println("Hello " + in.nextLine());
+
+        String input = getInput();
+        Parser parser = new Parser(input);
+    }
+
+    private static String getInput() {
+        return SCANNER.nextLine().trim();
     }
 }
