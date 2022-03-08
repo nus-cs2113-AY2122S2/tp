0c818564febc624e70a5bfcce9c85013673a56cb
2211743035 <e0589902@u.nus.edu>
2022-03-08 14:47:13 +0800

    Modify the CommandResult to support ArrayList type result
    Modify the CommandResult to support ArrayList type result

--- src/main/java/seedu/duke/commands/CommandResult.java
+++ src/main/java/seedu/duke/commands/CommandResult.java
@@ -1,27 +1,61 @@
 package seedu.duke.commands;
 
+import java.util.ArrayList;
+
 public class CommandResult {
 
+    private static final String ARRAYLIST_RESULT = "ArrayList";
+    private static final String NULL_STRING = "";
     private static final String STRING_RESULT = "String";
-    private String commandResultType = "String";
-    private Object result;
 
-    public CommandResult(Object result) {
-        this.result = result;
+    private String commandResultType = "String";
+    private Object resultString;
+    private ArrayList<String> resultArrayList;
+    private String startWords = "";
+    private String endWords = "";
+
+    public CommandResult(String result) {
+        this.resultString = result;
     }
 
-    public CommandResult(Object result, String commandResultType) {
-        this.result = result;
+    public CommandResult(String resultString, String commandResultType) {
+        this.resultString = resultString;
         this.commandResultType = commandResultType;
+    }
+
+    public CommandResult(ArrayList<String> result, String commandResultType) {
+        this.resultArrayList = result;
+        this.commandResultType = commandResultType;
+    }
+
+    public void setStartWords(String startWords) {
+        this.startWords = startWords;
+    }
+
+
+    public void setEndWords(String endWords) {
+        this.startWords = endWords;
     }
 
     @Override
     public String toString() {
         switch (commandResultType) {
         case STRING_RESULT:
-            return result.toString();
+            return resultString.toString();
+        case ARRAYLIST_RESULT:
+            String result = NULL_STRING;
+            if (!startWords.equals(NULL_STRING)) {
+                result = startWords + "\n";
+            }
+            for (int i = 0; i < resultArrayList.size(); i++) {
+                result += String.format("%s. %s\n", i + 1, resultArrayList.get(i).toString());
+            }
+            if (!endWords.equals(NULL_STRING)) {
+                result = startWords + "\n";
+            }
+            return result;
         default:
-            throw new UnsupportedOperationException(result.toString());
+            throw new UnsupportedOperationException();
         }
     }
 
