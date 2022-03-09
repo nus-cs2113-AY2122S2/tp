169596bb64b1fe57a8ecc57526a5558f46b5f0f4
warrencxw <cxw.warren@gmail.com>
2022-03-09 15:49:25 +0800

    Add PersonCostPair utility class to support SessionSummaryCommand
    Add new package util for utility classes

---
+++ src/main/java/seedu/splitlah/util/PersonCostPair.java
@@ -0,0 +1,12 @@
+package seedu.splitlah.util;
+
+import seedu.splitlah.data.Person;
+
+import java.util.Comparator;
+
+/**
+ * Represents a total cost that the person must pay or receive to settle the transactions for the session.
+ * A negative value represents a debt to pay another person with a positive value.
+ */
+public class PersonCostPair {
+}
