db6d81e204e781bc49aa5ddc5cca75e4912cafcc
johnmcmonigle <60244374+johnmcmonigle@users.noreply.github.com>
2022-03-09 17:34:25 +0800

    Refactor 'Duke' to be called 'CSProjPlanner'

--- src/main/java/seedu/duke/CSProjPlanner.java
+++ src/main/java/seedu/duke/CSProjPlanner.java
@@ -1,19 +1,15 @@
 package seedu.duke;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
 import seedu.duke.command_handler.CommandHandler;
-import seedu.duke.projects.Project;
 import seedu.duke.projects.ProjectList;
 
-public class Duke {
+public class CSProjPlanner {
 
     private static ProjectList projectList = new ProjectList();
     private static CommandHandler commandHandler = new CommandHandler();
 
     /**
-     * Main entry-point for the java.duke.Duke application.
+     * Main entry-point for the CSProjPlanner application.
      */
     public static void main(String[] args) {
 
