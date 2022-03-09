e6f26ce7783a6ee4b21086b3126b4d6d52e971df
warrencxw <cxw.warren@gmail.com>
2022-03-09 16:20:00 +0800

    Add minor supporting methods
    Add necessary constants

--- src/main/java/seedu/splitlah/command/SessionSummaryCommand.java
+++ src/main/java/seedu/splitlah/command/SessionSummaryCommand.java
@@ -1,18 +1,23 @@
 package seedu.splitlah.command;
 
 import seedu.splitlah.data.Manager;
+import seedu.splitlah.exceptions.InvalidFormatException;
 
 /**
  * Represents a command object that when run, produces a summary of expenditure for that session.
- * 
+ *
  * @author Warren
  */
 public class SessionSummaryCommand extends Command {
     public static final String COMMAND_TEXT = "session /summary";
-    
+
     public static final String COMMAND_FORMAT = "Syntax: session /summary /sid <SESSIONID>";
-    
+
     private int sessionId;
+
+    // MISC CONSTANTS
+    private static final int ZERO_INDEXING_OFFSET = 1;
+    private static final double SMALL_DIFFERENCE_LIMIT = 0.0001;
 
     public static Command prepare(String commandArgs) {
         try {
@@ -22,6 +27,14 @@ public class SessionSummaryCommand extends Command {
             String invalidCommandMessage = exception.getMessage() + "\n" + COMMAND_FORMAT;
             return new InvalidCommand(invalidCommandMessage);
         }
+    }
+
+    private static boolean isDifferenceSmall(double cost1, double cost2) {
+        return Math.abs(cost1 - cost2) <= SMALL_DIFFERENCE_LIMIT;
+    }
+    
+    private static boolean isValueSmall(double cost) {
+        return Math.abs(cost) <= SMALL_DIFFERENCE_LIMIT;
     }
 
     /**
