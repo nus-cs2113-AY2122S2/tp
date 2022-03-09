13ce0d600658da3166e3fccff70b892b23621a51
khseah <seahkithan55@gmail.com>
2022-03-09 15:25:25 +0800

    Rename test class to MindMyMoney

---
+++ src/test/java/seedu/mindmymoney/MindMyMoneyTest.java
@@ -0,0 +1,12 @@
+package seedu.mindmymoney;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class MindMyMoneyTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
