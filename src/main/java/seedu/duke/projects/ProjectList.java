341d33e7e5e36e14337b74f707ad0a7e8799cc73
johnmcmonigle <60244374+johnmcmonigle@users.noreply.github.com>
2022-03-09 16:47:45 +0800

    Add addProject and deleteProject methods

--- src/main/java/seedu/duke/projects/ProjectList.java
+++ src/main/java/seedu/duke/projects/ProjectList.java
@@ -14,5 +14,21 @@ public class ProjectList {
         projectList = new ArrayList<Project>();
     }
 
-    
+    /**
+     * Adds a new Project to projectList
+     *
+     * @param title Name of the Project
+     */
+    public void addProject(String title) {
+        projectList.add(new Project(title));
+    }
+
+    /**
+     * Deletes a Project from projectList
+     *
+     * @param title Name of the project
+     */
+    public void deleteProject(String title) {
+        projectList.remove(title);
+    }
 }
