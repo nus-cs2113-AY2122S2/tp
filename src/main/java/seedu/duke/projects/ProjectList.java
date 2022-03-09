d84a73a513fdb22e4102aac4219177463a4dcfbc
johnmcmonigle <60244374+johnmcmonigle@users.noreply.github.com>
2022-03-09 16:43:27 +0800

    Add new ProjectList.java with constructor

---
+++ src/main/java/seedu/duke/projects/ProjectList.java
@@ -0,0 +1,16 @@
+package seedu.duke.projects;
+
+import java.util.ArrayList;
+
+public class ProjectList {
+    ArrayList<Project> projectList;
+
+    /**
+     * Constructs a ProjectList object
+     *
+     * @return a ProjectList object
+     */
+    public ProjectList() {
+        projectList = new ArrayList<Project>();
+    }
+}
