c8720d99ce4e746ad2a0ffc0d013b9f12227b320
johnmcmonigle <60244374+johnmcmonigle@users.noreply.github.com>
2022-03-09 16:59:56 +0800

    Give commandHandler ability to add and delete projects

--- src/main/java/seedu/duke/command_handler/CommandHandler.java
+++ src/main/java/seedu/duke/command_handler/CommandHandler.java
@@ -1,2 +1,38 @@
-package seedu.duke.command_handler;public class CommandHandler {
+package seedu.duke.command_handler;
+
+import seedu.duke.projects.Project;
+import seedu.duke.projects.ProjectList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CommandHandler {
+
+    Scanner sc;
+
+    public String line;
+
+    public String[] commands;
+
+    /**
+     *
+     */
+    public void handleUserInput(ProjectList projectList) {
+       while (true) {
+           Scanner in = new Scanner(System.in);
+           line = sc.nextLine();
+           commands = line.split(" ");
+
+           switch (commands[0].toLowerCase()) {
+           case "addproject": //end the program
+               projectList.addProject(commands[1]);
+               break;
+           case "deleteproject": //list out all tasks
+               projectList.deleteProject(commands[1]);
+               break;
+           case "exit":
+               System.exit(0);
+
+       }
+    }
 }
