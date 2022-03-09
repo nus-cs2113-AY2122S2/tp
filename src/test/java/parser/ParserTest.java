ed79dc8155bd666c0d30443aae31bc669bd462a1
shunyao643 <63784032+shunyao643@users.noreply.github.com>
2022-03-10 02:02:11 +0800

    Revise basic structure for Parser

--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -1,4 +1,32 @@
+package parser;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import parser.Parser;
+
 class ParserTest {
-  
+
+    Parser parser;
+
+    @BeforeEach
+    void setup(){
+        parser = new Parser();
+    }
+
+    @Test
+    void parseCommand_validCommand_splitArrayList() {
+        ArrayList<String> desiredResult = new ArrayList<>(
+                Arrays.asList("add", "n/ITEM_NAME sn/SERIAL_NUMBER t/TYPE c/COST pf/PURCHASED_FROM pd/PURCHASED_DATE")
+        );
+        ArrayList<String> actualResult = parser.parseCommand("add n/ITEM_NAME sn/SERIAL_NUMBER t/TYPE c/COST pf/PURCHASED_FROM pd/PURCHASED_DATE");
+        assertEquals(desiredResult, actualResult);
+        assertEquals(desiredResult.get(0), actualResult.get(0));
+        assertEquals(desiredResult.get(1), actualResult.get(1));
+    }
 }
