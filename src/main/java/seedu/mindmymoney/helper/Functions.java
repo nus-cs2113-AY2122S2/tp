43693631d2910d3b489b9029d9f2ba2cf3769928
Sean Ho <69450377+SeanHoWB@users.noreply.github.com>
2022-03-09 20:06:21 +0800

    Update Functions.java

--- src/main/java/seedu/mindmymoney/helper/Functions.java
+++ src/main/java/seedu/mindmymoney/helper/Functions.java
@@ -2,13 +2,12 @@ package seedu.mindmymoney.helper;
 
 import seedu.mindmymoney.constants.Indexes;
 
-/**
- * Separates the user input into the command and description for easy reference.
- *
- * @return String array of user input.
- */
 public class Functions {
-
+    /**
+     * Separates the user input into the command and description for easy reference.
+     *
+     * @return String array of user input.
+     */
     public static String[] parseInput(String inputCommand) {
         String[] inputAsArray = inputCommand.split(" ", Indexes.SPLIT_LIMIT);
         return inputAsArray;
