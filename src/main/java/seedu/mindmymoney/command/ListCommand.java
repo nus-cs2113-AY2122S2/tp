747137356b8693f57b0f7cee9bf04eff81eafe5f
GlendonNotGlen <glendonng38@gmail.com>
2022-03-10 02:11:05 +0800

    Add list command

---
+++ src/main/java/seedu/mindmymoney/command/ListCommand.java
@@ -0,0 +1,30 @@
+package seedu.mindmymoney.command;
+
+import seedu.mindmymoney.constants.PrintStrings;
+import seedu.mindmymoney.data.Lists;
+import seedu.mindmymoney.userfinancial.Expenditure;
+
+public class ListCommand extends Command {
+    public ListCommand() {
+    };
+
+    /**
+     * Prints user's current list of expenditures
+     */
+    @Override
+    public void executeCommand() {
+        if (Lists.expenditures.size() == 0) {
+            System.out.println(PrintStrings.LINE +
+                    "Your list is currently empty! Try adding expenditures first using the add command"
+                    + System.lineSeparator() + PrintStrings.LINE);
+        } else {
+            System.out.println(PrintStrings.LINE);
+            Integer indexOfList = 1;
+            for (Expenditure i : Lists.expenditures) {
+                System.out.println(indexOfList + ". $" + i.getAmount() + " on " + i.getDescription());
+                indexOfList++;
+            }
+            System.out.println(PrintStrings.LINE);
+        }
+    }
+}
