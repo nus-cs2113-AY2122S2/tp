558adf1ee8d723ac6c3db8864b8406b78ac3372c
johnmcmonigle <60244374+johnmcmonigle@users.noreply.github.com>
2022-03-09 17:29:19 +0800

    Remove logo

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -16,12 +16,8 @@ public class Duke {
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Welcome to CSProjPlanner\n");
 
         while (true) {
             commandHandler.handleUserInput(projectList);
