0fa24ebae275c1365979179f99cc5a3119993e2a
warrencxw <cxw.warren@gmail.com>
2022-03-09 15:56:00 +0800

    Add member attributes, getters and setters to PersonCostPair class

--- src/main/java/seedu/splitlah/util/PersonCostPair.java
+++ src/main/java/seedu/splitlah/util/PersonCostPair.java
@@ -9,4 +9,23 @@ import java.util.Comparator;
  * A negative value represents a debt to pay another person with a positive value.
  */
 public class PersonCostPair {
+    private Person person;
+    private double cost;
+    private boolean isProcessed;
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public boolean isProcessed() {
+        return isProcessed;
+    }
 }
