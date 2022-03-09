589dfa2b3e630a42332db0664047882bb3108c47
Zhou Qi <>
2022-03-09 11:07:41 +0800

    Add Appointment.java and Ui.java

---
+++ src/main/java/tp/Ui.java
@@ -0,0 +1,36 @@
+package tp;
+
+import java.util.Scanner;
+
+public class Ui {
+    public static String boundary = "____________________________________________________________" + System.lineSeparator();
+
+    // Prints welcome message.
+    public void sayHello() {
+        System.out.print(boundary);
+        System.out.println("Hello! This is IHospital.");
+        System.out.print("What can I do for you?" + System.lineSeparator() + boundary);
+    }
+
+    // Prints goodbye message.
+    public void sayGoodbye() {
+        System.out.print(boundary + "Bye. Hope to see you again soon!" + System.lineSeparator() + boundary);
+    }
+
+    // Interacts with user and edit the given task list accordingly.
+    public void interact() {
+
+    }
+
+    public void printDoctorPage(DoctorList doctors) {
+        System.out.print(boundary);
+        System.out.println("Welcome to Doctor Page.");
+        doctors.printDoctorList();
+    }
+
+    public void printPatientPage(PatientList patients) {
+        System.out.print(boundary);
+        System.out.println("Welcome to Patient Page.");
+        patients.printPatientList();
+    }
+}
