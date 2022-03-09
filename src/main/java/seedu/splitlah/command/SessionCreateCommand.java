f51245304023b1bdb283ff472b1703aadc17f926
Roy Tang <roy_tql@hotmail.com>
2022-03-09 15:41:58 +0800

    Remove unused imports

--- src/main/java/seedu/splitlah/command/SessionCreateCommand.java
+++ src/main/java/seedu/splitlah/command/SessionCreateCommand.java
@@ -10,8 +10,6 @@ import seedu.splitlah.ui.Message;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Locale;
 import java.util.Set;
 
 /**
