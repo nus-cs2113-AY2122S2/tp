e0bdde52a5137f087495546e952bca3f114273f9
Kr0sfyre <teanweijun@gmail.com>
2022-03-09 14:40:57 +0800

    Create person constructor

--- src/main/java/person/Person.java
+++ src/main/java/person/Person.java
@@ -4,4 +4,10 @@ public class Person {
     private static String name;
     private static ArrayList<Income> incomeList;
     private static ArrayList<Expenditure> expenditureList;
+
+    public Person(String name) {
+        this.name = name;
+        incomeList = new IncomeList();
+        expenditureList = new ExpenditureList();
+    }
 }
