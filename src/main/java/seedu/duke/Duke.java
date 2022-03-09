505bff750c1e25c754b1ccb7bae77f21beda0b47
DESKTOP-OJ3Q66Q\angyo <angyongming@gmail.com>
2022-03-09 12:47:37 +0800

    Comply with coding standards

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -21,7 +21,7 @@ public class Duke {
         System.out.println("Hello " + SCANNER.nextLine());
 
         //check for bye message
-        while(true) {
+        while (true) {
             String input = getInput();
             Parser parser = new Parser(input);
 
--- src/main/java/seedu/duke/Timetable.java
+++ src/main/java/seedu/duke/Timetable.java
@@ -6,7 +6,7 @@ public class Timetable {
 
     public static final ArrayList<Event> list = new ArrayList<>();
 
-    public static void add (Event event) {
+    public static void add(Event event) {
         list.add(event);
     }
 
