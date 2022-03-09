672137319b3ccc44f5ac035341d006f7503f454d
Kr0sfyre <teanweijun@gmail.com>
2022-03-09 14:40:34 +0800

    Create person class with simple members

---
+++ src/main/java/person/Person.java
@@ -0,0 +1,7 @@
+package person;
+
+public class Person {
+    private static String name;
+    private static ArrayList<Income> incomeList;
+    private static ArrayList<Expenditure> expenditureList;
+}
