b6405ab139d93647e8072fefb61ed1ff4f78ae2c
Kr0sfyre <teanweijun@gmail.com>
2022-03-09 15:47:43 +0800

    Create constructor for PersonList

--- src/main/java/person/PersonList.java
+++ src/main/java/person/PersonList.java
@@ -1,2 +1,13 @@
-package person;public class PersonList {
+package person;
+
+import java.util.ArrayList;
+
+public class PersonList {
+    private static ArrayList<Person> personList;
+    private static int numberOfMembers;
+
+    public PersonList() {
+        personList = new ArrayList<Person>();
+        numberOfMembers = 0;
+    }
 }
