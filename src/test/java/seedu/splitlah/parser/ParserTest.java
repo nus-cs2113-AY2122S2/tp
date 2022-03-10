20c2d02aeee29c1d130e1832f3a343681c83f529
warrencxw <cxw.warren@gmail.com>
2022-03-10 21:27:56 +0800

    Add ParserTest class for JUnit tests of Parser class

---
+++ src/test/java/seedu/splitlah/parser/ParserTest.java
@@ -0,0 +1,12 @@
+package seedu.splitlah.parser;
+
+import org.junit.jupiter.api.Test;
+import seedu.splitlah.command.Command;
+import seedu.splitlah.command.InvalidCommand;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class ParserTest {
+
+}
