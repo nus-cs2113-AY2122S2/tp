9ff69f8a936bc43135228200f8263311cd3ab31f
Roy Tang <roy_tql@hotmail.com>
2022-03-10 14:54:08 +0800

    Add setup function for SessionCreateCommand Junit file

--- src/test/java/seedu/splitlah/command/SessionCreateCommandTest.java
+++ src/test/java/seedu/splitlah/command/SessionCreateCommandTest.java
@@ -1,4 +1,14 @@
 import static org.junit.jupiter.api.Assertions.*;
 class SessionCreateCommandTest {
   
+    Manager manager = new Manager();
+    @BeforeEach
+    void setUp() {
+        String sessionOneArgs = "session /create /n Class outing /d 2022-02-15 /pl Alice Bob";
+        String sessionTwoArgs = "session /create /n Family gathering /d 2022-02-20  /pl Eves Mallory";
+        Command createSessionOne = SessionCreateCommand.prepare(sessionOneArgs);
+        createSessionOne.run(manager);
+        Command createSessionTwo = SessionCreateCommand.prepare(sessionTwoArgs);
+        createSessionTwo.run(manager);
+    }
 }
