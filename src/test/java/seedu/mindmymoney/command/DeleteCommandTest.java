c2bdef6bd604985d656d6db6f440d05051060c42
limjierui <jieruilim@u.nus.edu>
2022-03-10 23:12:32 +0800

    Fix coding standard 3 for DeleteCommandTest

--- src/test/java/seedu/mindmymoney/command/DeleteCommandTest.java
+++ src/test/java/seedu/mindmymoney/command/DeleteCommandTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
- * Represents the tests for delete commands.
+ * Represents the tests for delete command.
  */
 class DeleteCommandTest {
     /**
