efff9cb713a2d64d7067226c61c5935a4d826d17
shunyao643 <63784032+shunyao643@users.noreply.github.com>
2022-03-10 16:52:38 +0800

    Define pattern for Add Command

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -1,6 +1,7 @@
 package parser;
 
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,24 +17,66 @@ public class Parser {
      */
     public static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)\\s+(?<arguments>.*)");
 
+    public static final Pattern ADD_COMMAND_FORMAT = Pattern.compile(
+            "n/(?<itemName>.+)" + "\\s+" +
+                    "sn/(?<serialNumber>.+)" + "\\s+" +
+                    "t/(?<equipmentType>.+)" + "\\s+" +
+                    "c/\\$?(?<cost>[\\d,\\.]+)" + "\\s+" + // TODO: maybe have error checking formula separately?
+                    "pf/(?<purchasedFrom>.+)" + "\\s+" +
+                    "pd/(?<purchasedDate>.+)"
+    );
+
+    public static final String MESSAGE_INCOMPLETECOMMAND_MISSING_DELIMITER =
+            "Please split your command into arguments with each argument seperated by spaces!";
+
+    public void parseCommand(String userInput) {
+        // TODO: replace void with Command
+        ArrayList<String> commandAndArgument;
+        try {
+            commandAndArgument = splitCommandTerm(userInput);
+        } catch (IncompleteCommandException e) {
+            System.out.println(MESSAGE_INCOMPLETECOMMAND_MISSING_DELIMITER);
+            return;
+        }
+
+        switch (commandAndArgument.get(0)) {
+
+        case "add":
+            // TODO: replace "add" with constant as defined in AddCommand
+            prepareAdd(commandAndArgument.get(1));
+
+        }
+
+    }
+
+    private void prepareAdd(String args) {
+        final Matcher matcher = ADD_COMMAND_FORMAT.matcher(args.trim());
+        // validate arg string format
+        if (!matcher.matches()) {
+            
+            return;
+        }
+    }
 
     /**
      * Break down a command into the command term to be parsed and the remainder of the arguments.
      * Assumes command term and remainder arguments are delimited by minimally one space.
+     *
      * @param userInput
      * @return ArrayList of String, first element being the command term and the second element being arguments
+     * @throws IncompleteCommandException if no space is found
      */
-    public ArrayList<String> parseCommand(String userInput) {
+    public ArrayList<String> splitCommandTerm(String userInput) throws IncompleteCommandException {
         ArrayList<String> resultArrayList = new ArrayList<>();
         final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(userInput.trim());
         // guard against no match
         if (!matcher.matches()) {
-            System.out.println("Invalid input"); // TODO: print correct command
-            return resultArrayList;
+            throw new IncompleteCommandException("Could not find space delimiter between command and arguments!");
         }
         resultArrayList.add(matcher.group("commandWord"));
         resultArrayList.add(matcher.group("arguments"));
         return resultArrayList;
     }
+
 
 }
--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -8,8 +8,6 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import parser.Parser;
-
 class ParserTest {
 
     Parser parser;
@@ -20,19 +18,30 @@ class ParserTest {
     }
 
     @Test
-    void parseCommand_validCommand_success() {
-        ArrayList<String> desiredResult = new ArrayList<>(
+    void splitCommandTerm_validCommand_success() throws IncompleteCommandException {
+        ArrayList<String> expectedResult = new ArrayList<>(
                 Arrays.asList("add", "n/ITEM_NAME sn/SERIAL_NUMBER t/TYPE c/COST pf/PURCHASED_FROM pd/PURCHASED_DATE")
         );
-        ArrayList<String> actualResult = parser.parseCommand("add n/ITEM_NAME sn/SERIAL_NUMBER t/TYPE c/COST pf/PURCHASED_FROM pd/PURCHASED_DATE");
-        assertEquals(desiredResult, actualResult);
-        assertEquals(desiredResult.get(0), actualResult.get(0));
-        assertEquals(desiredResult.get(1), actualResult.get(1));
+        ArrayList<String> actualResult = parser.splitCommandTerm(
+                "add n/ITEM_NAME sn/SERIAL_NUMBER t/TYPE c/COST pf/PURCHASED_FROM pd/PURCHASED_DATE");
+        assertEquals(expectedResult, actualResult);
+        assertEquals(expectedResult.get(0), actualResult.get(0));
+        assertEquals(expectedResult.get(1), actualResult.get(1));
     }
 
     @Test
-    void parseCommand_incompleteCommand_exceptionThrown(){
-
+    void splitCommandTerm_incompleteCommand_exceptionThrown(){
+        ArrayList<String> unexpectedResult = new ArrayList<>(
+                Arrays.asList("add","n/ITEM_NAMEsn/SERIAL_NUMBERt/TYPEc/COSTpf/PURCHASED_FROMpd/PURCHASED_DATE")
+        );
+        try {
+            ArrayList<String> actualResult = parser.splitCommandTerm(
+                    "addn/ITEM_NAMEsn/SERIAL_NUMBERt/TYPEc/COSTpf/PURCHASED_FROMpd/PURCHASED_DATE");
+            assertEquals(unexpectedResult, actualResult);
+            fail();
+        } catch (IncompleteCommandException e){
+            assertEquals("Could not find space delimiter between command and arguments!", e.getMessage());
+        }
     }
 
 }
