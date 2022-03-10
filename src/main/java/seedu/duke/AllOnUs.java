b29adfb6cf4fb02ddd9062c1885015082d2524f3
OzairHasan <ozairhasan98@gmail.com>
2022-03-10 22:35:04 +0800

    Rename Duke.java and DukeTest.java to AllOnUs.java and AllOnUsTest.java
    Update menu functionality to support help, exit commands and in-app navigation from menu

---
+++ src/main/java/seedu/duke/AllOnUs.java
@@ -0,0 +1,103 @@
+package seedu.duke;
+
+import seedu.duke.ui.TextUi;
+
+import static seedu.duke.contacts.ContactsManager.contactsRunner;
+
+public class AllOnUs {
+    private TextUi ui;
+
+    private static void greet() {
+        System.out.println("greeting message");
+    }
+
+    private void exit() {
+        System.out.println("Goodbye! Hope to see you again...");
+        System.exit(0);
+    }
+
+    public static boolean isContactsManagerCommand(String userInput) {
+        return userInput.equals("goto m/Contacts_Manager");
+    }
+
+    public static boolean isStudyManagerCommand(String userInput) {
+        return userInput.equals("goto m/Study_Manager");
+    }
+
+    public static boolean isExpenseTrackerCommand(String userInput) {
+        return userInput.equals("goto m/Expense_Tracker");
+    }
+
+    public static boolean isExitCommand(String userInput) {
+        return userInput.equals("exit");
+    }
+
+    public static boolean isNotEmpty(String userInput) {
+        return userInput.length() > 0;
+    }
+
+    public static boolean isHelpCommand(String userInput) {
+        return userInput.equals("help");
+    }
+
+    public static void displayHelp() {
+        displayMainMenuFeatures();
+    }
+
+    private static void displayMainMenuFeatures() {
+        System.out.println("Going from menu to section of interest:\n\nAllows " +
+                "access to subsections of the application, namely, to the expense tracker, " +
+                "contacts manager and task manager.\n\nFormat: goto m/SECTION\n\nExamples:" +
+                "\n\n- goto m/Expense_Tracker\n- goto m/Study_Manager\n- goto m/Contacts_Manager" +
+                "\n\n\nGoing back to menu:\n\nAllows navigation back to the menu section of the application. " +
+                "\n\nFormat: menu\n\nExample:\n\n- menu" +
+                "\n\n\nGetting guidance on the usage of the application:\n\nDisplays user guide." +
+                "\n\nFormat: help\n\nExample:\n\n- help" +
+                "\n\n\nExiting the application:\n\nTerminates the application.\n\nFormat: exit" +
+                "\n\nExample:\n\n- exit");
+    }
+
+    public void run() {
+        this.ui = new TextUi();
+        String userInput;
+
+        while (true) {
+            userInput = ui.getUserInput();
+            if (isExitCommand(userInput)) {
+                exit();
+            } else if (isHelpCommand(userInput)) {
+                displayHelp();
+            } else if (isContactsManagerCommand(userInput)) {
+                contactsRunner(ui);
+                printMainMenuMessage();
+            } else if (isStudyManagerCommand(userInput)) {
+                //studyManagerRunner(ui);
+                printMainMenuMessage();
+            } else if (isExpenseTrackerCommand(userInput)) {
+                //expenseTrackerRunner(ui);
+                printMainMenuMessage();
+            } else if (isNotEmpty(userInput)) {
+                printInvalidMainMenuCommandMessage();
+            } else {
+                continue;
+            }
+        }
+    }
+
+    private void printInvalidMainMenuCommandMessage() {
+        System.out.println("Oops I am not sure what you mean by this command. Please retry. '" +
+                "Enter \"help\" for guidance.");
+    }
+
+    private void printMainMenuMessage() {
+        System.out.println("Welcome back to the main menu");
+    }
+
+    /**
+     * Main entry-point for the java.duke.Duke application.
+     */
+    public static void main(String[] args) {
+        greet();
+        new AllOnUs().run();
+    }
+}
--- src/test/java/seedu/duke/AllOnUsTest.java
+++ src/test/java/seedu/duke/AllOnUsTest.java
@@ -1,10 +1,12 @@
 package seedu.duke;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
-class DukeTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class AllOnUsTest {
     @Test
     public void sampleTest() {
         assertTrue(true);
