8c7ce9b72c416fa6d64dd4454f9a89c8107e9e50
unknown <brendanawjang@gmail.com>
2022-03-10 23:18:49 +0800

    Add OOP abstractions

---
+++ src/main/java/seedu/duke/command/DeleteCommand.java
@@ -0,0 +1,22 @@
+package seedu.duke.command;
+
+import seedu.duke.Packages;
+
+public class DeleteCommand extends Command {
+    private int id;
+
+    public DeleteCommand(int id) {
+        super(false);
+        this.id = id;
+    }
+
+    public void execute(Packages packages) {
+        for (int i = 0; i < packages.getSize(); i++) {
+            if (packages.getPackage(i).getID() == id) {
+                packages.removePackage(i);
+                break;
+            }
+        }
+
+    }
+}
