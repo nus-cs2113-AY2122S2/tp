440c815c3a4b367b2f37153561b9ed5677921a97
BotBw <wang1570@e.ntu.edu.sg>
2022-03-10 19:09:41 +0800

    Add JUnit test and javadoc

--- src/main/java/seedu/duke/manager/DishManager.java
+++ src/main/java/seedu/duke/manager/DishManager.java
@@ -1,49 +1,88 @@
 package seedu.duke.manager;
 
-import seedu.duke.Dish;
+import seedu.duke.entities.Dish;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * DishManager
+ */
 public class DishManager {
-    private List<Dish> dishes;
+    private final List<Dish> dishes;
 
     public DishManager() {
         dishes = new ArrayList<>();
     }
 
+    /**
+     * Print all the dishes/menu.
+     */
     public void printDishes() {
         for (int i = 0; i < dishes.size(); i++) {
             System.out.println((i + 1) + ". " + dishes.get(i));
         }
     }
 
+    /**
+     * @return the number of current dishes
+     */
+    public int getNumOfDishes() {
+        return dishes.size();
+    }
+
+    /**
+     * Add one dish to menu.
+     *
+     * @param dish the dish to add
+     */
     public void addDish(Dish dish) {
         dishes.add(dish);
     }
 
-    public boolean tryDeleteDish(int menuIndex) {
+    /**
+     * Try to delete dish in menu
+     *
+     * @param menuIndex the index of dish to delete in menu
+     * @throws IndexOutOfBoundsException if menuIndex is not valid
+     */
+    public void deleteDish(int menuIndex) throws IndexOutOfBoundsException {
         int listIndex = menuIndex - 1;
-        if (!isListIndexValid(listIndex)) {
-            return false;
-        }
+        throwIfIndexNotValid(listIndex);
         dishes.remove(listIndex);
-        return true;
     }
 
-    public boolean trySetPrice(int menuIndex, double newPrice) {
+    /**
+     * Try to set the price of one dish.
+     *
+     * @param menuIndex the index of dish to set price in menu
+     * @param newPrice  the new price of the dish
+     * @throws IndexOutOfBoundsException if menuIndex is not valid
+     * @throws IllegalArgumentException  if new price is negative
+     */
+    public void setPrice(int menuIndex, double newPrice) throws IndexOutOfBoundsException, IllegalArgumentException {
         int listIndex = menuIndex - 1;
-        if (!isListIndexValid(listIndex)) {
-            return false;
-        }
+        throwIfIndexNotValid(listIndex);
         dishes.get(listIndex).setPrice(newPrice);
-        return true;
     }
 
-    private boolean isListIndexValid(int listIndex) {
+    /**
+     * Try to set the name of one dish.
+     *
+     * @param menuIndex the index of dish to set price in menu
+     * @param newName   the new name of the dish
+     * @throws IndexOutOfBoundsException if menuIndex is not valid
+     * @throws IllegalArgumentException  if newName is null
+     */
+    public void setName(int menuIndex, String newName) throws IndexOutOfBoundsException, IllegalArgumentException {
+        int listIndex = menuIndex - 1;
+        throwIfIndexNotValid(listIndex);
+        dishes.get(listIndex).setName(newName);
+    }
+
+    private void throwIfIndexNotValid(int listIndex) throws IndexOutOfBoundsException {
         if (listIndex < 0 || listIndex >= dishes.size()) {
-            return false;
+            throw new IndexOutOfBoundsException();
         }
-        return true;
     }
 }
