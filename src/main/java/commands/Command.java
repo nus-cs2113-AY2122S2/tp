3b7c0b9587b85f51df1eca6c7b2e1c58f13b9da1
hlwang56 <e0550418@u.nus.edu>
2022-03-10 17:21:46 +0800

    fix conflict

--- src/main/java/commands/Command.java
+++ src/main/java/commands/Command.java
@@ -1,6 +1,7 @@
 package commands;
 
 import exceptions.UnknownException;
+import seedu.planitarium.parser.Parser;
 import ui.UI;
 
 public class Command {
@@ -15,6 +16,7 @@ public class Command {
     protected static final String EXIT = "bye";
     protected String userInput;
     protected String replyMsg;
+    protected Parser parser = new Parser();
     protected UI ui = new UI();
 
     public Command(String userInput) {
@@ -23,31 +25,53 @@ public class Command {
 
     /**
      * Executes the instruction according to the input after parsing
+     * @param userInput the instruction after parsing by parser
      * @throws UnknownException if the instruction cannot be executed
     */
-    /**
+
     public void execute() throws UnknownException {
-        //parser
-        try () {
-            switch () {
+        String name;
+        String uid;
+        String amount;
+        try {
+            switch (parser.parseKeyword(userInput)) {
             case ADD_PERSON_CMD:
-                //this.replyMsg =
+                name  = parser.parseName(userInput);
+                personList.addPerson(name);
                 break;
             case DELETE_PERSON_CMD:
+                uid = parser.parseUserIndex(userInput);
+                personList.deletePerson(uid);
                 break;
             case ADD_INCOME_CMD:
+                amount = parser.parseIncome(userInput);
+                uid = parser.parseUserIndex(userInput);
+                expenditureList.addIncome(amount, uid);
                 break;
             case DELETE_INCOME_CMD:
+                amount = parser.parseIncome(userInput);
+                uid = parser.parseUserIndex(userInput);
+                expenditureList.deleteIncome(amount, uid);
                 break;
             case ADD_SPENT_CMD:
+                amount = parser.parseExpenditure((userInput));
+                uid = parser.parseUserIndex(userInput);
+                expenditureList.addExpenditure(amount, uid);
                 break;
             case DELETE_SPEND_CMD:
+                amount = parser.parseExpenditure((userInput));
+                uid = parser.parseUserIndex(userInput);
+                expenditureList.addExpenditure(amount, uid);
                 break;
             case CALC_REMAIN:
+                personList.remain();
                 break;
             case LIST:
+                personList.list();
                 break;
             case EXIT:
+                ui.exit();
+                System.exit(0);
                 break;
             default:
                 throw new UnknownException();
@@ -57,5 +81,5 @@ public class Command {
         }
         ui.printMsg(replyMsg);
     }
-    */
+
 }
--- src/main/java/seedu/planitarium/PlanITarium.java
+++ src/main/java/seedu/planitarium/PlanITarium.java
@@ -1,40 +1,27 @@
 package seedu.planitarium;
 
-import java.util.Scanner;
-
 import commands.Command;
+
+import java.util.Scanner;
 
 public class PlanITarium {
     protected Scanner userInput;
     protected Command commandExecuter;
 
-    public void run() {
-        printWelcomeMessage();
+    public void run() throws Exception{
+        ui.printWelcomeMessage();
         while (true) {
             userInput = new Scanner(System.in);
             commandExecuter = new Command(userInput.nextLine());
-            //commandExecuter.execute();
+            commandExecuter.execute();
         }
     }
 
     /**
      * Entry-point for the PlanITarium application.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         new PlanITarium().run();
     }
 
-    private static void printWelcomeMessage() {
-        String logo = "$$$$$$$  $$                   $$ $$$$$$$$               $$\n"
-            + "$$$$$$$  $$                   $$ $$$$$$$$\n"
-            + "$$ $$$$  $$  $$$$$$  $$$$$$$  $$    $$    $$$$$$  $$$$$ $$  $$   $$  $$$$$$$$$$\n"
-            + "$$ $$$$  $$  $$$$$$  $$$$$$$  $$    $$    $$$$$$  $$$$$ $$  $$   $$  $$$$$$$$$$\n"
-            + "$$$$$$$  $$    $$$$  $$$$$$$  $$    $$      $$$$  $$$$$ $$  $$   $$  $$$$$$$$$$\n"
-            + "$$$$$$$  $$   $$$$$  $$$ $$$  $$    $$     $$$$$  $$$   $$  $$  $$$  $$$$$$$$$$\n"
-            + "$$       $$  $$$$$$  $$$  $$  $$    $$    $$$$$$  $$$   $$  $$$ $$$  $$  $$  $$\n"
-            + "$$       $$  $$$$$$$ $$   $$  $$    $$    $$$$$$$ $$    $$  $$$$$$$  $$  $$  $$\n"
-            + "$$       $$  $$$$$$$ $$   $$  $$    $$    $$$$$$$ $$    $$  $$$$$$$  $$  $$  $$\n"
-            + "$$       $$  $$$$$$$ $$   $$  $$    $$    $$$$$$$ $$    $$  $$$$$$$  $$  $$  $$\n";
-        System.out.println(logo);
-    }
 }
