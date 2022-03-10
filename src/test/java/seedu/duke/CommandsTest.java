491771bade2fc6188f9eb542c4ba954bf4782d50
Nnythingy <ngjongrayedward8@gmail.com>
2022-03-11 00:17:06 +0800

    Create JUnit test for addGood

---
+++ src/test/java/seedu/duke/CommandsTest.java
@@ -0,0 +1,28 @@
+package seedu.duke;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommandsTest {
+    public void addGoodTest() {
+        //test 1, adding good normally - success
+        ArrayList<Goods> goodsArrayList = new ArrayList<>();
+        Commands.addGood("1", "chairs", "15", goodsArrayList);
+        assertEquals(1, goodsArrayList.size());
+
+        //test 2, blank inputs - fail, good is not added
+        Commands.addGood("1", "     ", "15", goodsArrayList);
+        assertEquals(0, goodsArrayList.size());
+
+        //test 3, empty inputs - fail, good is not added
+        Commands.addGood("1", "", "15", goodsArrayList);
+        assertEquals(0, goodsArrayList.size());
+
+        //test 4, non-numerical inputs for id - good is not added
+        Commands.addGood("hi", "chairs", "15", goodsArrayList);
+        assertEquals(0, goodsArrayList.size());
+
+    }
+
+}
