98638e4bb7b8dd5ea757e7eb586e57e5e5867b1e
Mathan <mathanmahe99@gmail.com>
2022-03-10 20:33:36 +0800

    Add JUnit test for Module class

---
+++ src/test/java/seedu/duke/modules/ModuleTest.java
@@ -0,0 +1,13 @@
+package seedu.duke.modules;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ModuleTest {
+
+    @Test
+    public void testStringConversion() {
+        assertEquals("[Module] Code='CS2113', Category='Lecture'",new Module("CS2113","Lecture").toString());
+    }
+}
