5582c28c6789a2d3b64f5c89e1ca63c4aedda724
RenTianle <lele.rer@outlook.com>
2022-03-12 00:46:40 +0800

    Revert "Integrate ActivityViewCommand with Parser class"
    This reverts commit f3c477b1342f1f89b5fa40db67ffe5cfd4421f64.

--- src/main/java/seedu/splitlah/parser/Parser.java
+++ src/main/java/seedu/splitlah/parser/Parser.java
@@ -302,7 +302,7 @@ public class Parser {
         case SessionListCommand.COMMAND_TEXT:
             return new SessionListCommand();
         case ActivityListCommand.COMMAND_TEXT:
-            return ActivityListCommand.prepare(remainingArgs);
+            // FALLTHROUGH
         case ActivityViewCommand.COMMAND_TEXT:
             // return Relevant command.prepare(remainingArgs);
             return new InvalidCommand("Command is currently not implemented, please try again later.");
