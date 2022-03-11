c64fc091e15b0a62bf8c635a846594c0f6eeb770
Daryl <aka.hjd@gmail.com>
2022-03-12 00:30:04 +0800

    task: test controller choices and first option

---
+++ src/test/java/seedu/duke/controllers/ControllerTest.java
@@ -0,0 +1,25 @@
+package seedu.duke.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+    @Test
+    public void testHaveExitOption() {
+        Scanner scanner = new Scanner(System.in);
+        Controller[] controllers = {
+                new MainController(scanner),
+                new DishController(scanner),
+        };
+
+        for (Controller c: controllers) {
+            // Each controller should have at least one choice.
+            assertTrue(c.choices.length >= 1);
+            // The first choice should always be to exit.
+            assertTrue(c.choices[0].toLowerCase().contains("exit"));
+        }
+    }
+}
