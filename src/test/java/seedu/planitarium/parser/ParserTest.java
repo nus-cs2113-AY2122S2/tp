67e95170db000afe0f1a798000eec5468ece85fb
1szheng <wansizheng@gmail.com>
2022-03-11 21:20:32 +0800

    Add parser JUnit test skeleton Actual JUnit tests for Parser methods to be implemented at a later date

---
+++ src/test/java/seedu/planitarium/parser/ParserTest.java
@@ -0,0 +1,33 @@
+package seedu.planitarium.parser;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class ParserTest {
+
+    @Test
+    void parseDelimitedTerm() {
+    }
+
+    @Test
+    void parseKeyword() {
+    }
+
+    @Test
+    void parseName() {
+    }
+
+    @Test
+    void parseUserIndex() {
+    }
+
+    @Test
+    void parseIncome() {
+    }
+
+    @Test
+    void parseExpenditure() {
+    }
+}
