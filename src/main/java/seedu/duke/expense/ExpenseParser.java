e780d5d0bab20385a3af868758c228243be99396
YitHien <e0028345@u.nus.edu>
2022-03-11 11:20:09 +0800

    Minor changes to Text-UI-Test files

--- src/main/java/seedu/duke/expense/ExpenseParser.java
+++ src/main/java/seedu/duke/expense/ExpenseParser.java
@@ -5,36 +5,26 @@ public class ExpenseParser {
     public static final String AMOUNT_DELIMITER = "/a";
     public static final String CATEGORY_DELIMITER = "/c";
     public static final String REMARKS_DELIMITER = "/r";
-    public static final String END_DELIMITER = "/[dacr]";
+    public static final String END = "/r";
 
 
     // /d date /a amount /c category /r remarks
 
     public static String[] parseNewExpense(String userInput) {
-        String rawInput = userInput.split(" ", 2)[1].trim();
-        String date = parseExpenseDelimiters(END_DELIMITER, DATE_DELIMITER, rawInput);
-        String amount = parseExpenseDelimiters(END_DELIMITER, AMOUNT_DELIMITER, rawInput);
-        String category = parseExpenseDelimiters(END_DELIMITER, CATEGORY_DELIMITER, rawInput);
-        String remarks = parseExpenseDelimiters(END_DELIMITER, REMARKS_DELIMITER, rawInput);
+        String[] rawInput = userInput.split(" ", 2);
+        //String date = parseExpenseDate(DATE_DELIMITER, rawInput);
+        //String amount = parseExpenseAmount(AMOUNT_DELIMITER, rawInput);
+        //String category = parseExpenseCategory(CATEGORY_DELIMITER, rawInput);
+        //String remarks = parseExpenseRemarks(REMARKS_DELIMITER, rawInput);
+        String[] parameters = rawInput[1].split(" ", 4);
+        String date = parameters[0].substring(2);
+        String amount = parameters[1].substring(2);
+        String category = parameters[2].substring(2);
+        String remarks = parameters[3].substring(2);
 
         String[] result = {date, amount, category, remarks};
         return result;
     }
-
-    private static String parseExpenseDelimiters(String end, String delimiter, String rawInput) {
-        String[] stripLeftAway =  rawInput.split(delimiter, 2);
-        String rightInput;
-        if (stripLeftAway.length == 2) {
-            rightInput = stripLeftAway[1];
-        } else {
-            rightInput = stripLeftAway[0];
-        }
-        String[] stripRightAway = rightInput.split(end, 2);
-        String result = stripRightAway[0].trim();
-        return result;
-
-    }
-
 
 
     public static int parseDeleteExpense(String userInput) {
