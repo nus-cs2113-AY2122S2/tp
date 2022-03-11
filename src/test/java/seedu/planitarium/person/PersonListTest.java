a5de76957f626c5d9aa951d8f9d049ef3308bb67
Kr0sfyre <teanweijun@gmail.com>
2022-03-11 19:03:50 +0800

    Create method to test setup of new PersonList object

--- src/test/java/seedu/planitarium/person/PersonListTest.java
+++ src/test/java/seedu/planitarium/person/PersonListTest.java
@@ -1,4 +1,13 @@
 package seedu.planitarium.person;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class PersonListTest {
+    @Test
+    public void getNumberOfMembers_newPersonList_returnZero() {
+        PersonList list = new PersonList();
+        assertEquals(0, list.getNumberOfMembers());
+    }
 }
