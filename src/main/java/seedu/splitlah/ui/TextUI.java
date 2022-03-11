df553186691f298e1023f04af3b806f35fccc9dd
matheril <sauravvm237@gmail.com>
2022-03-11 18:45:48 +0800

    Add > prompt when expecting input
    There was no visual indicator to tell the user when the program was
    expecting input.
    
    Let's fix this by printing > to the terminal before reading in input
    from the user.

--- src/main/java/seedu/splitlah/ui/TextUI.java
+++ src/main/java/seedu/splitlah/ui/TextUI.java
@@ -34,6 +34,7 @@ public class TextUI {
     }
 
     public String readNextLine() {
+        printlnMessage(Message.PROMPT_TEXTUI_AWAITING_INPUT);
         return in.nextLine();
     }
 
@@ -63,8 +64,8 @@ public class TextUI {
                 printlnMessage(Message.ERROR_TEXTUI_USER_DID_NOT_CONFIRM);
                 return false;
             default:
-                confirmation = readNextLine().toLowerCase();
                 printlnMessage(Message.ERROR_TEXTUI_REENTER_INPUT);
+                confirmation = readNextLine().toLowerCase();
             }
         }
     }
