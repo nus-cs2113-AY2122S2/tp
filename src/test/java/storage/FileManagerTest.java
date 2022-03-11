c9b51315585e58089ba27815efded4a2574c65a3
alanlowzies <30099983+alanlowzies@users.noreply.github.com>
2022-03-11 08:25:10 +0800

    Create JUnit test for FileManager.java
    Create test for convertWorkoutToFileDataFormat method.

---
+++ src/test/java/storage/FileManagerTest.java
@@ -0,0 +1,27 @@
+package storage;
+
+import data.workouts.Workout;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FileManagerTest {
+    @Test
+    public void convertWorkoutToFileDataFormat_normalWorkoutInput_conversionSuccess() throws IOException {
+        Workout testSample1 = new Workout("push up", 1);
+        Workout testSample2 = new Workout("russian twist", 1000);
+        Workout testSample3 = new Workout("swimming", 20);
+
+        String expectedOutput1 = "push up | 1";
+        String expectedOutput2 = "russian twist | 1000";
+        String expectedOutput3 = "swimming | 20";
+
+        FileManager fm = new FileManager();
+
+        assertEquals(expectedOutput1, fm.convertWorkoutToFileDataFormat(testSample1));
+        assertEquals(expectedOutput2, fm.convertWorkoutToFileDataFormat(testSample2));
+        assertEquals(expectedOutput3, fm.convertWorkoutToFileDataFormat(testSample3));
+    }
+}
