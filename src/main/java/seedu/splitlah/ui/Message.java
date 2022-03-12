024cc6f5e8bda86b9ecd8d45eb76ab9f91879372
warrencxw <cxw.warren@gmail.com>
2022-03-12 16:11:25 +0800

    Add error message for invalid date format

--- src/main/java/seedu/splitlah/ui/Message.java
+++ src/main/java/seedu/splitlah/ui/Message.java
@@ -43,6 +43,8 @@ public abstract class Message {
             "Please enter a valid integer after the following delimiter: ";
     public static final String ERROR_PARSER_ID_VALUE_NOT_POSITIVE =
             "Please enter a positive value for identifier numbers.";
+    public static final String ERROR_PARSER_INVALID_DATE_FORMAT =
+            "Please enter the date in the form DD-MM-YYYY or enter 'today' for today's date.";
     public static final String ERROR_PARSER_NON_MONETARY_VALUE_ARGUMENT =
             "Please enter a valid monetary value(s) after the following delimiter: ";
     public static final String ERROR_PARSER_INVALID_GST_SURCHARGE =
