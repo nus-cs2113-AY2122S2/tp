b00de5bc65d9445839cb36723ee906577f3eb5a5
z0723-julie <ZHAO0348@e.ntu.edu.sg>
2022-03-13 11:36:22 +0800

    Merge branch 'AY2122S2-CS2113-T11-4:master' into master

---
+++ src/main/java/seedu/duke/controllers/DishController.java
@@ -0,0 +1,118 @@
+package seedu.duke.controllers;
+
+import seedu.duke.entities.Dish;
+import seedu.duke.manager.DishManager;
+
+import java.util.Scanner;
+
+/**
+ * DishController (or MenuController if you like).
+ */
+public class DishController extends Controller {
+    private static final String[] CHOICES = {"Exit Menu", "Add Dish", "Delete Dish",
+        "Change the price of a dish", "Change the name of a dish"
+    };
+    private final DishManager dishManager;
+
+    public DishController(Scanner scanner) {
+        super(CHOICES, scanner);
+        dishManager = new DishManager();
+    }
+
+    @Override
+    protected boolean optionSwitcher(int choice) throws IllegalArgumentException {
+        switch (choice) {
+        case 1:
+            addDish();
+            break;
+        case 2:
+            deleteDish();
+            break;
+        case 3:
+            changePrice();
+            break;
+        case 4:
+            changeName();
+            break;
+        case 0:
+            // Relinquish control. This will return to the programme main function.
+            System.out.println("Exiting application...");
+            return true;
+        default:
+            // This should not happen, since the choice argument for this method is always parsed from the
+            // getChoice() method, which also checks for the range of index input.
+            System.out.println("Unknown choice!");
+        }
+        System.out.println("Now in Main Menu.");
+        System.out.println(this);
+        return false;
+    }
+
+    /**
+     * Give user prompt and get input to change name of one dish.
+     * @throws IllegalArgumentException when input cannot be parsed
+     */
+    private void changeName() throws IllegalArgumentException {
+        System.out.println("Changing name");
+        System.out.print("The index of dish");
+        int index = scanner.nextInt();
+        System.out.print("The new name of dish");
+        String name = scanner.nextLine();
+        try {
+            dishManager.setName(index, name);
+        } catch (IndexOutOfBoundsException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
+
+    /**
+     * Give user prompt and get input to change price of one dish.
+     * @throws IllegalArgumentException when input cannot be parsed.
+     */
+    private void changePrice() throws IllegalArgumentException {
+        System.out.println("Changing price");
+        System.out.print("The index of dish: ");
+        int index = scanner.nextInt();
+        System.out.print("The new price of dish: ");
+        double newPrice = scanner.nextDouble();
+        try {
+            dishManager.setPrice(index, newPrice);
+        } catch (IndexOutOfBoundsException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
+
+    /**
+     * Give user prompt and get input to delete one dish.
+     * @throws IllegalArgumentException when input cannot be parsed
+     */
+    private void deleteDish() throws IllegalArgumentException {
+        System.out.println("Deleting dish");
+        System.out.print("The index of dish : ");
+        int index = scanner.nextInt();
+        try {
+            dishManager.deleteDish(index);
+        } catch (IndexOutOfBoundsException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
+
+    /**
+     * Give user prompt and get input to add one dish.
+     * @throws IllegalArgumentException When input cannot be parsed
+     */
+    private void addDish() throws IllegalArgumentException {
+        System.out.println("Adding new dish");
+        System.out.print("The name of dish: ");
+        String name = scanner.nextLine();
+        System.out.print("The price of dish: ");
+        double price = scanner.nextDouble();
+        dishManager.addDish(new Dish(name, price));
+    }
+
+    @Override
+    public void takeControl() {
+        System.out.println("You are using Menu function");
+        super.takeControl();
+    }
+}
---
+++ src/test/java/seedu/duke/manager/DishManagerTest.java
@@ -0,0 +1,64 @@
+package seedu.duke.manager;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.entities.Dish;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+
+class DishManagerTest {
+    @Test
+    void deleteDish_IndexNotValid_ThrowIllegalArgumentException() {
+        DishManager dishManager = new DishManager();
+        dishManager.addDish(new Dish("A", 1));
+        dishManager.addDish(new Dish("B", 0));
+        dishManager.addDish(new Dish("C", 30));
+        dishManager.addDish(new Dish("D", 2));
+        dishManager.addDish(new Dish("E", 7));
+        assertDoesNotThrow(() -> dishManager.deleteDish(1));
+        assertDoesNotThrow(() -> dishManager.deleteDish(2));
+        assertThrows(IndexOutOfBoundsException.class, () -> dishManager.deleteDish(-1));
+        assertThrows(IndexOutOfBoundsException.class, () -> dishManager.deleteDish(0));
+        assertThrows(IndexOutOfBoundsException.class, () -> dishManager.deleteDish(4));
+        assertDoesNotThrow(() -> dishManager.deleteDish(3));
+        assertDoesNotThrow(() -> dishManager.deleteDish(1));
+        assertDoesNotThrow(() -> dishManager.deleteDish(1));
+        assertEquals(0, dishManager.getNumOfDishes());
+    }
+
+
+    @Test
+    void setPrice_IndexNotValidOrPriceNotValid_Throw() {
+        DishManager dishManager = new DishManager();
+        dishManager.addDish(new Dish("A", 1));
+        dishManager.addDish(new Dish("B", 0));
+        dishManager.addDish(new Dish("C", 30));
+        dishManager.addDish(new Dish("D", 2));
+        dishManager.addDish(new Dish("E", 7));
+        assertDoesNotThrow(() -> dishManager.setPrice(1, 10));
+        assertDoesNotThrow(() -> dishManager.setPrice(2, 20));
+        assertThrows(IllegalArgumentException.class, () -> dishManager.setPrice(1, -1));
+        assertThrows(IndexOutOfBoundsException.class, () -> dishManager.setPrice(0, -1));
+        assertThrows(IndexOutOfBoundsException.class, () -> dishManager.setPrice(-1, 10));
+        assertThrows(IndexOutOfBoundsException.class, () -> dishManager.setPrice(6, 100));
+    }
+
+    @Test
+    void setName_IndexNotValidOrNameNotValid_Throw() {
+        DishManager dishManager = new DishManager();
+        dishManager.addDish(new Dish("A", 1));
+        dishManager.addDish(new Dish("B", 0));
+        dishManager.addDish(new Dish("C", 30));
+        dishManager.addDish(new Dish("D", 2));
+        dishManager.addDish(new Dish("E", 7));
+        assertDoesNotThrow(() -> dishManager.setName(1, "AAAAA"));
+        assertDoesNotThrow(() -> dishManager.setName(2, "BBBBB"));
+        assertThrows(IllegalArgumentException.class, () -> dishManager.setName(1, ""));
+        assertThrows(IllegalArgumentException.class, () -> dishManager.setName(1, null));
+        assertThrows(IndexOutOfBoundsException.class, () -> dishManager.setName(0, "CCCCC"));
+        assertThrows(IndexOutOfBoundsException.class, () -> dishManager.setName(-1, "DDDDD"));
+        assertThrows(IndexOutOfBoundsException.class, () -> dishManager.setName(6, "EEEEE"));
+    }
+}
