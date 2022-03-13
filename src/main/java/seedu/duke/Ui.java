5c03324b134c55bedd5995432aeae57c04b02f05
siewyangzhi <syangzhi54@gmail.com>
2022-03-13 13:57:00 +0800

    Add Ui class and the function to read in user input.
    Fixes#25

---
+++ src/main/java/seedu/duke/Ui.java
@@ -0,0 +1,25 @@
+package seedu.duke;
+
+import java.util.Scanner;
+/**
+ * Represents the User Interface component of the program.
+ * The Ui object handles all interactions with the user such as printing the outputs to the user
+ * as well as reading in the user inputs .
+ */
+public class Ui {
+    private Scanner in;
+    public Ui() {
+        in = new Scanner(System.in);
+    }
+
+    /**
+     * Returns a string containing the user input.
+     *
+     * @return User input
+     */
+    public String readUserInput() {
+        String userInput = in.nextLine();
+        return userInput;
+    }
+
+}
