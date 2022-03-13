9fd7bbbed254a70c6c01548df540fee54356338c
Kr0sfyre <teanweijun@gmail.com>
2022-03-13 17:11:32 +0800

    Create a logger for the program

--- src/main/java/seedu/planitarium/PlanITarium.java
+++ src/main/java/seedu/planitarium/PlanITarium.java
@@ -1,15 +1,11 @@
 package seedu.planitarium;
 
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.*;
 
 public class PlanITarium {
-    
-    public void run() {
-        Scanner in = new Scanner();
-        initialisePlanitarium();
-        // executeCommands();
-        printExitMessage();
-    }
+    public static final Logger logger = Logger.getLogger(PlanITarium.class.getName());
 
     /**
      * Entry-point for the PlanITarium application.
@@ -18,8 +14,29 @@ public class PlanITarium {
         new PlanITarium().run();
     }
 
+    public void run() {
+        Scanner in = new Scanner();
+        initialisePlanitarium();
+        // executeCommands();
+        printExitMessage();
+    }
+
     private static void initialisePlanitarium() {
+        try {
+            initialiseLogger();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "File logger not working.", e);
+        }
         printWelcomeMessage();
+    }
+
+    private static void initialiseLogger() throws IOException {
+        LogManager.getLogManager().reset();
+        logger.setLevel(Level.ALL);
+        FileHandler fh = new FileHandler("PlanITarium.log");
+        fh.setFormatter(new SimpleFormatter());
+        fh.setLevel(Level.ALL);
+        logger.addHandler(fh);
     }
 
     private static void printWelcomeMessage() {
