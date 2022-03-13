b7217931886f19e8a6930cc42cdb636587cf0bc2
z0723-julie <ZHAO0348@e.ntu.edu.sg>
2022-03-13 12:30:32 +0800

    Modify the checkstyle

--- src/main/java/seedu/duke/manager/OrderManager.java
+++ src/main/java/seedu/duke/manager/OrderManager.java
@@ -47,12 +47,13 @@ public class OrderManager {
         }
         return total;
     }
-    public void printReceipt(){
+
+    public void printReceipt() {
         if (this.orders.size() == 0) {
             System.out.println("No orders!");
         }
         for (Order order: orders) {
-            for(Dish dish : dishes) {
+            for (Dish dish : dishes) {
                 System.out.println(dish.toString());
             }
             System.out.println("Total price:" + order.getTotalPrice());
