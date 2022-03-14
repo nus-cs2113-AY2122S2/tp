7ddd689fed60074a0860388bc15f6ba04051e045
DESKTOP-91KNFFF\LI HAO <yeaoming@gmail.com>
2022-03-14 15:10:53 +0800

    Add a exit command class

---
+++ src/main/java/seedu/duke/commands/ExitCommand.java
@@ -0,0 +1,13 @@
+package seedu.duke.commands;
+
+import seedu.duke.Timetable;
+
+public class ExitCommand extends Command {
+
+    public static final String EXIT_COMMAND = "exit";
+
+    @Override
+    public String execute(Timetable timetable) {
+        return " ";
+    }
+}
