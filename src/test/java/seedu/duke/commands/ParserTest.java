e5cb59fb526c59661e9c1b8c78def0f2693041ca
ibrahimisramos <64336014+ibrahimisramos@users.noreply.github.com>
2022-03-14 23:26:45 +0800

    Change JUnit Test for assert

--- src/test/java/seedu/duke/commands/ParserTest.java
+++ src/test/java/seedu/duke/commands/ParserTest.java
@@ -5,29 +5,16 @@ import org.junit.jupiter.api.Test;
 import seedu.duke.Parser;
 import seedu.duke.Timetable;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static seedu.duke.ErrorMessages.ERROR_INDEX_OUT_OF_BOUND;
-import static seedu.duke.ErrorMessages.ERROR_INVALID_INDEX_FORMAT;
 
 public class ParserTest {
-
-    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-
-    @BeforeEach
-    public void setUp() {
-        System.setOut(new PrintStream(outputStreamCaptor));
-    }
 
     @Test
     public void prepareDeleteCommand_InvalidIndex_throwException() {
         Timetable timetable = new Timetable();
         String inputString = "delete hello";
         Parser parser = new Parser(inputString);
-        parser.parseCommand();
-        //assertEquals(ERROR_INVALID_INDEX_FORMAT + "\n",outputStreamCaptor.toString());
-        assertEquals(new HelpCommand().execute(timetable),parser.prepareDelete().execute(timetable));
+        Command command = parser.parseCommand();
+        assertEquals(HelpCommand.class,command.getClass());
     }
 }
