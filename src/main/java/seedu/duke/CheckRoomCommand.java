b47d18fd1f38292ccab91090518a072a6576d23a
Xunyi <73570047+icthenic@users.noreply.github.com>
2022-03-14 21:22:57 +0800

    fix bugs

---
+++ src/main/java/seedu/duke/CheckRoomCommand.java
@@ -0,0 +1,25 @@
+package seedu.duke;
+
+public class CheckRoomCommand extends Command {
+    private int roomId;
+    public CheckRoomCommand(String commandStringWithoutCommand) {
+        roomId = Integer.parseInt(commandStringWithoutCommand.trim());
+    }
+
+    @Override
+    public void execute(){
+
+    }
+
+    @Override
+    // check if the room id is correct
+    public void execute(RoomList list) {
+        for(Room room : list.getRoomList()) {
+            if (room.getRoomId() == roomId) {
+                System.out.println(room);
+                return;
+            }
+        }
+    }
+}
+
