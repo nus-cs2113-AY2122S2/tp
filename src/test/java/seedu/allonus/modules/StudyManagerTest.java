264225be04b06bdec1b5f881ef58a18f60200300
Mathan <mathanmahe99@gmail.com>
2022-03-14 20:07:32 +0800

    Add JUnit for StudyManager class

---
+++ src/test/java/seedu/allonus/modules/StudyManagerTest.java
@@ -0,0 +1,68 @@
+package seedu.allonus.modules;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class StudyManagerTest {
+
+    public static final String USER_INPUT_FOR_DELETE = "rm 1";
+    private Module cs2113;
+    private Module cs3244;
+    private Module ee4204;
+
+    ArrayList<Module> testList = new ArrayList<>();
+
+    @BeforeAll
+    public void setUp() {
+        cs2113 = new Module("CS2113", "Lecture", "Friday", "4pm-6pm");
+        cs3244 = new Module("CS3244", "Tutorial", "Monday", "2pm-3pm");
+        ee4204 = new Module("EE4204", "Exam", "Monday", "10am-12pm");
+
+        testList.add(cs2113);
+        testList.add(cs3244);
+        testList.add(ee4204);
+    }
+
+    @Test
+    public void testAddModule() {
+        StudyManager studyManager = new StudyManager();
+
+        String userInputCs2113 = "add m/CS2113 c/lec d/Friday t/4pm-6pm";
+        String userInputCs3244 = "add m/CS3244 c/tut d/Monday t/2pm-3pm";
+        String userInputEe4204 = "add m/EE4204 c/exam d/Monday t/10am-12pm";
+
+        studyManager.addModule(userInputCs2113);
+        studyManager.addModule(userInputCs3244);
+        studyManager.addModule(userInputEe4204);
+
+        for (int i = 0; i < testList.size(); i++) {
+            assertEquals(testList.get(i).getModuleCode(), studyManager.getModulesList().get(i).getModuleCode());
+            assertEquals(testList.get(i).getCategory(), studyManager.getModulesList().get(i).getCategory());
+            assertEquals(testList.get(i).getDay(), studyManager.getModulesList().get(i).getDay());
+            assertEquals(testList.get(i).getTimeSlot(), studyManager.getModulesList().get(i).getTimeSlot());
+        }
+    }
+
+    @Test
+    public void testDeleteModule() {
+        StudyManager studyManager = new StudyManager();
+
+        String USER_INPUT_FOR_DELETE = "rm 1";
+        studyManager.deleteModule(USER_INPUT_FOR_DELETE);
+
+        // after deletion cs2113 should not be in the list
+        for (Module m: studyManager.getModulesList()) {
+            assertNotEquals(m.getModuleCode(), cs2113.getModuleCode());
+            assertNotEquals(m.getCategory(), cs2113.getCategory());
+            assertNotEquals(m.getDay(), cs2113.getDay());
+            assertNotEquals(m.getTimeSlot(), cs2113.getTimeSlot());
+        }
+    }
+}
