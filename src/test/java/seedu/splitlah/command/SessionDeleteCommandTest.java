41acd58abde2939a316fabd7351ed55808cac432
Roy Tang <roy_tql@hotmail.com>
2022-03-14 15:51:43 +0800

    Add setup function for SessionDeleteCommandTest file

--- src/test/java/seedu/splitlah/command/SessionDeleteCommandTest.java
+++ src/test/java/seedu/splitlah/command/SessionDeleteCommandTest.java
@@ -1,12 +1,27 @@
 package seedu.splitlah.command;
 
 import org.junit.jupiter.api.BeforeEach;
+import seedu.splitlah.data.Manager;
+import seedu.splitlah.parser.Parser;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SessionDeleteCommandTest {
 
+    Manager manager = new Manager();
+
+    /**
+     * Creates 2 sessions that is stored and managed by the Manager object.
+     */
     @BeforeEach
     void setUp() {
+        String sessionOneArgs = "session /create /n Class outing /d 15-02-2022 /pl Alice Bob";
+        String sessionTwoArgs = "session /create /n Family gathering /d 20-02-2022  /pl Eves Mallory";
+        Command createSessionOne = Parser.getCommand(sessionOneArgs);
+        createSessionOne.run(manager);
+        Command createSessionTwo = Parser.getCommand(sessionTwoArgs);
+        createSessionTwo.run(manager);
     }
+
+
 }
