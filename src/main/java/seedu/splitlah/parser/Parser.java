36c6f9b84e1a4d6c65725df8e3d897daeb1f7403
warrencxw <cxw.warren@gmail.com>
2022-03-14 15:55:34 +0800

    Convert delimiter constants to public visibility in Parser class

--- src/main/java/seedu/splitlah/parser/Parser.java
+++ src/main/java/seedu/splitlah/parser/Parser.java
@@ -27,18 +27,18 @@ import java.time.format.DateTimeParseException;
 public class Parser {
     
     // DELIMITERS
-    private static final String NAME_DELIMITER = "/n";
-    private static final String PERSON_LIST_DELIMITER = "/pl";
-    private static final String INVOLVED_DELIMITER = "/i";
-    private static final String PAYER_DELIMITER = "/p";
-    private static final String SESSION_ID_DELIMITER = "/sid";
-    private static final String ACTIVITY_ID_DELIMITER = "/aid";
-    private static final String GROUP_ID_DELIMITER = "/gid";
-    private static final String DATE_DELIMITER = "/d";
-    private static final String TOTAL_COST_DELIMITER = "/c";
-    private static final String COST_LIST_DELIMITER = "/cl";
-    private static final String GST_DELIMITER = "/gst";
-    private static final String SERVICE_CHARGE_DELIMITER = "/sc";
+    public static final String NAME_DELIMITER = "/n";
+    public static final String PERSON_LIST_DELIMITER = "/pl";
+    public static final String INVOLVED_DELIMITER = "/i";
+    public static final String PAYER_DELIMITER = "/p";
+    public static final String SESSION_ID_DELIMITER = "/sid";
+    public static final String ACTIVITY_ID_DELIMITER = "/aid";
+    public static final String GROUP_ID_DELIMITER = "/gid";
+    public static final String DATE_DELIMITER = "/d";
+    public static final String TOTAL_COST_DELIMITER = "/c";
+    public static final String COST_LIST_DELIMITER = "/cl";
+    public static final String GST_DELIMITER = "/gst";
+    public static final String SERVICE_CHARGE_DELIMITER = "/sc";
 
     // MISC CONSTANTS
     private static final String DELIMITER_INDICATOR = "/";
