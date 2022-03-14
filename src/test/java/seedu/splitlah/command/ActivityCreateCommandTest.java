2779ac3d8b2f5b61947e65479a924d63cc49b089
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-14 17:00:50 +0800

    Update total cost delimiter in Parser and ActivityCreateCommandTest
    Change "/c" to "/co".

--- src/test/java/seedu/splitlah/command/ActivityCreateCommandTest.java
+++ src/test/java/seedu/splitlah/command/ActivityCreateCommandTest.java
@@ -21,7 +21,7 @@ class ActivityCreateCommandTest {
         String sessionOneArgs = "session /create /n Class outing /d 15-02-2022 /pl Alice Bob Charlie";
         Command createSessionOne = Parser.getCommand(sessionOneArgs);
         createSessionOne.run(manager);
-        String activityOneArgs = "activity /create /sid 1 /n Lunch /p Alice /i Alice Bob Charlie /c 15";
+        String activityOneArgs = "activity /create /sid 1 /n Lunch /p Alice /i Alice Bob Charlie /co 15";
         Command createActivityOne = Parser.getCommand(activityOneArgs);
         createActivityOne.run(manager);
     }
@@ -45,7 +45,7 @@ class ActivityCreateCommandTest {
      */
     @Test
     public void prepare_hasBothCostAndCostList_invalidCommand() throws InvalidDataException {
-        String userInput = "activity /create /sid 1 /n Dinner /p Alice /i Alice Bob Charlie /c 30 /cl 10 10 10";
+        String userInput = "activity /create /sid 1 /n Dinner /p Alice /i Alice Bob Charlie /co 30 /cl 10 10 10";
         Command command = Parser.getCommand(userInput);
         assertEquals(InvalidCommand.class, command.getClass());
     }
@@ -72,7 +72,7 @@ class ActivityCreateCommandTest {
      */
     @Test
     public void run_hasNameDuplicatesInInvolvedList_activityListSizeRemainsOne() throws InvalidDataException {
-        String userInput = "activity /create /sid 1 /n Dinner /p Alice /i Alice Alice Charlie /c 30";
+        String userInput = "activity /create /sid 1 /n Dinner /p Alice /i Alice Alice Charlie /co 30";
         Command command = Parser.getCommand(userInput);
         command.run(manager);
         assertEquals(1, manager.getProfile().getSession(1).getActivityList().size());
@@ -84,22 +84,22 @@ class ActivityCreateCommandTest {
     @Test
     public void prepare_hasMissingDelimiter_InvalidCommand() {
         // Case 1: Missing /sid delimiter
-        String argsMissingSessionIdDelimiter = "activity /create /n Dinner /p Alice /i Alice Bob Charlie /c 15";
+        String argsMissingSessionIdDelimiter = "activity /create /n Dinner /p Alice /i Alice Bob Charlie /co 15";
         Command sessionWithMissingSessionIdDelimiter = Parser.getCommand(argsMissingSessionIdDelimiter);
         assertEquals(InvalidCommand.class, sessionWithMissingSessionIdDelimiter.getClass());
 
         // Case 2: Missing /n delimiter
-        String argsMissingNameDelimiter = "activity /create /sid 1 /p Alice /i Alice Bob Charlie /c 15";
+        String argsMissingNameDelimiter = "activity /create /sid 1 /p Alice /i Alice Bob Charlie /co 15";
         Command sessionWithMissingNameDelimiter = Parser.getCommand(argsMissingNameDelimiter);
         assertEquals(InvalidCommand.class, sessionWithMissingNameDelimiter.getClass());
 
         // Case 3: Missing /p delimiter
-        String argsMissingPayerListDelimiter = "activity /create /sid 1 /n Dinner /i Alice Bob Charlie /c 15";
+        String argsMissingPayerListDelimiter = "activity /create /sid 1 /n Dinner /i Alice Bob Charlie /co 15";
         Command sessionWithMissingPayerListDelimiter = Parser.getCommand(argsMissingPayerListDelimiter);
         assertEquals(InvalidCommand.class, sessionWithMissingPayerListDelimiter.getClass());
 
         // Case 4: Missing /i delimiter
-        String argsMissingInvolvedListDelimiter = "activity /create /sid 1 /n Dinner /p Alice /c 15";
+        String argsMissingInvolvedListDelimiter = "activity /create /sid 1 /n Dinner /p Alice /co 15";
         Command activityWithMissingInvolvedListDelimiter = Parser.getCommand(argsMissingInvolvedListDelimiter);
         assertEquals(InvalidCommand.class, activityWithMissingInvolvedListDelimiter.getClass());
     }
