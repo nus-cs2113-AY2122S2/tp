bec88021f5dade3dc071a5a327cd0d3c1f07ef0b
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-14 11:43:40 +0800

    Update setUp method in ActivityCreateCommandTest

--- src/test/java/seedu/splitlah/command/ActivityCreateCommandTest.java
+++ src/test/java/seedu/splitlah/command/ActivityCreateCommandTest.java
@@ -1,10 +1,20 @@
 package seedu.splitlah.command;
 
 import org.junit.jupiter.api.BeforeEach;
+import seedu.splitlah.data.Manager;
 
 class ActivityCreateCommandTest {
 
+    Manager manager = new Manager();
+
     @BeforeEach
     void setUp() {
+        String sessionOneArgs = "session /create /n Class outing /d 15-02-2022 /pl Alice Bob Charlie";
+        Command createSessionOne = SessionCreateCommand.prepare(sessionOneArgs);
+        createSessionOne.run(manager);
+        String activityOneArgs = "activity /create /n Lunch /p Alice /i Alice Bob Charlie /c 15";
+        Command createActivityOne = ActivityCreateCommand.prepare(activityOneArgs);
+        createActivityOne.run(manager);
     }
+
 }
