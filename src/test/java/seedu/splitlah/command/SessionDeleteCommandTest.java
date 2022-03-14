729ae7d2dd21b5ec278da1a3aa9995f9d853c765
Roy Tang <roy_tql@hotmail.com>
2022-03-14 15:49:03 +0800

    Add SessionDeleteCommandTest file

---
+++ src/test/java/seedu/splitlah/command/SessionDeleteCommandTest.java
@@ -0,0 +1,12 @@
+package seedu.splitlah.command;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionDeleteCommandTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
