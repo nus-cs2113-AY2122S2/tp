f9398e942d010a74f8c5ac2e37ec7d8947ec36fe
striris <irisxinran@gmail.com>
2022-03-14 13:13:35 +0800

    added in messages for ui display

---
+++ src/main/java/common/Messages.java
@@ -0,0 +1,17 @@
+package common;
+
+/**
+ * Container for user visible messages.
+ */
+public class Messages {
+    public static final String MESSAGE_GOODBYE = "Good bye!";
+    public static final String MESSAGE_INIT_FAILED = "Failed to initialise PersonBook application. Exiting...";
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
+    public static final String MESSAGE_PERSON_NOT_IN_PERSONBOOK = "Person could not be found in PersonBook";
+    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
+    public static final String MESSAGE_PROGRAM_LAUNCH_ARGS_USAGE = "Launch command format: "
+            + "java seedu.personbook.Main";
+    public static final String MESSAGE_WELCOME = "Welcome to your PersonBook!";
+}
+
