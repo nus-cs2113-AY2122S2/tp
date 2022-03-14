0dba249fdb23a3cee61465927c066fdfd8c66e55
Zhou Qi <>
2022-03-14 23:26:25 +0800

    Update Command classes

--- src/main/java/tp/ListDoctorListCommand.java
+++ src/main/java/tp/ListDoctorListCommand.java
@@ -5,6 +5,6 @@ public class ListDoctorListCommand extends Command {
     @Override
     public void execute(DoctorList doctorList, PatientList patientList,
                         AppointmentList appointmentList, Ui ui, Storage storage) throws IHospitalException {
-        doctorList.printDoctorList();
+        System.out.print(doctorList);
     }
 }
