fbea2adc74282bf8808cfdc09d4be8361c0c4283
alanlowzies <30099983+alanlowzies@users.noreply.github.com>
2022-03-14 14:54:18 +0800

    Add ability to check for duplicate workouts when user creates a new workout

--- src/main/java/data/workouts/InvalidWorkoutException.java
+++ src/main/java/data/workouts/InvalidWorkoutException.java
@@ -5,8 +5,10 @@ package data.workouts;
  */
 public class InvalidWorkoutException extends Exception {
     // Pre-defined error messages
-    public static final String INVALID_REPS_VALUE = "Uh oh, the number of reps specified is invalid.\n"
+    public static final String INVALID_REPS_VALUE_ERROR_MSG = "Uh oh, the number of reps specified is invalid.\n"
             + "(Needs to be a value of at least 1.)";
+    public static final String DUPLICATE_WORKOUT_ERROR_MSG = "Uh oh, there already is an existing workout with "
+            + "an identical\nexercise name and number of repetitions.";
 
     private String throwingClass;
 
