f583b4234b0af175d70bb748f2d549914e56a756
ibrahimisramos <64336014+ibrahimisramos@users.noreply.github.com>
2022-03-14 15:15:53 +0800

    Fixes #8 and #9

--- src/main/java/seedu/duke/commands/ClearCommand.java
+++ src/main/java/seedu/duke/commands/ClearCommand.java
@@ -1,12 +1,21 @@
 package seedu.duke.commands;
 
 import seedu.duke.Timetable;
+import seedu.duke.events.Event;
 
 public class ClearCommand extends Command {
     public static final String COMMAND_WORD = "clear";
 
     @Override
     public void execute(Timetable timetable) {
-        timetable.clear();
+        for (int i = 0; i < timetable.size(); i++){
+            timetable.remove(i);
+        }
+        printClearConfirmation();
+    }
+
+    public static void printClearConfirmation() {
+        System.out.println("Your whole timetable has been cleared");
+
     }
 }
