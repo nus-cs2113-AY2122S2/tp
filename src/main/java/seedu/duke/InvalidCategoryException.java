be542653281a81bb9190fa50c0f02f0b4bc38a18
Xunyi <73570047+icthenic@users.noreply.github.com>
2022-03-14 23:20:08 +0800

    add exceptions

---
+++ src/main/java/seedu/duke/InvalidCategoryException.java
@@ -0,0 +1,11 @@
+package seedu.duke;
+
+public class InvalidCategoryException extends HotelLiteManagerException{
+        private static final String ERROR_MESSAGE = "Error! Room number is invalid";
+
+        @Override
+        public String getErrorMessage() {
+            return ERROR_MESSAGE;
+        }
+
+}
