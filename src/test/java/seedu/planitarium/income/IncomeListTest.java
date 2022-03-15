fe9d91c71a50187d17e665ec8a189605a9799e51
tjiarong <jia8991@gmail.com>
2022-03-15 18:16:22 +0800

    Created skeleton JUnit file for Income and IncomeList

---
+++ src/test/java/seedu/planitarium/income/IncomeListTest.java
@@ -0,0 +1,36 @@
+package seedu.planitarium.income;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IncomeListTest {
+
+    @Test
+    void addIncome() {
+    }
+
+    @Test
+    void getIncomeValue() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getNumberOfIncomes() {
+    }
+
+    @Test
+    void getTotalIncome() {
+    }
+
+    @Test
+    void printIncomeList() {
+    }
+}
---
+++ src/test/java/seedu/planitarium/income/IncomeTest.java
@@ -0,0 +1,20 @@
+package seedu.planitarium.income;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IncomeTest {
+
+    @Test
+    void getAmount() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
