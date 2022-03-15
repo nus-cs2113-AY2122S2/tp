6a88d998133dc74b5fc9710b7eaef5430e547475
JL <tha.junlim@gmail.com>
2022-03-16 02:13:58 +0800

    Add help feature for Study Command

--- src/main/java/seedu/sherpass/command/StudyCommand.java
+++ src/main/java/seedu/sherpass/command/StudyCommand.java
@@ -6,10 +6,14 @@ import seedu.sherpass.util.Storage;
 import seedu.sherpass.util.TimerLogic;
 import seedu.sherpass.util.Ui;
 
+import static seedu.sherpass.constant.Message.HELP_MESSAGE_STUDY;
 import static seedu.sherpass.constant.Message.WELCOME_MESSAGE_STUDY;
 
 public class StudyCommand extends Command {
     public static final String COMMAND_WORD = "study";
+    public static final String COMMAND_USAGE = "Study: Enters a study session.\n\n"
+            + "User can access timer features while in a study session.\n\n"
+            + HELP_MESSAGE_STUDY;
 
     @Override
     public void execute(TaskList taskList, Ui ui, Storage storage) {
