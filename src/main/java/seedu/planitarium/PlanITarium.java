bf5b06c82a8b4ba8b2a21ecce94fa558da4d4ca8
Kr0sfyre <teanweijun@gmail.com>
2022-03-15 20:27:04 +0800

    Fix merge conflicts and reorder methods in main driver file

--- src/main/java/seedu/planitarium/PlanITarium.java
+++ src/main/java/seedu/planitarium/PlanITarium.java
@@ -1,18 +1,27 @@
 package seedu.planitarium;
 
-<<<<<<< HEAD
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.*;
 import commands.Command;
 import ui.UI;
-import java.util.Scanner;
 
 public class PlanITarium {
     public static final Logger logger = Logger.getLogger(PlanITarium.class.getName());
     protected Scanner userInput;
     protected Command commandExecuter;
     protected UI ui = new UI();
+
+    /**
+     * Entry-point for the PlanITarium application.
+     */
+    public static void main(String[] args) {
+        try {
+            new PlanITarium().run();
+        } catch (Exception e) {
+            UI.exit(); // Need fix later
+        }
+    }
 
     public void run() throws Exception {
         initialisePlanitarium(ui);
@@ -39,16 +48,5 @@ public class PlanITarium {
         fh.setFormatter(new SimpleFormatter());
         fh.setLevel(Level.ALL);
         logger.addHandler(fh);
-    }
-
-    /**
-     * Entry-point for the PlanITarium application.
-     */
-    public static void main(String[] args) {
-        try {
-            new PlanITarium().run();
-        } catch (Exception e) {
-            UI.exit(); // Need fix later
-        }
     }
 }
