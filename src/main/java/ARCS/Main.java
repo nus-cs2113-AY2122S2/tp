a1232bd316557c7e6ea1ebad0500d04f59a02e61
ElaineQT <wangqianteng@gmail.com>
2022-03-16 11:01:08 +0800

    Set file structure

---
+++ src/main/java/ARCS/Main.java
@@ -0,0 +1,12 @@
+package ARCS;
+
+import java.util.Scanner;
+
+public class Main {
+    /**
+     * Main entry-point for the ARCS application.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello! Welcome to ARCS system.");
+    }
+}
--- src/test/java/ARCS/DukeTest.java
+++ src/test/java/ARCS/DukeTest.java
@@ -1,10 +1,10 @@
-package seedu.duke;
+package ARCS;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-class DukeTest {
+class ARCSTest {
     @Test
     public void sampleTest() {
         assertTrue(true);
