3992ac1adf4091339b38abdfb4576cbf91bf0887
khseah <seahkithan55@gmail.com>
2022-03-16 22:40:05 +0800

    Edit runtest.bat file

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -12,8 +12,6 @@ for /f "tokens=*" %%a in (
     set jarloc=%%a
 )
 
-java -jar %jarloc% < ..\..\text-ui-test\input.txt > ..\..\text-ui-test\ACTUAL.TXT
-
 cd ..\..\text-ui-test
 
 FC ACTUAL.TXT EXPECTED.TXT >NUL && ECHO Test passed! || Echo Test failed!
