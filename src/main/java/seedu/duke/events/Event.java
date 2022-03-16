fef4689cfedae51017daf376a86438789f3e5719
DESKTOP-OJ3Q66Q\angyo <angyongming@gmail.com>
2022-03-16 19:07:54 +0800

    Rectify function to check overlap

--- src/main/java/seedu/duke/events/Event.java
+++ src/main/java/seedu/duke/events/Event.java
@@ -49,4 +49,35 @@ public class Event {
         return String.format("NAME: %s\t\tTITLE: %s\t\tDAY: %s\t\tSTART: %04d\t\tEND: %04d\t\tMODE: %s",
                             name, title, day, startTime, endTime, mode);
     }
+
+    /**
+     * Checks if the events belongs to the same day and person. If it does not, there is definitely no overlap.
+     * If it does, check if there is timing overlap.
+     *
+     * @param obj The new event to be added
+     * @return True if the event belongs to the same person and on the same day with timing clash. False otherwise
+     */
+    public boolean overlaps(Object obj) {
+        if (!(obj instanceof Event)) {
+            return false;
+        }
+        Event event = (Event) obj;
+        if (!day.equals(event.day)) {
+            return false;
+        }
+        if (!name.equals(event.name)) {
+            return false;
+        }
+        boolean startTimeOverlap = event.startTime >= startTime
+                && event.startTime < endTime;
+        boolean endTimeOverlap = event.endTime > startTime
+                && event.endTime <= endTime;
+        boolean totalOverlap = event.startTime <= startTime
+                && event.endTime >= endTime;
+        if (startTimeOverlap || endTimeOverlap || totalOverlap) {
+            return true;
+        }
+        return false;
+    }
+
 }
