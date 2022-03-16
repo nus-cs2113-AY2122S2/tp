4b4f9a5f8e3f0a14b947e3265b41d1eab13dfeaf
DESKTOP-OJ3Q66Q\angyo <angyongming@gmail.com>
2022-03-16 12:24:06 +0800

    implement JUnit testing for prepareAdd in parser class

--- src/test/java/seedu/duke/commands/ParserTest.java
+++ src/test/java/seedu/duke/commands/ParserTest.java
@@ -7,14 +7,50 @@ import seedu.duke.Timetable;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import static seedu.duke.common.ErrorMessages.ERROR_INVALID_DAY;
+
 public class ParserTest {
 
     //setup
 
-    //prepareAdd
+    @Test
+    public void prepareAdd_invalidDay_throwException() {
+        Timetable timetable = new Timetable();
+        String inputString = "add n/john l/cs2113 d/mon st/1600 et/1800 m/online";
+        Parser parser = new Parser(inputString);
+        Command command = parser.parseCommand();
+        assertEquals(HelpCommand.class, command.getClass());
+    }
 
     @Test
-    public void prepareDeleteCommand_InvalidIndex_throwException() {
+    public void prepareAdd_timeOutOfRange_throwException() {
+        Timetable timetable = new Timetable();
+        String inputString = "add n/john l/cs2113 d/monday st/1690 et/1800 m/online";
+        Parser parser = new Parser(inputString);
+        Command command = parser.parseCommand();
+        assertEquals(HelpCommand.class, command.getClass());
+    }
+
+    @Test
+    public void prepareAdd_timeNotInteger_throwException() {
+        Timetable timetable = new Timetable();
+        String inputString = "add n/john l/cs2113 d/mon st/noon et/1800 m/online";
+        Parser parser = new Parser(inputString);
+        Command command = parser.parseCommand();
+        assertEquals(HelpCommand.class, command.getClass());
+    }
+
+    @Test
+    public void prepareAdd_invalidMode_throwException() {
+        Timetable timetable = new Timetable();
+        String inputString = "add n/john l/cs2113 d/monday st/1600 et/1800 m/lecture";
+        Parser parser = new Parser(inputString);
+        Command command = parser.parseCommand();
+        assertEquals(HelpCommand.class, command.getClass());
+    }
+
+    @Test
+    public void prepareDeleteCommand_invalidIndex_throwException() {
         Timetable timetable = new Timetable();
         String inputString = "delete hello";
         Parser parser = new Parser(inputString);
