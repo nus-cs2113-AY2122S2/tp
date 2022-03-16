00f7ed35e0daf71656df0e7ae9ae155b7710c93e
Demonshaha <572819396@qq.com>
2022-03-16 20:52:28 +0800

    Fixing bugs for v1.0

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,21 +1,28 @@
 package seedu.duke;
 
+import tp.*;
+import tp.person.Patient;
+
 import java.util.Scanner;
 
 public class Duke {
     /**
      * Main entry-point for the java.duke.Duke application.
      */
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                              + "|  _ \\ _   _| | _____ \n"
-                              + "| | | | | | | |/ / _ \\\n"
-                              + "| |_| | |_| |   <  __/\n"
-                              + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+    private static final Ui ui = new Ui();
+    private static final Parser parser = new Parser();
+    private static PatientList patientList = new PatientList();
+    private static DoctorList doctorList = new DoctorList();
+    private static AppointmentList appointmentList = new AppointmentList();
+    private static Storage storage = new Storage();
+    public static void main(String[] args) throws IHospitalException {
+        ui.sayHello();
+        String fullCommand = parser.getCommand();
+        while(!fullCommand.equals("bye")) {
+            Command command = parser.parse(fullCommand);
+            command.execute(doctorList, patientList, appointmentList, ui, storage);
+            fullCommand = parser.getCommand();
+        }
+        ui.sayGoodbye();
     }
 }
---
+++ src/main/java/tp/ExitCommand.java
@@ -0,0 +1,13 @@
+package tp;
+
+public class ExitCommand extends Command {
+    public ExitCommand() {
+
+    }
+
+
+    @Override
+    public void execute(DoctorList doctorList, PatientList patientList, AppointmentList appointmentList, Ui ui, Storage storage) throws IHospitalException {
+        return ;
+    }
+}
