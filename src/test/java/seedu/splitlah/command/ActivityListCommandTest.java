0f89d0609a32e35bb81d5aa1ddc86ce388286830
RenTianle <lele.rer@outlook.com>
2022-03-17 00:14:21 +0800

    Add test to check missing delimiter in the input

--- src/test/java/seedu/splitlah/command/ActivityListCommandTest.java
+++ src/test/java/seedu/splitlah/command/ActivityListCommandTest.java
@@ -1,4 +1,43 @@
 package seedu.splitlah.command;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.splitlah.data.Manager;
+import seedu.splitlah.parser.Parser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ActivityListCommandTest {
+
+    Manager manager = new Manager();
+
+    /**
+     * Creates a session managed by the Manager object.
+     * Creates two activities in the new session.
+     */
+    @BeforeEach
+    void setUp() {
+        String sessionArgs = "session /create /n Class outing /d 15-02-2022 /pl Alice Bob Charlie";
+        Command createSession = Parser.getCommand(sessionArgs);
+        createSession.run(manager);
+        String activityOneArgs = "activity /create /sid 1 /n Lunch /p Alice /i Alice Bob Charlie /co 15";
+        Command createActivityOne = Parser.getCommand(activityOneArgs);
+        createActivityOne.run(manager);
+        String activityTwoArgs = "activity /create /sid 1 /n Lunch /p Alice /i Bob Charlie /cl 5 5";
+        Command createActivityTwo = Parser.getCommand(activityTwoArgs);
+        createActivityTwo.run(manager);
+    }
+
+
+    /**
+     * Checks if activities are listed with missing sid.
+     */
+    @Test
+    public void prepare_hasMissingDelimiter_InvalidCommand() {
+        //Missing sid
+        String argsMissingSidDelimiter = "activity /list";
+        Command activityWithMissingSidDelimiter = Parser.getCommand(argsMissingSidDelimiter);
+        assertEquals(InvalidCommand.class, activityWithMissingSidDelimiter.getClass());
+    }
+
 }
