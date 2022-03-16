99a89504759ff50c88faf6f70b3bba397a6d8738
ElaineQT <wangqianteng@gmail.com>
2022-03-16 21:06:43 +0800

    Add ExitCommand

--- src/main/java/ARCS/commands/Command.java
+++ src/main/java/ARCS/commands/Command.java
@@ -4,9 +4,15 @@ import ARCS.data.RouteManager;
 
 public abstract class Command {
     protected RouteManager routeManager;
+    protected boolean isExit = false;
 
     public void setData(RouteManager routeManager) {
         this.routeManager = routeManager;
     }
+
     public abstract CommandResult execute();
+
+    public boolean isExit() {
+        return isExit;
+    }
 }
