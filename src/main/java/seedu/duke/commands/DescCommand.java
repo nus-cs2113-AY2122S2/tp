27ac8b130b3e64d30787a910cb12b509cf330ff3
Lester Sim <lester.sim@u.nus.edu>
2022-03-16 23:52:42 +0800

    Update DescCommand Class
    Changed the user input to search from a search string to an index

--- src/main/java/seedu/duke/commands/DescCommand.java
+++ src/main/java/seedu/duke/commands/DescCommand.java
@@ -3,27 +3,26 @@ package seedu.duke.commands;
 import seedu.duke.data.Item;
 import seedu.duke.data.ItemList;
 import seedu.duke.exceptions.InvMgrException;
-import seedu.duke.storage.Storage;
 import seedu.duke.ui.Ui;
 
 public class DescCommand extends Command {
-    private final String itemName;
+    private final int index;
 
-    public DescCommand(String itemName) {
-        this.itemName = itemName;
+    public DescCommand(int index) {
+        this.index = index;
     }
 
     @Override
-    public void execute(ItemList itemList, Ui ui, Storage storage) {
+    public void execute(ItemList itemList, Ui ui) {
         try {
             // Get item from itemList by searching for the item's name
-            Item item = itemList.contains(itemName);
+            Item item = itemList.get(index);
 
             // Get description of item found
             String itemDescription = item.getDescription();
 
             // Print item name and description
-            ui.showOutput("Name of Item: " + itemName + System.lineSeparator()
+            ui.showOutput("Name of Item: " + item.toString() + System.lineSeparator()
                     + "Description: " + itemDescription);
         } catch (InvMgrException e) {
             ui.showOutput("Please enter another search phrase");
--- src/main/java/seedu/duke/data/ItemList.java
+++ src/main/java/seedu/duke/data/ItemList.java
@@ -18,21 +18,4 @@ public class ItemList {
     public Item removeItem(int index) {
         return this.itemList.remove(index);
     }
-
-    /**
-     * Returns the item from the current inventory that contains the search term.
-     *
-     * @param searchTerm User input of search term.
-     * @return Item from current inventory that contains the search term.
-     * @throws InvMgrException If current inventory does not contain the search term in item name
-     */
-    public Item contains(String searchTerm) throws InvMgrException {
-        for (Item item : itemList) {
-            if (item.contains(searchTerm)) {
-                return item;
-            }
-        }
-
-        throw new InvMgrException("Current inventory does not contain this item.");
-    }
 }
