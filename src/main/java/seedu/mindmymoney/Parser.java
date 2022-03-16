33c39f946a84d7539faeebe2f0c55cb5a27ada81
limjierui <jieruilim@u.nus.edu>
2022-03-16 19:55:51 +0800

    Add Bye Command

--- src/main/java/seedu/mindmymoney/Parser.java
+++ src/main/java/seedu/mindmymoney/Parser.java
@@ -1,11 +1,6 @@
 package seedu.mindmymoney;
 
-import seedu.mindmymoney.command.Command;
-import seedu.mindmymoney.command.HelpCommand;
-import seedu.mindmymoney.command.UpdateCommand;
-import seedu.mindmymoney.command.AddCommand;
-import seedu.mindmymoney.command.ListCommand;
-import seedu.mindmymoney.command.DeleteCommand;
+import seedu.mindmymoney.command.*;
 import seedu.mindmymoney.data.ExpenditureList;
 import seedu.mindmymoney.helper.Functions;
 
@@ -32,9 +27,7 @@ public class Parser {
             case "help":
                 return new HelpCommand(true);
             case "bye":
-                System.out.println("Goodbye!");
-                System.exit(0);
-                return new HelpCommand(false); //solving fall through issue, need return something leh
+                return new ByeCommand();
             case "add":
                 return new AddCommand(parsedInput[INDEX_OF_SECOND_ITEM_IN_STRING], itemList);
             case "update":
