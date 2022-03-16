408ab925e0e57448fa900706a6d9da721b9e130b
quitejasper <jasper.chia9@gmail.com>
2022-03-16 22:31:24 +0800

    Update JavaDoc
    Added documentation for AddCommand Class.

--- src/main/java/seedu/duke/commands/AddCommand.java
+++ src/main/java/seedu/duke/commands/AddCommand.java
@@ -6,9 +6,16 @@ import seedu.duke.data.Item;
 import seedu.duke.data.ItemList;
 import seedu.duke.ui.Ui;
 
+/**
+ * Adds an item to the item list.
+ */
 public class AddCommand extends Command {
     private final Item itemToAdd;
 
+    /**
+     * Creates an AddCommand to add the specified {@code Item}.
+     * @param itemToAdd The specified item.
+     */
     public AddCommand(Item itemToAdd) {
         requireNonNull(itemToAdd, "item should not be null!");
         this.itemToAdd = itemToAdd;
