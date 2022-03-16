04159e22c4759a5fe0c9a09138f3eeef5b3cdb97
khseah <seahkithan55@gmail.com>
2022-03-16 22:50:17 +0800

    Edit runtest.sh file

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -8,8 +8,6 @@ cd ..
 
 cd text-ui-test
 
-java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt > ACTUAL.TXT
-
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
 dos2unix EXPECTED-UNIX.TXT ACTUAL.TXT
 diff EXPECTED-UNIX.TXT ACTUAL.TXT
