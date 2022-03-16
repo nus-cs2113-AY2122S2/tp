00604e960c5a718fe678f521ff3ab5fc49b38144
RenTianle <lele.rer@outlook.com>
2022-03-17 01:11:53 +0800

    Set up Group class

---
+++ src/main/java/seedu/splitlah/data/Group.java
@@ -0,0 +1,57 @@
+package seedu.splitlah.data;
+
+import seedu.splitlah.exceptions.InvalidDataException;
+import seedu.splitlah.ui.Message;
+
+import java.util.ArrayList;
+
+
+public class Group {
+
+    private ArrayList<Person> personList;
+
+    public Group(ArrayList<Person> personList) {
+        this.personList = personList;
+    }
+
+    public ArrayList<Person> getPersonList() {
+        return personList;
+    }
+
+    public boolean isEmptyGroup() {
+        return personList.isEmpty();
+    }
+
+    public void addPerson(Person person) {
+        personList.add(person);
+    }
+
+    public boolean hasPerson(Person person) {
+        if (personList.isEmpty()) {
+            return false;
+        }
+
+        for (Person p : personList) {
+            if (p.getName() == person.getName()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void removePerson(Person person) throws InvalidDataException {
+        if (personList.isEmpty()) {
+            throw new InvalidDataException(Message.ERROR_GROUP_EMPTY_PERSON_LIST);
+        }
+        Person deleteTarget = null;
+        for (Person p : personList) {
+            if (p.getName() == person.getName()) {
+                deleteTarget = p;
+                break;
+            }
+        }
+        if (deleteTarget == null) {
+            throw new InvalidDataException(Message.ERROR_GROUP_PERSON_NOT_IN_LIST);
+        }
+    }
+}
