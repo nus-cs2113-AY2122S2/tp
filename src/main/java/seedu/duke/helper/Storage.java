a58363d7f27b0eb8df26b7cbfc0feec0656df4b3
shxr3f <e0540641@u.nus.edu>
2022-03-16 12:18:04 +0800

    edited saveMedicineData method

--- src/main/java/seedu/duke/helper/Storage.java
+++ src/main/java/seedu/duke/helper/Storage.java
@@ -1,8 +1,14 @@
 package seedu.duke.helper;
 
 import seedu.duke.assets.DoctorList;
+import seedu.duke.assets.Medicine;
 import seedu.duke.assets.MedicineList;
 import seedu.duke.assets.PatientList;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.SQLOutput;
 
 public class Storage {
     private final String PATH_DOC = "data/doctor.txt";
@@ -28,6 +34,24 @@ public class Storage {
     }
 
     private void saveMedicineData() {
+        File medicineFile = new File(PATH_MED);
+        if (!medicineFile.exists()) {
+            try {
+                medicineFile.createNewFile();
+            } catch (IOException ioException) {
+                System.out.println("medicine.txt cannot be created");
+                return;
+            }
+        }
+        try {
+            FileWriter dataWrite = new FileWriter(PATH_MED,false);
+            for(Medicine m : ) {
+                dataWrite.write(a.saveString() + "\n");
+            }
+            dataWrite.close();
+        } catch (IOException e) {
+            System.out.println("Unable to save data...");
+        }
     }
 
     private void savePatientData() {
