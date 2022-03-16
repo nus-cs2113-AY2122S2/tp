d5b08f25dfcd89517369e2d6c758321549d9b572
yuzhongng <yuzhongng@gmail.com>
2022-03-16 22:16:23 +0800

    Add ReminderTest to test the method in the Reminder class

---
+++ src/test/java/seedu/sherpass/util/ReminderTest.java
@@ -0,0 +1,12 @@
+package seedu.sherpass.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReminderTest {
+
+    @Test
+    void showDailyTask() {
+    }
+}
