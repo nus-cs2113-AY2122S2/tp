adb2ff649c67a06a961b9027b34364c68383e85f
Haofeng <walterhuang123@gmail.com>
2022-03-16 16:14:54 +0800

    Second attempt to fix Java CI issue

--- src/main/java/werkit/Parser.java
+++ src/main/java/werkit/Parser.java
@@ -202,7 +202,8 @@ public class Parser {
         case LIST_ACTION_KEYWORD:
             if (userInput.split(" ", -1).length > EXPECTED_NUMBER_OF_PARAMETERS_NO_ARGUMENTS) {
                 logger.log(Level.WARNING, "User has entered an invalid list exercise command action.");
-                throw new InvalidCommandException(className, InvalidCommandException.INVALID_EXERCISE_LIST_COMMAND_ERROR_MSG);
+                throw new InvalidCommandException(className,
+                        InvalidCommandException.INVALID_EXERCISE_LIST_COMMAND_ERROR_MSG);
             }
             break;
         default:
