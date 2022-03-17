b663a063ed3b3dc2b701fe7539e505ef544eb7cd
Roy Tang <roy_tql@hotmail.com>
2022-03-17 12:23:04 +0800

    Add BeforeEach method for ProfileTest class

--- src/test/java/seedu/splitlah/data/ProfileTest.java
+++ src/test/java/seedu/splitlah/data/ProfileTest.java
@@ -1,4 +1,13 @@
 import static org.junit.jupiter.api.Assertions.*;
 class ProfileTest {
   
+    @BeforeEach
+    void setUp() {
+        String sessionOneArgs = "session /create /n Class outing /d 15-02-2022 /pl Alice Bob";
+        String sessionTwoArgs = "session /create /n Family gathering /d 20-02-2022  /pl Eves Mallory";
+        Command createSessionOne = Parser.getCommand(sessionOneArgs);
+        createSessionOne.run(manager);
+        Command createSessionTwo = Parser.getCommand(sessionTwoArgs);
+        createSessionTwo.run(manager);
+    }
 }
