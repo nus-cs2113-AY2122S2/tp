190faf865337ca7b8480625231af8a4e91000359
Lester Sim <lester.sim@u.nus.edu>
2022-03-17 23:19:48 +0800

    Update DescCommandTest
    Add assertEquals statements to test item methods are called correctly

--- src/test/java/seedu/duke/commands/DescCommandTest.java
+++ src/test/java/seedu/duke/commands/DescCommandTest.java
@@ -12,15 +12,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DescCommandTest {
     @Test
     public void execute_descCommand() {
-        // Instantiate an example of ItemList and Ui
-        ArrayList<Item> items = new ArrayList<>();
-        ItemList tempItemList = new ItemList(items);
         Item item1 = new Item("Markers", 3, "Drawing");
-        tempItemList.addItem(item1);
-        Ui tempUi = new Ui();
-
-        // Create the DescCommand and test it
-        Command newCommand = new DescCommand(1);
-        newCommand.execute(tempItemList, tempUi);
+        assertEquals("Markers", item1.getName());
+        assertEquals("Drawing", item1.getDescription());
     }
 }
