17fdf781a4a394bb3ed3c21c53d1526c699a51f2
1szheng <wansizheng@gmail.com>
2022-03-17 14:32:58 +0800

    Add JUnit test for parseName

--- src/test/java/seedu/planitarium/parser/ParserTest.java
+++ src/test/java/seedu/planitarium/parser/ParserTest.java
@@ -48,6 +48,45 @@ class ParserTest {
             assertEquals("User input should not be null", e.getMessage());
         }
     }
+
+    @Test
+    void parseName_delimiterExist_success() throws DuplicateDelimiterException, MissingDelimiterException {
+        String input = "add /n bill";
+        String output = Parser.parseName(input);
+        assertEquals("bill", output);
+    }
+
+    @Test
+    void parseName_nullInput_assertThrown() {
+        try {
+            Parser.parseKeyword(null);
+            fail();
+        } catch (AssertionError e) {
+            assertEquals("User input should not be null", e.getMessage());
+        }
+    }
+
+    @Test
+    void parseName_delimiterIssues_exceptionThrown() {
+        try {
+            String noDelimiter = "add bill";
+            Parser.parseName(noDelimiter);
+            fail();
+        } catch (MissingDelimiterException e) {
+            assertEquals("Missing delimiter `/n`", e.getMessage());
+        } catch (DuplicateDelimiterException e) {
+            fail();
+        }
+        try {
+            String tooManyDelimiter = "add /n bill /n alice";
+            Parser.parseName(tooManyDelimiter);
+            fail();
+        } catch (DuplicateDelimiterException e) {
+            assertEquals("Too many delimiter `/n`", e.getMessage());
+        } catch (MissingDelimiterException e) {
+            fail();
+        }
+    }
     @Test
     void parseKeyword() {
     }
