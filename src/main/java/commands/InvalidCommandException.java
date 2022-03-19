0244e98964fab9d438c2bbca8e91b231c37f0a3d
Haofeng <walterhuang123@gmail.com>
2022-03-19 13:46:09 +0800

    Add InvalidCommandException message for search exercise command

--- src/main/java/commands/InvalidCommandException.java
+++ src/main/java/commands/InvalidCommandException.java
@@ -18,6 +18,8 @@ public class InvalidCommandException extends Exception {
             + "Do you mean 'workout /delete <workout number to delete>'?";
     public static final String INVALID_UPDATE_WORKOUT_COMMAND_ERROR_MSG = "Uh oh, the command entered is wrong.\n"
             + "Do you mean 'workout /update <workout number to update> <new number of reps>'?";
+    public static final String INVALID_SEARCH_EXERCISE_COMMAND_ERROR_MSG = "Uh oh, the command entered is wrong.\n"
+            + "Do you mean 'search /exercise <exercise keyword>'?";
 
     private String throwingClass;
 
