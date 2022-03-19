9d91265dac3a6d0b16dd4c6a7af4eb32a2a9c76b
Haofeng <walterhuang123@gmail.com>
2022-03-20 04:00:43 +0800

    Update UI to capture new messages to be printed

--- src/main/java/werkit/UI.java
+++ src/main/java/werkit/UI.java
@@ -45,11 +45,15 @@ public class UI {
     public static final String WORKOUT_FILE_NOT_FOUND_MSG = "- The workout file was not found. It will be created.";
     public static final String WORKOUT_FILE_CREATED_MSG = "- The workout file 'workouts.txt' has been created in\n"
             + "  the WerkIt! resource directory.";
+    public static final String PLAN_FILE_NOT_FOUND_MSG = "- The plan file was not found. It will be created.";
+    public static final String PLAN_FILE_CREATED_MSG = "- The plan file 'plans.txt' has been created in\n"
+            + "  the WerkIt! resource directory.";
     public static final String FILE_LOAD_OK = "OK!";
     public static final String FILE_LOAD_NOT_OK = "Not OK...";
     public static final String LOADING_FILE_DATA_MSG = "Loading saved file data...";
     public static final String EXERCISES_FILE_LOADED_MSG =  "- Exercises file\t%s\n";
     public static final String WORKOUTS_FILE_LOADED_MSG = "- Workouts file \t%s\n";
+    public static final String PLANS_FILE_LOADED_MSG = "- Plans file \t%s\n";
     // Workout-related messages
     public static final String NEW_WORKOUT_CREATED_MESSAGE = "Alright, the following workout has been created:";
     public static final String DELETED_WORKOUT_MESSAGE = "Alright, the following workout has been removed:";
@@ -317,6 +321,9 @@ public class UI {
         case FileManager.WORKOUT_FILENAME:
             messageToPrint = WORKOUTS_FILE_LOADED_MSG;
             break;
+        case FileManager.PLAN_FILENAME:
+            messageToPrint = PLANS_FILE_LOADED_MSG;
+            break;
         default:
             logger.log(Level.WARNING, "Unknown filename encountered.");
             throw new UnknownFileException(filename, UnknownFileException.UNKNOWN_FILE_MSG);
@@ -374,11 +381,20 @@ public class UI {
         System.out.println(WORKOUT_FILE_NOT_FOUND_MSG);
     }
 
+    public void printPlanFileNotFoundMessage() {
+        System.out.println();
+        System.out.println(PLAN_FILE_NOT_FOUND_MSG);
+    }
+
     /**
      * Prints a message to tell the user that the workout file has been successfully created.
      */
     public void printWorkoutFileCreatedMessage() {
         System.out.println(WORKOUT_FILE_CREATED_MSG);
+    }
+
+    public void printPlanFileCreatedMessage() {
+        System.out.println(PLAN_FILE_CREATED_MSG);
     }
 
     /**
