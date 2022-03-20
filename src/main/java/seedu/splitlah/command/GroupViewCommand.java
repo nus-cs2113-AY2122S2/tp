bf2c8334f753981384c9631e6abe058e780d9b24
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-20 19:09:56 +0800

    Add necessary constants and attributes to GroupViewCommand class

--- src/main/java/seedu/splitlah/command/GroupViewCommand.java
+++ src/main/java/seedu/splitlah/command/GroupViewCommand.java
@@ -1,8 +1,20 @@
 package seedu.splitlah.command;
 
 import seedu.splitlah.data.Manager;
+import seedu.splitlah.parser.Parser;
 
 public class GroupViewCommand extends Command {
+
+    public static final String COMMAND_TEXT = "group /view";
+
+    public static final String COMMAND_FORMAT = "Syntax: group /view /gid [GROUP_ID]";
+
+    public static final String[] COMMAND_DELIMITERS = {
+            Parser.GROUP_ID_DELIMITER
+    };
+
+    private int groupId;
+
     @Override
     public void run(Manager manager) {
         
