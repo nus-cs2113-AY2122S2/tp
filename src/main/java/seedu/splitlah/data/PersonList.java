91d8e048c3951ca4b92677cf5c75996d704bad1e
Roy Tang <roy_tql@hotmail.com>
2022-03-20 15:10:16 +0800

    Add support functions for PersonList class

--- src/main/java/seedu/splitlah/data/PersonList.java
+++ src/main/java/seedu/splitlah/data/PersonList.java
@@ -1,2 +1,34 @@
 package seedu.splitlah.data;public class PersonList {
+    private ArrayList<Person> personList;
+    public PersonList() {
+        this.personList = new ArrayList<>();
+    }
+    public int getSize() {
+        return personList.size();
+    }
+    public boolean isEmpty() {
+        return personList.isEmpty();
+    }
+    public Person getPerson(int index) {
+        return personList.get(index);
+    }
+    public void addPerson(Person person) {
+        if (!personList.contains(person)) {
+            personList.add(person);
+        }
+    }
+    public ArrayList<Person> getPersonList() {
+        return personList;
+    }
+    public void convertToPersonList(String[] personNames) {
+        for (String name : personNames) {
+            Person newPerson = new Person(name);
+            addPerson(newPerson);
+        }
+    }
+    public void mergeListOfPersons(ArrayList<Person> groupPersonList) {
+        for (Person person : groupPersonList) {
+            addPerson(person);
+        }
+    }
 }
