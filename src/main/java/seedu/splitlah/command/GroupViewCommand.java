1329c11d78ae7eff05f59e658a178d87e1873aa2
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-20 19:07:06 +0800

    Add GroupViewCommand class

---
+++ src/main/java/seedu/splitlah/command/GroupViewCommand.java
@@ -0,0 +1,10 @@
+package seedu.splitlah.command;
+
+import seedu.splitlah.data.Manager;
+
+public class GroupViewCommand extends Command {
+    @Override
+    public void run(Manager manager) {
+        
+    }
+}
