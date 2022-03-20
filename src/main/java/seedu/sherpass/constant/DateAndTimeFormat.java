f5bbae93584cc567a1e8a4751d43cf7194092732
JL <tha.junlim@gmail.com>
2022-03-20 19:14:40 +0800

    Add new DateTimeFormatters used in Timetable class

--- src/main/java/seedu/sherpass/constant/DateAndTimeFormat.java
+++ src/main/java/seedu/sherpass/constant/DateAndTimeFormat.java
@@ -3,7 +3,8 @@ package seedu.sherpass.constant;
 import java.time.format.DateTimeFormatter;
 
 public class DateAndTimeFormat {
-    public static final DateTimeFormatter parseFormat = DateTimeFormatter.ofPattern("d/M/yyyy");
     public static final DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("d/M/yyyy");
     public static final DateTimeFormatter outputFormat = DateTimeFormatter.ofPattern("EEE, dd/MM/yyyy");
+    public static final DateTimeFormatter dayOnlyFormat = DateTimeFormatter.ofPattern("EEE");
+    public static final DateTimeFormatter dateOnlyFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 }
