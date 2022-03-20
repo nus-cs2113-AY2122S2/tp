b7f755ec94396045c783b0469158d23a9e3fe0bb
JL <tha.junlim@gmail.com>
2022-03-20 19:14:09 +0800

    Rename DateAndTimeFormatter parseFormat to inputFormat

--- src/main/java/seedu/sherpass/constant/DateAndTimeFormat.java
+++ src/main/java/seedu/sherpass/constant/DateAndTimeFormat.java
@@ -4,5 +4,6 @@ import java.time.format.DateTimeFormatter;
 
 public class DateAndTimeFormat {
     public static final DateTimeFormatter parseFormat = DateTimeFormatter.ofPattern("d/M/yyyy");
+    public static final DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("d/M/yyyy");
     public static final DateTimeFormatter outputFormat = DateTimeFormatter.ofPattern("EEE, dd/MM/yyyy");
 }
