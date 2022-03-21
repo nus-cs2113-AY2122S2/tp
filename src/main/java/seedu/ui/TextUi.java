4e92084993321fc7b9f6c438dd650823fb05aa73
yuhaochua <yuhaochua99@gmail.com>
2022-03-21 22:57:52 +0800

    Add UI class to have UI displaying messages to users.
    Fixes #36

---
+++ src/main/java/seedu/ui/TextUi.java
@@ -0,0 +1,42 @@
+package seedu.ui;
+
+import seedu.command.Command;
+import seedu.command.CommandResult;
+import seedu.equipment.Equipment;
+
+/**
+ * Text UI for the application, referenced from
+ * https://github.com/se-edu/addressbook-level2/blob/master/src/seedu/addressbook/ui/TextUi.java
+ */
+public class TextUi {
+    private final static String LOGO = "                  _                            _                                               \n" +
+            "                  (_)                          | |                                              \n" +
+            "   ___  __ _ _   _ _ _ __  _ __ ___   ___ _ __ | |_ _ __ ___   __ _ _ __   __ _  __ _  ___ _ __ \n" +
+            "  / _ \\/ _` | | | | | '_ \\| '_ ` _ \\ / _ \\ '_ \\| __| '_ ` _ \\ / _` | '_ \\ / _` |/ _` |/ _ \\ '__|\n" +
+            " |  __/ (_| | |_| | | |_) | | | | | |  __/ | | | |_| | | | | | (_| | | | | (_| | (_| |  __/ |   \n" +
+            "  \\___|\\__, |\\__,_|_| .__/|_| |_| |_|\\___|_| |_|\\__|_| |_| |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|   \n" +
+            "          | |       | |                                                          __/ |          \n" +
+            "          |_|       |_|                                                         |___/";
+
+    public void showWelcomeMessage() {
+        System.out.println("Hello from" + System.lineSeparator() + LOGO);
+        System.out.println("What do you want to do?");
+    }
+
+    /**
+     * Shows the result of command execution to user.
+     * @param result CommandResult returned from the execution of a command.
+     */
+    public void showResultToUser(CommandResult result) {
+        System.out.println(result.getResultToShow());
+        int indexShown = 1;
+        if  (result.getRelevantEquipment() != null) {
+            for (Equipment equipment : result.getRelevantEquipment()) {
+                System.out.print(indexShown + ". ");
+                System.out.println(equipment);
+                indexShown++;
+            }
+        }
+    }
+
+}
