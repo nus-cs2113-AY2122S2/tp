418012dfbe8a5bfafb9878471525db43c1d7ebc4
tuan0369 <tuannguyen111202@gmail.com>
2022-03-21 21:53:50 +0800

    Update help

--- src/main/java/seedu/duke/Commands.java
+++ src/main/java/seedu/duke/Commands.java
@@ -88,14 +88,14 @@ public class Commands {
     }
 
     public static void help() {
-        System.out.println("* Add order `add -o id/ID r/RECEIVER a/SHIPPING_ADDRESS`\n"
-                + "* Add good `add -g oid/ORDER_ID gid/GOOD_ID n/NAME q/QUANTITY d/DESCRIPTION`\n"
-                + "* Remove order: `remove -o i/ID`\n"
-                + "* Remove good: `remove -g i/ID q/QUANTITY`\n"
-                + "* List all orders: `list -o`\n"
-                + "* List all goods: `list -g`\n"
-                + "* View order: `view -o id/ORDER_ID`\n"
-                + "* View good: `view -g id/GOOD_ID`\n"
+        System.out.println("* Add order `add o/ id/ID r/RECEIVER a/SHIPPING_ADDRESS`\n"
+                + "* Add good `add g/ oid/ORDER_ID gid/GOOD_ID n/NAME q/QUANTITY d/DESCRIPTION`\n"
+                + "* Remove order: `remove o/ i/ID`\n"
+                + "* Remove good: `remove g/ i/ID q/QUANTITY`\n"
+                + "* List all orders: `list o/`\n"
+                + "* List all goods: `list g/`\n"
+                + "* View order: `view o/ id/ORDER_ID`\n"
+                + "* View good: `view g/ id/GOOD_ID`\n"
                 + "* Total quantity of goods: `total`");
     }
 
