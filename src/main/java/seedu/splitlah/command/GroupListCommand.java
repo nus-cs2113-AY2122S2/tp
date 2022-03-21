e59b0662e773c2a5ed6668a48122a48b83324993
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-21 16:22:55 +0800

    Add GroupListCommand class

---
+++ src/main/java/seedu/splitlah/command/GroupListCommand.java
@@ -0,0 +1,10 @@
+package seedu.splitlah.command;
+
+import seedu.splitlah.data.Manager;
+
+public class GroupListCommand extends Command {
+    @Override
+    public void run(Manager manager) {
+        
+    }
+}
