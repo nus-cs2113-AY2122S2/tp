9060c74d3baaff9292b0b5366b476de4b72e6df9
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-21 17:22:14 +0800

    Update run method in GroupListCommand class

--- src/main/java/seedu/splitlah/command/GroupListCommand.java
+++ src/main/java/seedu/splitlah/command/GroupListCommand.java
@@ -1,6 +1,10 @@
 package seedu.splitlah.command;
 
+import seedu.splitlah.data.Group;
 import seedu.splitlah.data.Manager;
+import seedu.splitlah.ui.Message;
+
+import java.util.ArrayList;
 
 public class GroupListCommand extends Command {
 
@@ -8,8 +12,24 @@ public class GroupListCommand extends Command {
 
     public static final String COMMAND_FORMAT = "Syntax: group /list";
 
+    private static final String GROUP_LIST_HEADER = "List of Groups";
+
     @Override
     public void run(Manager manager) {
+        /*
+        ArrayList<Group> groupsToBePrinted = manager.getProfile().getGroupList();
+        if (groupsToBePrinted.isEmpty()) {
+            manager.getUi().printlnMessage(Message.ERROR_PROFILE_SESSION_LIST_EMPTY);
+            return;
+        }
 
+        manager.getUi().printlnMessageWithDashDivider(GROUP_LIST_HEADER);
+        int groupListSize = groupsToBePrinted.size();
+        for (int i = 0; i < groupListSize - 1; i++) {
+            manager.getUi().printlnMessage(groupsToBePrinted.get(i).getGroupSimplifiedString());
+        }
+        String lastGroupToPrint = groupsToBePrinted.get(groupListSize - 1).getGroupSimplifiedString();
+        manager.getUi().printlnMessageWithDivider(lastGroupToPrint);
+         */
     }
 }
