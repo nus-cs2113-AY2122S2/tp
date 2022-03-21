649d6545d28fabfd7abcafecd8dc599b9aebaf81
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-21 16:25:06 +0800

    Add constants to GroupListCommand class

--- src/main/java/seedu/splitlah/command/GroupListCommand.java
+++ src/main/java/seedu/splitlah/command/GroupListCommand.java
@@ -3,8 +3,13 @@ package seedu.splitlah.command;
 import seedu.splitlah.data.Manager;
 
 public class GroupListCommand extends Command {
+
+    public static final String COMMAND_TEXT = "group /list";
+
+    public static final String COMMAND_FORMAT = "Syntax: group /list";
+
     @Override
     public void run(Manager manager) {
-        
+
     }
 }
