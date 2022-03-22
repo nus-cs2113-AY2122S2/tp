39b8fe16f859ef523abd9b32ac2ca5a7bc18ff26
Roy Tang <roy_tql@hotmail.com>
2022-03-22 16:12:21 +0800

    Add functions to support storage in Manager class

--- src/main/java/seedu/splitlah/data/Manager.java
+++ src/main/java/seedu/splitlah/data/Manager.java
@@ -34,6 +34,40 @@ public class Manager {
         this.isUsingStorage = isUsingStorage;
         initializeStorage();
     }
+    private void initializeStorage() {
+        storage = new Storage();
+        boolean isDirectoryCreated = storage.hasDataDirectory();
+        boolean isFileCreated = storage.hasDataFile();
+        if (!isDirectoryCreated || !isFileCreated) {
+            ui.printlnMessage(Message.ERROR_STORAGE_PATH_LOCATION_CREATION_FAILED);
+            profile = new Profile();
+            isUsingStorage = false;
+        } else {
+            loadFileSave();
+        }
+    }
+    private void loadFileSave() {
+        try {
+            storage.loadStorage();
+        } catch (IOException ioException) {
+            logger.log(Level.FINEST, Message.LOGGER_STORAGE_FILE_ERROR);
+            ui.printlnMessage(Message.ERROR_STORAGE_FILE_NOT_FOUND);
+        } catch (ClassNotFoundException classNotFoundException) {
+            logger.log(Level.FINEST,Message.LOGGER_STORAGE_CLASS_NOT_FOUND);
+            ui.printlnMessage(Message.ERROR_STORAGE_CLASS_EXCEPTION_ISSUE);
+        }
+        profile = storage.getProfile();
+    }
+    public void saveProfile() {
+        if (isUsingStorage) {
+            try {
+                storage.saveProfileToFile(profile);
+            } catch (IOException exception) {
+                ui.printlnMessage(Message.ERROR_STORAGE_DATA_NOT_SAVED);
+            }
+        }
+    }
+
     /**
      * Returns a TextUI object for user inputs and outputs.
      *
