0725ebe3014d98033f2314bd937b0f507f12da0a
Roy Tang <roy_tql@hotmail.com>
2022-03-22 16:07:48 +0800

    Set up attributes and constructor for Storage class

--- src/main/java/seedu/splitlah/storage/Storage.java
+++ src/main/java/seedu/splitlah/storage/Storage.java
@@ -1,2 +1,24 @@
-package seedu.splitlah.storage;public class Storage {
+package seedu.splitlah.storage;
+
+import seedu.splitlah.data.Profile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Storage {
+
+    private static final String FILE_DIRECTORY = "data/";
+    private static final String FILE_NAME = "SplitLah.data";
+    private static final String FILE_FULL_PATH = FILE_DIRECTORY + FILE_NAME;
+
+    private Profile profile;
+
+    public Storage() {
+        profile = new Profile();
+    }
+    }
 }
