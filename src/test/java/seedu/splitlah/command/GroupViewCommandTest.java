87788cc69f71a6ffe8f5e424e5abaadee1f88e7d
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-22 12:25:05 +0800

    Add test regarding missing group unique identifier delimiter

--- src/test/java/seedu/splitlah/command/GroupViewCommandTest.java
+++ src/test/java/seedu/splitlah/command/GroupViewCommandTest.java
@@ -1,6 +1,7 @@
 package seedu.splitlah.command;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import seedu.splitlah.data.Manager;
 import seedu.splitlah.parser.Parser;
 
@@ -15,5 +16,12 @@ class GroupViewCommandTest {
         String groupArgs = "group /view /gid 1";
         Command createGroup = Parser.getCommand(groupArgs);
         createGroup.run(manager);
+    }
+
+    @Test
+    void prepare_MissingGidDelimiter_InvalidCommand() {
+        String argMissingGidDelimiter = "group /view 1";
+        Command groupWithMissingGidDelimiter = Parser.getCommand(argMissingGidDelimiter);
+        assertEquals(InvalidCommand.class, groupWithMissingGidDelimiter.getClass());
     }
 }
