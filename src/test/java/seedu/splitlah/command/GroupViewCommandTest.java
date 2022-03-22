5927416e7bbee724eb14444afc7dbc4b3aa0104e
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-22 12:22:03 +0800

    Update setUp method in GroupViewCommandTest

--- src/test/java/seedu/splitlah/command/GroupViewCommandTest.java
+++ src/test/java/seedu/splitlah/command/GroupViewCommandTest.java
@@ -1,12 +1,19 @@
 package seedu.splitlah.command;
 
 import org.junit.jupiter.api.BeforeEach;
+import seedu.splitlah.data.Manager;
+import seedu.splitlah.parser.Parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GroupViewCommandTest {
 
+    Manager manager = new Manager();
+
     @BeforeEach
     void setUp() {
+        String groupArgs = "group /view /gid 1";
+        Command createGroup = Parser.getCommand(groupArgs);
+        createGroup.run(manager);
     }
 }
