f8bd4d0cf609828d443e3f0c86c02bba5918ca76
warrencxw <cxw.warren@gmail.com>
2022-03-23 12:09:21 +0800

    Add new JUnit testing class for HelpCommandParser

---
+++ src/test/java/seedu/splitlah/parser/commandparser/HelpCommandParserTest.java
@@ -0,0 +1,16 @@
+package seedu.splitlah.parser.commandparser;
+
+import org.junit.jupiter.api.Test;
+import seedu.splitlah.command.Command;
+import seedu.splitlah.command.HelpCommand;
+import seedu.splitlah.command.InvalidCommand;
+import seedu.splitlah.data.Manager;
+import seedu.splitlah.parser.Parser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HelpCommandParserTest {
+    
+    Manager manager = new Manager();
+
+}
