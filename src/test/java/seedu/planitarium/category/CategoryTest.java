b0b20033e29b4c2252a477ea17e7fc7629e28a51
1szheng <wansizheng@gmail.com>
2022-03-23 17:22:10 +0800

    Add JUnit test skeleton for category

---
+++ src/test/java/seedu/planitarium/category/CategoryTest.java
@@ -0,0 +1,29 @@
+package seedu.planitarium.category;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class CategoryTest {
+
+    @Test
+    void getLabel() {
+    }
+
+    @Test
+    void getAllLabels() {
+    }
+
+    @Test
+    void getLabelForIndex() {
+    }
+
+    @Test
+    void values() {
+    }
+
+    @Test
+    void valueOf() {
+    }
+}
