614e97613c55a7fb4f6a68788d4b11b07fc62fa1
warrencxw <cxw.warren@gmail.com>
2022-03-23 12:11:26 +0800

    Add two JUnit tests for getCommand() method

--- src/test/java/seedu/splitlah/parser/commandparser/HelpCommandParserTest.java
+++ src/test/java/seedu/splitlah/parser/commandparser/HelpCommandParserTest.java
@@ -13,4 +13,38 @@ class HelpCommandParserTest {
     
     Manager manager = new Manager();
 
+    @Test
+    void getCommand_validUserInput_HelpCommand() {
+        String validUserInput = "help";
+        Command command = Parser.getCommand(validUserInput);
+        assertEquals(HelpCommand.class, command.getClass());
+    }
+
+    @Test
+    void getCommand_additionalArguments_InvalidCommand() {
+        // Single additional token, no delimiters
+        String helpWithAdditionalArguments = "help apple";
+        Command command = Parser.getCommand(helpWithAdditionalArguments);
+        assertEquals(InvalidCommand.class, command.getClass());
+
+        // Single additional token, with delimiters
+        helpWithAdditionalArguments = "help /apple";
+        command = Parser.getCommand(helpWithAdditionalArguments);
+        assertEquals(InvalidCommand.class, command.getClass());
+
+        // Two additional tokens, without delimiters
+        helpWithAdditionalArguments = "help apple orange";
+        command = Parser.getCommand(helpWithAdditionalArguments);
+        assertEquals(InvalidCommand.class, command.getClass());
+
+        // Two additional tokens, one delimiters
+        helpWithAdditionalArguments = "help /apple orange";
+        command = Parser.getCommand(helpWithAdditionalArguments);
+        assertEquals(InvalidCommand.class, command.getClass());
+
+        // Three additional tokens, two delimiters
+        helpWithAdditionalArguments = "help /apple /sid 1";
+        command = Parser.getCommand(helpWithAdditionalArguments);
+        assertEquals(InvalidCommand.class, command.getClass());
+    }
 }
