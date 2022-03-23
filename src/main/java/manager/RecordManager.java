737ce3554209350c1685a4586d8eb6c4e7ba3c9f
allyfern72 <79740655+allyfern72@users.noreply.github.com>
2022-03-24 03:24:18 +0800

    Add JavaDoc to RecordManager

--- src/main/java/manager/RecordManager.java
+++ src/main/java/manager/RecordManager.java
@@ -5,15 +5,11 @@ import records.Record;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Manager class that is able to store, edit, and access a list of records.
- */
+/** Manager class that is able to store, edit, and access a list of records. */
 public class RecordManager {
     private List<Record> records;
 
-    /**
-     * Constructs a <code>RecordManager</code> with an empty list of records.
-     */
+    /** Constructs a <code>RecordManager</code> with an empty list of records. */
     public RecordManager() {
         records = new ArrayList<>();
     }
@@ -21,7 +17,7 @@ public class RecordManager {
     /**
      * Adds a record to the end of the record list.
      *
-     * @param newRecord Record to add to record list.
+     * @param newRecord Record to add to record list
      */
     public void addRecord(Record newRecord) {
         records.add(newRecord);
@@ -30,8 +26,8 @@ public class RecordManager {
     /**
      * Removes the record at specified index.
      *
-     * @param index Index (in <code>records</code>) of the record to remove.
-     * @throws IndexOutOfBoundsException invalid index is given.
+     * @param index Index (in <code>records</code>) of the record to remove
+     * @throws IndexOutOfBoundsException invalid index is given
      */
     public void deleteRecord(int index) throws IndexOutOfBoundsException{
         try {
@@ -52,10 +48,17 @@ public class RecordManager {
         }
     }
 
+    /**
+     * @return List of records
+     */
     public List<Record> getAllRecords() {
         return records;
     }
 
+    /**
+     * @param index Index (in <code>records</code>) of the record to retrieve
+     * @return Record to retrieve
+     */
     public Record getRecordByIndex(int index) {
         return records.get(index);
     }
