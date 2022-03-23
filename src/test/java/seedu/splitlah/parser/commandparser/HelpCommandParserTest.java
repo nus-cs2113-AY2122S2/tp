7dd101fad064c295ef848056203175f7418ea975
warrencxw <cxw.warren@gmail.com>
2022-03-23 12:24:30 +0800

    Remove unnecessary dependencies

--- src/test/java/seedu/splitlah/parser/commandparser/HelpCommandParserTest.java
+++ src/test/java/seedu/splitlah/parser/commandparser/HelpCommandParserTest.java
@@ -4,14 +4,11 @@ import org.junit.jupiter.api.Test;
 import seedu.splitlah.command.Command;
 import seedu.splitlah.command.HelpCommand;
 import seedu.splitlah.command.InvalidCommand;
-import seedu.splitlah.data.Manager;
 import seedu.splitlah.parser.Parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HelpCommandParserTest {
-    
-    Manager manager = new Manager();
 
     /**
      * Checks if a HelpCommand object is correctly returned when the command is correctly entered.
