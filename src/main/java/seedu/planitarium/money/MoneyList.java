c00e95c46d7aab180e606f33cefd675f83db900e
tjiarong <jia8991@gmail.com>
2022-03-23 19:37:32 +0800

    Created constructor for super class

--- src/main/java/seedu/planitarium/money/MoneyList.java
+++ src/main/java/seedu/planitarium/money/MoneyList.java
@@ -6,4 +6,15 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 
 public abstract class MoneyList {
+
+    private ArrayList<Money> moneyArrayList;
+
+    /**
+     * Creates a new Money Object.
+     */
+    public MoneyList() {
+        this.moneyArrayList = new ArrayList<>();
+        logger = new ProjectLogger(LOG_CLASS_NAME, LOG_FILE_PATH);
+        logger.getLogger().log(Level.INFO, LOG_INIT);
+    }
 }
