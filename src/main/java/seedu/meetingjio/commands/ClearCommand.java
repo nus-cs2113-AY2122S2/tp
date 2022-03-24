95d894b595691e7f38d48ebb790d31d7d93e60fa
ibrahimisramos <64336014+ibrahimisramos@users.noreply.github.com>
2022-03-24 19:54:50 +0800

    add delete commands

--- src/main/java/seedu/meetingjio/commands/ClearCommand.java
+++ src/main/java/seedu/meetingjio/commands/ClearCommand.java
@@ -1,11 +1,12 @@
+//@@author ibrahimisramos
 package seedu.meetingjio.commands;
 
 import seedu.meetingjio.timetables.MasterTimetable;
 import seedu.meetingjio.timetables.Timetable;
 
 import static seedu.meetingjio.common.ErrorMessages.ERROR_NON_EMPTY_LIST;
+import static seedu.meetingjio.timetables.MasterTimetable.timetables;
 
-//@@author ibrahimisramos
 public class ClearCommand extends Command {
     public static final String COMMAND_WORD = "clear";
 
@@ -17,13 +18,20 @@ public class ClearCommand extends Command {
      */
     @Override
     public String execute(MasterTimetable masterTimetable) {
-        Timetable timetable = masterTimetable.getByIndex(0); // changes needed
+        int num_Timetables = timetables.size();
+        for (int i = 0; i < num_Timetables; i++) {
+            Timetable timetable = masterTimetable.getByIndex(i);
+            clearTimetable(timetable);
+        }
+        return printClearConfirmation();
+    }
+
+    private void clearTimetable(Timetable timetable){
         int numEntries = timetable.size();
         for (int i = 0; i < numEntries; i++) {
             timetable.remove(0);
         }
         assert (timetable.size() == 0) : ERROR_NON_EMPTY_LIST;
-        return printClearConfirmation();
     }
 
     public static String printClearConfirmation() {
