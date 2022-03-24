07cfc172d645093c57eadd4cbed9766f03b5aa58
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-24 15:05:53 +0800

    Add ActivityCreateCommandParser class

---
+++ src/main/java/seedu/splitlah/parser/commandparser/ActivityCreateCommandParser.java
@@ -0,0 +1,11 @@
+package seedu.splitlah.parser.commandparser;
+
+import seedu.splitlah.command.ActivityCreateCommand;
+import seedu.splitlah.exceptions.InvalidFormatException;
+
+public class ActivityCreateCommandParser implements CommandParser<ActivityCreateCommand> {
+    @Override
+    public ActivityCreateCommand getCommand(String commandArgs) throws InvalidFormatException {
+        return null;
+    }
+}
