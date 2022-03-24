1792273051846010b4ac541cae669481550396ee
johnmcmonigle <60244374+johnmcmonigle@users.noreply.github.com>
2022-03-24 18:30:50 +0800

    Remove empty return

--- src/main/java/cpp/logic/CommandHandler.java
+++ src/main/java/cpp/logic/CommandHandler.java
@@ -77,13 +77,10 @@ public class CommandHandler {
     private String getProjectName(String[] userInput) {
         assert (userInput.length >= Constants.TWO_ARGUMENTS) : "You must provide the name for the project!";
         // get the splited version of project name
-        if (userInput.length < Constants.TWO_ARGUMENTS) {
-            System.out.println(Constants.MESSAGE_INVALID_COMMAND_FORMAT);
-        } else {
-            String[] splitedName = Arrays.copyOfRange(userInput, 1, userInput.length);
-            String projectName = String.join(" ", splitedName);
-            return projectName;
-        }
+        String[] splitedName = Arrays.copyOfRange(userInput, 1, userInput.length);
+        String projectName = String.join(" ", splitedName);
+        return projectName;
+
     }
 
     private void listProjects(ProjectList projectList) {
