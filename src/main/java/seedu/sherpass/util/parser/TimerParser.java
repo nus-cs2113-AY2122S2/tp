e08809d3ce6d87bf2d94adc26a1234e7f41e827b
yuzhongng <yuzhongng@gmail.com>
2022-03-25 00:51:46 +0800

    Add TimerParser class
    Original method is in Parser class. Extract timer method and study session method to TimerParser class

---
+++ src/main/java/seedu/sherpass/util/parser/TimerParser.java
@@ -0,0 +1,89 @@
+package seedu.sherpass.util.parser;
+
+import seedu.sherpass.exception.InvalidTimeException;
+import seedu.sherpass.util.TimerLogic;
+import seedu.sherpass.util.Ui;
+
+import static seedu.sherpass.constant.Index.*;
+import static seedu.sherpass.constant.Index.DEFAULT_TIMER_INDEX;
+import static seedu.sherpass.constant.Message.ERROR_INVALID_STUDY_INPUT_MESSAGE;
+
+public class TimerParser {
+
+    /**
+     * Parses commands for study mode.
+     *
+     * @param rawUserInput Raw user input.
+     * @param ui           UI.
+     */
+    public static void parseStudyMode(String rawUserInput, Ui ui, TimerLogic timerLogic) {
+        String[] parsedInput = rawUserInput.trim().split(" ", 2);
+        switch (parsedInput[STUDY_COMMAND_INDEX].trim().toLowerCase()) {
+        case "start":
+            timerLogic.callStartTimer(parsedInput);
+            break;
+        case "pause":
+            timerLogic.callPauseTimer();
+            break;
+        case "resume":
+            timerLogic.callResumeTimer();
+            break;
+        case "stop":
+            timerLogic.callStopTimer();
+            break;
+        default:
+            ui.showToUser(ERROR_INVALID_STUDY_INPUT_MESSAGE);
+        }
+    }
+
+    /**
+     * Parses input to the timer.
+     *
+     * @param parsedInput Parsed input.
+     * @return Returns duration of custom timer input, or the duration of
+     *         selected default timer mode in seconds.
+     * @throws InvalidTimeException If timer input less than or equals to 0 or there is
+     *                              multiple timer inputs.
+     */
+    public static int parseTimerInput(String[] parsedInput) throws InvalidTimeException {
+        if (parsedInput[TIMER_FORMAT_INDEX].trim().contains("/custom")) {
+            if (parsedInput[TIMER_FORMAT_INDEX].trim().indexOf("/custom") != CUSTOM_COMMAND_INDEX) {
+                throw new InvalidTimeException();
+            }
+            String[] customTimerInput = parsedInput[TIMER_FORMAT_INDEX].split("/custom", 2);
+            int customDuration = Integer.parseInt(customTimerInput[CUSTOM_TIMER_INDEX].trim());
+            if (!isValidDuration(customDuration)) {
+                throw new InvalidTimeException();
+            }
+            return customDuration;
+        }
+        return selectDefaultTimer(parsedInput[DEFAULT_TIMER_INDEX].trim());
+    }
+
+    /**
+     * Parses the default timer modes.
+     *
+     * @param defaultTimerChoice Mode number.
+     * @return Returns the duration of the timer mode selected in seconds.
+     * @throws InvalidTimeException If defaultTimerChoice does not match
+     *                              with the given choices.
+     */
+    private static int selectDefaultTimer(String defaultTimerChoice) throws InvalidTimeException {
+        switch (defaultTimerChoice) {
+        case "0":
+            return DEFAULT_TIMER_ZERO;
+        case "1":
+            return DEFAULT_TIMER_ONE;
+        case "2":
+            return DEFAULT_TIMER_TWO;
+        case "3":
+            return DEFAULT_TIMER_THREE;
+        default:
+            throw new InvalidTimeException();
+        }
+    }
+
+    private static boolean isValidDuration(int duration) {
+        return duration > 0;
+    }
+}
