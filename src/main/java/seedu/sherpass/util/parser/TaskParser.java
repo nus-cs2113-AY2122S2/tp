f4ef91ce75f04b5554ee2d913256826f88790dc3
yuzhongng <yuzhongng@gmail.com>
2022-03-25 00:51:04 +0800

    Add TaskParser class
    Original method is in Parser class. Extract method related to task to TaskParser class

---
+++ src/main/java/seedu/sherpass/util/parser/TaskParser.java
@@ -0,0 +1,209 @@
+package seedu.sherpass.util.parser;
+
+import seedu.sherpass.command.*;
+import seedu.sherpass.exception.InputRepeatedException;
+import seedu.sherpass.exception.InvalidInputException;
+import seedu.sherpass.exception.WrongEditInfoFormatException;
+import seedu.sherpass.task.TaskList;
+import seedu.sherpass.util.Ui;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+import static seedu.sherpass.constant.DateAndTimeFormat.inputFormat;
+import static seedu.sherpass.constant.Index.*;
+import static seedu.sherpass.constant.Message.*;
+
+public class TaskParser {
+
+    protected static Command prepareMarkOrUnmark(String[] parsedInput, String commandWord, TaskList taskList) {
+        try {
+            int markIndex = Integer.parseInt(parsedInput[MARK_INDEX]) - 1;
+            if (commandWord.equals(MarkCommand.COMMAND_WORD)) {
+                return new MarkCommand(markIndex, taskList);
+            }
+            return new UnmarkCommand(markIndex, taskList);
+        } catch (IndexOutOfBoundsException | NumberFormatException e) {
+            System.out.println(ERROR_INVALID_MARKING_INDEX_MESSAGE);
+        }
+        return null;
+    }
+
+    protected static Command prepareAdd(String[] splitInput, TaskList taskList) {
+        String[] filteredTaskContent;
+        LocalDate byDate;
+        LocalDate doOnDate;
+        try {
+            if (!splitInput[TASK_CONTENT_INDEX].contains("/by")
+                    && !splitInput[TASK_CONTENT_INDEX].contains("/do_on")) {
+                return new AddCommand(splitInput[TASK_CONTENT_INDEX], taskList, null, null);
+            }
+
+            filteredTaskContent = splitInput[TASK_CONTENT_INDEX].split("/by", 2);
+            if (!splitInput[1].contains("/do_on")) {
+                byDate = prepareTaskDate(filteredTaskContent[1].trim());
+                return new AddCommand(filteredTaskContent[0].trim(), taskList, byDate, null);
+            }
+
+            String[] filteredDates = filteredTaskContent[1].split("/do_on");
+            byDate = prepareTaskDate(filteredDates[0].trim());
+            doOnDate = prepareTaskDate(filteredDates[1].trim());
+            return new AddCommand(filteredTaskContent[0], taskList, byDate, doOnDate);
+
+        } catch (ArrayIndexOutOfBoundsException | InvalidInputException e) {
+            printMissingInputMessage();
+        } catch (InputRepeatedException e) {
+            System.out.println("Oops! It seems that you've entered a duplicate task.\n"
+                    + "Please re-enter a new task if you wish to add one.");
+        }
+        return null;
+    }
+
+    protected static Command prepareEdit(String[] splitInput) {
+        String[] fullEditInfo = splitInput[1].trim().split(" ", 2);
+
+        // 7 possibilities of editing, incorrect format of inputs are rejected
+        try {
+
+            int taskNumberToEdit = Integer.parseInt(fullEditInfo[0]);
+            checkCorrectEditInfoFormat(fullEditInfo[1]);
+            return handleEdit(taskNumberToEdit, fullEditInfo[1]);
+
+        } catch (IndexOutOfBoundsException | NumberFormatException e) {
+            System.out.println("Please key in a valid task number");
+        } catch (InvalidInputException e) {
+            System.out.println("Invalid date");
+        } catch (WrongEditInfoFormatException e) {
+            System.out.println("Please use the correct order of keywords:\n"
+                    + "<task_description> /by <task_due_date> /do_on <date_to_work_on_task>\n\n"
+                    + "You only need to input the parts you want to edit.\n"
+                    + "e.g. edit 1 /do_on 2022/02/12\n"
+                    + "(The task_description and task_due_date is left out here)");
+        }
+
+        return null;
+    }
+
+    protected static Command prepareDelete(String[] parsedInput, TaskList taskList) {
+        try {
+            return new DeleteCommand(parsedInput, taskList);
+        } catch (IndexOutOfBoundsException | InvalidInputException | NumberFormatException e) {
+            System.out.println(ERROR_INVALID_DELETE_INDEX_MESSAGE + HELP_MESSAGE_SPECIFIC_COMMAND);
+        }
+        return null;
+    }
+
+    protected static Command prepareShow(String[] splitInput) {
+        try {
+            String selection = splitInput[SHOW_OPTION_INDEX].trim();
+            return parseShowCommandOptions(selection.toLowerCase());
+        } catch (ArrayIndexOutOfBoundsException | InvalidInputException e) {
+            System.out.println(ERROR_INVALID_INPUT_MESSAGE);
+        }
+        return null;
+    }
+
+    private static Command parseShowCommandOptions(String selection) throws InvalidInputException {
+        if (selection.isBlank()) {
+            throw new InvalidInputException();
+        }
+        try {
+            LocalDate dayInput = LocalDate.parse(selection, inputFormat);
+            return new ShowCommand(dayInput, null);
+        } catch (DateTimeParseException e) {
+            return new ShowCommand(null, selection);
+        }
+    }
+
+    private static void checkCorrectEditInfoFormat(String fullEditInfo) throws WrongEditInfoFormatException {
+        // tests to make sure the byDate is before the doOnDate
+        if (fullEditInfo.contains("/by") && fullEditInfo.contains("/do_on")) {
+            if (fullEditInfo.indexOf("/by") > fullEditInfo.indexOf("/do_on")) {
+                throw new WrongEditInfoFormatException();
+            }
+        }
+        // tests to make sure the task description is the first input if it is present
+        String[] splitEditInfo = fullEditInfo.split("/by \\d{4}/\\d{2}/\\d{2}|/do_on \\d{4}/\\d{2}/\\d{2}");
+        if (splitEditInfo.length > 1) {
+            throw new WrongEditInfoFormatException();
+        }
+    }
+
+    private static Command handleEdit(int taskNumberToEdit, String fullEditInfo) throws InvalidInputException {
+        String[] splitEditInfo = fullEditInfo.split(" ");
+        String descriptionToEdit;
+        LocalDate parsedByDateToEdit;
+        LocalDate parsedDoOnDateToEdit;
+
+        if (!splitEditInfo[0].trim().equals("/by") && !(splitEditInfo[0].trim().equals("/do_on"))) {
+            descriptionToEdit = splitEditInfo[0];
+        } else {
+            descriptionToEdit = EMPTY_STRING;
+        }
+
+        parsedByDateToEdit = getParsedDateToEdit(fullEditInfo, "/by");
+        parsedDoOnDateToEdit = getParsedDateToEdit(fullEditInfo, "/do_on");
+
+        return new EditCommand(taskNumberToEdit, descriptionToEdit, parsedByDateToEdit, parsedDoOnDateToEdit);
+    }
+
+    private static LocalDate getParsedDateToEdit(String fullEditInfo, String keyword) throws InvalidInputException {
+        if (fullEditInfo.contains(keyword)) {
+
+            int offsetForKeyword = keyword.length() + 1;
+            int offsetForSubstring = fullEditInfo.indexOf(keyword) + offsetForKeyword;
+
+            // gets the substring (of fullEditInfo) after the keyword (, which is either "/by" or "/do_on")
+            // splits the substring and obtains the first word (which should be the date of format yyyy/MM/dd)
+            String dateToEdit = fullEditInfo.substring(offsetForSubstring).split(" ")[0].trim();
+
+            return prepareTaskDate(dateToEdit);
+        }
+
+        return null;
+    }
+
+    private static LocalDate prepareTaskDate(String rawTaskDate) throws InvalidInputException {
+        if (rawTaskDate.isBlank()) {
+            return null;
+        }
+        try {
+            return LocalDate.parse(rawTaskDate, inputFormat);
+        } catch (DateTimeParseException e) {
+            return confirmInvalidDateFormat();
+        }
+    }
+
+    private static void printMissingInputMessage() {
+        System.out.println("Oops! The description of an 'add' command cannot be empty."
+                + HELP_MESSAGE_SPECIFIC_COMMAND);
+    }
+
+    private static LocalDate confirmInvalidDateFormat() throws InvalidInputException {
+        Ui anotherUi = new Ui();
+        anotherUi.showToUser("It seems that the date and time\nyou gave is not in the correct format.\n"
+                + "Would you like to re-enter a valid date and time? (Y/N)\n"
+                + "*Enter 'No' to skip the adding of this task*");
+        anotherUi.showLine();
+        while (true) {
+            String input = anotherUi.readCommand();
+            anotherUi.showLine();
+            if (input.trim().equalsIgnoreCase("Y")
+                    || input.trim().equalsIgnoreCase("Yes")) {
+                anotherUi.showToUser("Understood. Please key in the date and time you wish to save.");
+                anotherUi.showLine();
+                anotherUi.showToUser("Enter valid date input:");
+                input = anotherUi.readCommand();
+                anotherUi.showLine();
+                return prepareTaskDate(input.trim());
+            }
+            if (input.trim().equalsIgnoreCase("N")
+                    || input.trim().equalsIgnoreCase("No")) {
+                anotherUi.showToUser("Okay, proceeding to stop current task process...");
+                return null;
+            }
+            anotherUi.showToUser("Please confirm your choice with either Y (Yes) or N (No).");
+            anotherUi.showLine();
+        }
+    }
+}
