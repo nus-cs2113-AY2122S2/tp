41a792b518eb310b3f3af49120b27fc47dd7f2e8
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-24 15:44:29 +0800

    Add ActivityCreateCommandParserTest

---
+++ src/test/java/seedu/splitlah/parser/commandparser/ActivityCreateCommandParserTest.java
@@ -0,0 +1,8 @@
+package seedu.splitlah.parser.commandparser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ActivityCreateCommandParserTest {
+
+}
