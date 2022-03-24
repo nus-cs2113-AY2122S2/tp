ad196ebfcd3843b5c3d2e8c394d73b5e87c3ca63
Daryl <ithinkmynameisdaryl@gmail.com>
2022-03-24 20:49:25 +0800

    Fix bug when deleting recurring tasks

--- src/main/java/seedu/sherpass/command/DeleteRecurringCommand.java
+++ src/main/java/seedu/sherpass/command/DeleteRecurringCommand.java
@@ -11,11 +11,6 @@ import static seedu.sherpass.constant.Message.ERROR_INVALID_INDEX_MESSAGE;
 public class DeleteRecurringCommand extends Command {
     private int index;
 
-    public static final String COMMAND_WORD = "deleterecurring";
-    public static final String MESSAGE_USAGE = "deleterecurring: delete a recurring task in the task list.\n"
-            + "The index of the recurring task must be specified.\n"
-            + "\n\nTo execute the command,\nenter 'deleterecurring INDEX'.\n";
-
     public DeleteRecurringCommand() {
 
     }
@@ -48,6 +43,9 @@ public class DeleteRecurringCommand extends Command {
         int identifier = taskList.getTasks().get(index).getIdentifier();
         int count = 0;
         for (int i = taskList.getTasks().size() - 1; i >= 0; i--) {
+            if (i < index) {
+                break;
+            }
             if (taskList.getTasks().get(i).getIdentifier() == identifier) {
                 taskList.getTasks().remove(i);
                 ++count;
