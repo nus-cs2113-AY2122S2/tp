8412b74a41f680d8211fe8ecbfd0970c61e6ef1f
shxr3f <e0540641@u.nus.edu>
2022-03-25 17:32:45 +0800

    Implemented JUnit Tests for add Parser methods.

--- src/test/java/seedu/duke/helper/ParserTest.java
+++ src/test/java/seedu/duke/helper/ParserTest.java
@@ -5,21 +5,102 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import seedu.duke.exception.HalpmiException;
+import seedu.duke.helper.command.AddPatientCommand;
 
 import java.util.logging.Logger;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class ParserTest {
 
-    static final Logger logger = Logger.getLogger("ParserTest");
-
-    @BeforeEach
-    void printDetails(TestInfo testInfo) {
-        logger.info("Now testing -> " + testInfo.getDisplayName());
+    @Test
+    void parseAddPatientTest() {
+        String parameter = "S1234567A,JOHN,23,M,SINGAPORE,1999-01-01,2022-01-01";
+        String[] parameterArray = {"S1234567A","JOHN","23","M","SINGAPORE","1999-01-01","2022-01-01"};
+        try {
+            assertArrayEquals(parameterArray,Parser.parseAddPatient(parameter).parameterArray);
+        } catch (HalpmiException e) {
+            fail("Should not be throwing exception!");
+        }
     }
+
+    @Test
+    void parseAddPatientInvalidParamTest() {
+        String[] parameters = {"S1234567,JOHN,23,M,SINGAPORE,1999-01-01,2022-01-01",
+                "S1234567A,JOHN1,23,M,SINGAPORE,1999-01-01,2022-01-01",
+                "S1234567A,JOHN,2B,M,SINGAPORE,1999-01-01,2022-01-01",
+                "S1234567A,JOHN,23,T,SINGAPORE,1999-01-01,2022-01-01",
+                "S1234567A,JOHN,23,M,SINGAPORE,1999,2022-01-01",
+                "S1234567A,JOHN,23,M,SINGAPORE,1999-01-01,2022-100-01"};
+        for (String s : parameters) {
+            try {
+                Parser.parseAddPatient(s);
+                fail("Should be throwing exception! Failed for: " + s);
+            } catch (HalpmiException e) {
+                assert true;
+            }
+        }
+    }
+
+    @Test
+    void parseAddDoctorTest() {
+        String parameter = "S1234567A,JOHN,23,M,SINGAPORE,1999-01-01,Paediatrics";
+        String[] parameterArray = {"S1234567A","JOHN","23","M","SINGAPORE","1999-01-01","Paediatrics"};
+        try {
+            assertArrayEquals(parameterArray,Parser.parseAddDoctor(parameter).parameterArray);
+        } catch (HalpmiException e) {
+            fail("Should not be throwing exception!");
+        }
+    }
+
+    @Test
+    void parseAddDoctorInvalidParamTest() {
+        String[] parameters = {"S1234567,JOHN,23,M,SINGAPORE,1999-01-01,Paediatrics",
+                "S1234567A,JOHN1,23,M,SINGAPORE,1999-01-01,Paediatrics",
+                "S1234567A,JOHN,2B,M,SINGAPORE,1999-01-01,Paediatrics",
+                "S1234567A,JOHN,23,T,SINGAPORE,1999-01-01,Paediatrics",
+                "S1234567A,JOHN,23,M,SINGAPORE,1999,Paediatrics",
+                "S1234567A,JOHN,23,M,SINGAPORE,1999-01-01,Paediatrics1"};
+        for (String s : parameters) {
+            try {
+                Parser.parseAddDoctor(s);
+                fail("Should be throwing exception! Failed for: " + s);
+            } catch (HalpmiException e) {
+                assert true;
+            }
+        }
+    }
+
+    @Test
+    void parseAddMedicineTest() {
+        String parameter = "S123,Paracetamol,500,2025-02-02,Headaches,100";
+        String[] parameterArray = { "S123","Paracetamol","500","2025-02-02","Headaches","100"};
+        try {
+            assertArrayEquals(parameterArray,Parser.parseAddMedicine(parameter).parameterArray);
+        } catch (HalpmiException e) {
+            fail("Should not be throwing exception");
+        }
+    }
+
+    @Test
+    void parseAddMedicineInvalidParamTest() {
+        String[] parameters = {"S123,Paracetamol1,500,2025-02-02,Headaches,100",
+                "S123,Paracetamol,-1,2025-02-02,Headaches,100",
+                "S123,Paracetamol,50b,2025-02-02,Headaches,100",
+                "S123,Paracetamol,500,2025-24-02,Headaches,100"
+            };
+
+        for (String s : parameters) {
+            try {
+                Parser.parseAddMedicine(s);
+                fail("Should be throwing exception! Failed for: " + s);
+            } catch (HalpmiException e) {
+                assert true;
+            }
+        }
+    }
+
 
 
 }
