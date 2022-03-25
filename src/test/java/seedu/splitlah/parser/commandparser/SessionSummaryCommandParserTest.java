498f6ff4624370cece937d02c5d26a41b10ded1a
warrencxw <cxw.warren@gmail.com>
2022-03-25 14:31:12 +0800

    Add JUnit test class for SessionSummaryCommandParser class

---
+++ src/test/java/seedu/splitlah/parser/commandparser/SessionSummaryCommandParserTest.java
@@ -0,0 +1,19 @@
+package seedu.splitlah.parser.commandparser;
+
+import org.junit.jupiter.api.Test;
+import seedu.splitlah.command.Command;
+import seedu.splitlah.command.SessionSummaryCommand;
+import seedu.splitlah.exceptions.InvalidFormatException;
+import seedu.splitlah.parser.Parser;
+import seedu.splitlah.parser.ParserErrors;
+import seedu.splitlah.parser.ParserUtils;
+import seedu.splitlah.ui.Message;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class SessionSummaryCommandParserTest {
+
+    private static final String COMMAND_TYPE = SessionSummaryCommandParser.COMMAND_TEXT;
+    
+}
