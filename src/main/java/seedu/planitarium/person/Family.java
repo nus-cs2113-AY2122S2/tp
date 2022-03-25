c5080e50d51e4bda6745bd9b011d858610db266c
Kr0sfyre <teanweijun@gmail.com>
2022-03-25 20:34:45 +0800

    Add constructor to Family class

--- src/main/java/seedu/planitarium/person/Family.java
+++ src/main/java/seedu/planitarium/person/Family.java
@@ -1,5 +1,27 @@
+//@@author teanweijun
+
 package seedu.planitarium.person;
 
+import seedu.planitarium.ProjectLogger;
+import seedu.planitarium.global.Constants;
+
+import java.util.logging.Level;
+
 public class Family {
+    private PersonList parents;
+    private PersonList myGen;
+    private PersonList children;
+    private static ProjectLogger logger = new ProjectLogger(Family.class.getName(), "Family.log");
+
+    /**
+     * Constructs a new Family object.
+     */
+    public Family() {
+        parents = new PersonList();
+        myGen = new PersonList();
+        children = new PersonList();
+        String infoString = "Logger for PersonList initialised";
+        logger.getLogger().log(Level.INFO, infoString);
+    }
 
 }
