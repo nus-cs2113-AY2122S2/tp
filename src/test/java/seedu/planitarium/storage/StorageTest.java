d1a2c14d33529bce0a8245b35912300d414abb7e
HansHengGit <hans.hmk1@gmail.com>
2022-03-26 12:41:47 +0800

    Added Javadocs to Storage class added skeleton JUnit Testing

---
+++ src/test/java/seedu/planitarium/storage/StorageTest.java
@@ -0,0 +1,16 @@
+package seedu.planitarium.storage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StorageTest {
+
+    @Test
+    void checkFileExists() {
+    }
+
+    @Test
+    void loadData() {
+    }
+}
