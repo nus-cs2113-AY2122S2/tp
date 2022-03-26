0f97811e65ca372b97cf523b2fa88d2ae7668bda
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-26 17:22:15 +0800

    Add ActivityDeleteCommandParser class

---
+++ src/main/java/seedu/splitlah/parser/commandparser/ActivityDeleteCommandParser.java
@@ -0,0 +1,11 @@
+package seedu.splitlah.parser.commandparser;
+
+import seedu.splitlah.command.ActivityDeleteCommand;
+import seedu.splitlah.exceptions.InvalidFormatException;
+
+public class ActivityDeleteCommandParser implements CommandParser<ActivityDeleteCommand> {
+    @Override
+    public ActivityDeleteCommand getCommand(String commandArgs) throws InvalidFormatException {
+        return null;
+    }
+}
