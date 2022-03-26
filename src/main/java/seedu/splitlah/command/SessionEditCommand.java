6635d17f5c70f10027c56321fe1fa2a2a6c85146
Roy Tang <roy_tql@hotmail.com>
2022-03-27 00:10:12 +0800

    Add SessionEditCommand file

---
+++ src/main/java/seedu/splitlah/command/SessionEditCommand.java
@@ -0,0 +1,21 @@
+package seedu.splitlah.command;
+
+import seedu.splitlah.data.Manager;
+
+/**
+ * Represents a command which edits an existing Session object from a list of sessions managed by the Profile object.
+ *
+ * @author Roy
+ */
+public class SessionEditCommand extends Command {
+    
+    /**
+     * Runs the command to edit an existing Session object from the list of sessions managed by a Manager Object.
+     *
+     * @param manager A Manager object that manages the TextUI, Profile and Storage object.
+     */
+    @Override
+    public void run(Manager manager) {
+
+    }
+}
