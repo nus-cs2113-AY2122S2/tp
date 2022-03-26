6448f4e1544596c5f986b53c8263bb3e57a63bf2
yuzhongng <yuzhongng@gmail.com>
2022-03-26 21:11:53 +0800

    Add method to show timetable schedule for tomorrow, next week, current month and any specfic month

--- src/main/java/seedu/sherpass/util/timetable/Timetable.java
+++ src/main/java/seedu/sherpass/util/timetable/Timetable.java
@@ -4,11 +4,24 @@ import seedu.sherpass.task.Task;
 import seedu.sherpass.task.TaskList;
 import seedu.sherpass.util.Ui;
 
+import java.time.Month;
 import java.util.ArrayList;
 
 import java.time.temporal.ChronoUnit;
 import java.time.LocalDate;
 
+import static java.time.Month.APRIL;
+import static java.time.Month.AUGUST;
+import static java.time.Month.DECEMBER;
+import static java.time.Month.FEBRUARY;
+import static java.time.Month.JANUARY;
+import static java.time.Month.JULY;
+import static java.time.Month.JUNE;
+import static java.time.Month.MARCH;
+import static java.time.Month.MAY;
+import static java.time.Month.NOVEMBER;
+import static java.time.Month.OCTOBER;
+import static java.time.Month.SEPTEMBER;
 import static seedu.sherpass.constant.DateAndTimeFormat.dayOnlyFormat;
 import static seedu.sherpass.constant.DateAndTimeFormat.dateOnlyFormat;
 import static seedu.sherpass.constant.TimetableConstant.DATE_SPACE_FULL_LENGTH;
@@ -41,10 +54,26 @@ public class Timetable {
         return timetable;
     }
 
+    /**
+     * Generate a timetable for today
+     *
+     * @param taskList Representation of an array of tasks.
+     * @param ui       The user interface which interacts with the user.
+     */
     public static void showTodaySchedule(TaskList taskList, Ui ui) {
-        ArrayList<Task> filteredTasks = taskList.getFilteredTasksByDate(LocalDate.now());
-        Timetable timetable = prepareTimetable(LocalDate.now(), filteredTasks, ui);
-        timetable.printSchedule();
+        LocalDate todayDate = LocalDate.now();
+        showScheduleByDay(todayDate, taskList, ui);
+    }
+
+    /**
+     * Generate a timetable for tomorrow
+     *
+     * @param taskList Representation of an array of tasks.
+     * @param ui       The user interface which interacts with the user.
+     */
+    public static void showTomorrowSchedule(TaskList taskList, Ui ui) {
+        LocalDate tomorrowDate = LocalDate.now().plusDays(1);
+        showScheduleByDay(tomorrowDate, taskList, ui);
     }
 
     /**
@@ -60,19 +89,81 @@ public class Timetable {
         timetable.printSchedule();
     }
 
-    /**
-     * Generate a timetable for the current week.
-     *
-     * @param taskList Representation of an array of tasks.
-     * @param ui       The user interface which interacts with the user.
-     */
+
     public static void showScheduleOfTheWeek(TaskList taskList, Ui ui) {
-        LocalDate currentDate = TimetableLogic.resetDateToMonday(LocalDate.now().format(dayOnlyFormat), ui);
+        LocalDate currentDate = TimetableLogic.resetDateToMonday(LocalDate.now(), ui);
         for (int i = 0; i < DAYS_IN_A_WEEK; i++) {
             showScheduleByDay(currentDate, taskList, ui);
             assert (currentDate != null);
             currentDate = currentDate.plus(1, ChronoUnit.DAYS);
         }
+    }
+
+
+    /**
+     * Generate a weekly timetable schedule.
+     *
+     * @param taskList Representation of an array of tasks.
+     * @param ui       The user interface which interacts with the user.
+     */
+    public static void showNextWeekSchedule(TaskList taskList, Ui ui) {
+        LocalDate nextMondayDate = LocalDate.now().plusWeeks(1);
+        LocalDate nextDate = TimetableLogic.resetDateToMonday(nextMondayDate, ui);
+        for (int i = 0; i < DAYS_IN_A_WEEK; i++) {
+            showScheduleByDay(nextDate, taskList, ui);
+            assert (nextDate != null);
+            nextDate = nextDate.plusDays(1);
+        }
+    }
+
+    public static void showMonthlySchedule(TaskList taskList, Ui ui) {
+        Month currentMonth = LocalDate.now().getMonth();
+        TimetableLogic.showMonthlySchedule(taskList, ui, currentMonth);
+    }
+
+    public static void showJanuarySchedule(TaskList taskList, Ui ui) {
+        TimetableLogic.showMonthlySchedule(taskList, ui, JANUARY);
+    }
+
+    public static void showFeburarySchedule(TaskList taskList, Ui ui) {
+        TimetableLogic.showMonthlySchedule(taskList, ui, FEBRUARY);
+    }
+
+    public static void showMarchSchedule(TaskList taskList, Ui ui) {
+        TimetableLogic.showMonthlySchedule(taskList, ui, MARCH);
+    }
+
+    public static void showAprilSchedule(TaskList taskList, Ui ui) {
+        TimetableLogic.showMonthlySchedule(taskList, ui, APRIL);
+    }
+
+    public static void showMaySchedule(TaskList taskList, Ui ui) {
+        TimetableLogic.showMonthlySchedule(taskList, ui, MAY);
+    }
+    public static void showJuneSchedule(TaskList taskList, Ui ui) {
+        TimetableLogic.showMonthlySchedule(taskList, ui, JUNE);
+    }
+
+    public static void showJulySchedule(TaskList taskList, Ui ui) {
+        TimetableLogic.showMonthlySchedule(taskList, ui, JULY);
+    }
+
+    public static void showAuguestSchedule(TaskList taskList, Ui ui) {
+        TimetableLogic.showMonthlySchedule(taskList, ui, AUGUST);
+    }
+
+    public static void showSeptemberSchedule(TaskList taskList, Ui ui) {
+        TimetableLogic.showMonthlySchedule(taskList, ui, SEPTEMBER);
+    }
+
+    public static void showOctoberSchedule(TaskList taskList, Ui ui) {
+        TimetableLogic.showMonthlySchedule(taskList, ui, OCTOBER);
+    }
+    public static void showNovemberSchedule(TaskList taskList, Ui ui) {
+        TimetableLogic.showMonthlySchedule(taskList, ui, NOVEMBER);
+    }
+    public static void showDecemberSchedule(TaskList taskList, Ui ui) {
+        TimetableLogic.showMonthlySchedule(taskList, ui, DECEMBER);
     }
 
     private void printSchedule() {
