033ec6893c76e196e1f30c4b3bfa26f756893ecb
yuzhongng <yuzhongng@gmail.com>
2022-03-26 15:13:53 +0800

    Move the methods related to timetable from TaskParser to TimetableParser

--- src/main/java/seedu/sherpass/util/parser/TimetableParser.java
+++ src/main/java/seedu/sherpass/util/parser/TimetableParser.java
@@ -1,4 +1,37 @@
 package seedu.sherpass.util.parser;
 
+import seedu.sherpass.command.Command;
+import seedu.sherpass.command.ShowCommand;
+import seedu.sherpass.exception.InvalidInputException;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+import static seedu.sherpass.constant.DateAndTimeFormat.inputWithoutTimeFormat;
+import static seedu.sherpass.constant.Index.SHOW_OPTION_INDEX;
+import static seedu.sherpass.constant.Message.ERROR_INVALID_INPUT_MESSAGE;
+
 public class TimetableParser {
+
+    public static Command prepareShow(String[] splitInput) {
+        try {
+            String selection = splitInput[SHOW_OPTION_INDEX].trim();
+            return parseShowCommandOptions(selection.toLowerCase());
+        } catch (ArrayIndexOutOfBoundsException | InvalidInputException e) {
+            System.out.println(ERROR_INVALID_INPUT_MESSAGE);
+        }
+        return null;
+    }
+
+    private static Command parseShowCommandOptions(String selection) throws InvalidInputException {
+        if (selection.isBlank()) {
+            throw new InvalidInputException();
+        }
+        try {
+            LocalDate dayInput = LocalDate.parse(selection, inputWithoutTimeFormat);
+            return new ShowCommand(dayInput, null);
+        } catch (DateTimeParseException e) {
+            return new ShowCommand(null, selection);
+        }
+    }
 }
