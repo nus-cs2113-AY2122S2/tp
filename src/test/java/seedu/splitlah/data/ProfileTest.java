6f74d32cf93623a0f5e9f1f3cd63d74b614a4be1
Roy Tang <roy_tql@hotmail.com>
2022-03-26 17:57:25 +0800

    Remove BeforeEach test segment for ProfileTest

--- src/test/java/seedu/splitlah/data/ProfileTest.java
+++ src/test/java/seedu/splitlah/data/ProfileTest.java
@@ -1,41 +1,32 @@
 package seedu.splitlah.data;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import seedu.splitlah.command.Command;
 import seedu.splitlah.exceptions.InvalidDataException;
 import seedu.splitlah.parser.Parser;
 import seedu.splitlah.ui.Message;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
-
 
 class ProfileTest {
 
     Manager manager = new Manager();
 
     /**
-     * Creates two sessions that is stored and managed by the Manager object.
-     */
-    @BeforeEach
-    void setUp() {
-        String sessionOneArgs = "session /create /n Class outing /d 15-02-2022 /pl Alice Bob";
-        String sessionTwoArgs = "session /create /n Family gathering /d 20-02-2022  /pl Eves Mallory";
-        Command createSessionOne = Parser.getCommand(sessionOneArgs);
-        createSessionOne.run(manager);
-        Command createSessionTwo = Parser.getCommand(sessionTwoArgs);
-        createSessionTwo.run(manager);
-    }
-
-    /**
      * Checks if method returns true when a Session object with the specified session name is found.
      */
     @Test
     public void hasSessionName_inputContainsExistingSessionName_true() {
+        String sessionOneArgs = "session /create /n Class outing /d 15-02-2022 /pl Alice Bob";
+        Command createSessionOne = Parser.getCommand(sessionOneArgs);
+        createSessionOne.run(manager);
+
         String sessionNameToTest = "Class outing";
         boolean isExists = manager.getProfile().hasSessionName(sessionNameToTest);
-        assertEquals(true, isExists);
+        assertTrue(isExists);
     }
 
     /**
