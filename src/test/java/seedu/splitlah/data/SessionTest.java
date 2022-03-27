619b30301f3c960834f10098f1947ce93e03690f
warrencxw <cxw.warren@gmail.com>
2022-03-27 11:00:38 +0800

    Add set up method to prepare for subsequent JUnit tests

--- src/test/java/seedu/splitlah/data/SessionTest.java
+++ src/test/java/seedu/splitlah/data/SessionTest.java
@@ -14,4 +14,29 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 class SessionTest {
 
+    Manager manager = new Manager();
+    Session session;
+
+    private static final int TEST_SESSION = 1;
+    private static final String CREATE_TEST_SESSION_INPUT =
+            "session /create /n Class outing /d 15-02-2022 /pl Alice Bob Charlie";
+    private static final String CREATE_TEST_ACTIVITY_INPUT_ONE =
+            "activity /create /sid 1 /n Lunch /p Alice /i Alice Bob Charlie /co 15";
+    private static final String CREATE_TEST_ACTIVITY_INPUT_TWO =
+            "activity /create /sid 1 /n Dinner /p Alice /i Alice Bob Charlie /co 30";
+
+    /**
+     * Creates a session that is stored and managed by the Manager object.
+     */
+    @BeforeEach
+    void setUp() {
+        Command createSessionCommand = Parser.getCommand(CREATE_TEST_SESSION_INPUT);
+        createSessionCommand.run(manager);
+
+        try {
+            session = manager.getProfile().getSession(TEST_SESSION);
+        } catch (InvalidDataException exception) {
+            fail();
+        }
+    }
 }
