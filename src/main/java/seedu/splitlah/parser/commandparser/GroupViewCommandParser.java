8f91c5579f14b507451fc80f51efc88f07a8dd59
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-27 19:23:51 +0800

    Add GroupViewCommandParser class

---
+++ src/main/java/seedu/splitlah/parser/commandparser/GroupViewCommandParser.java
@@ -0,0 +1,11 @@
+package seedu.splitlah.parser.commandparser;
+
+import seedu.splitlah.command.GroupViewCommand;
+import seedu.splitlah.exceptions.InvalidFormatException;
+
+public class GroupViewCommandParser implements CommandParser<GroupViewCommand> {
+    @Override
+    public GroupViewCommand getCommand(String commandArgs) throws InvalidFormatException {
+        return null;
+    }
+}
