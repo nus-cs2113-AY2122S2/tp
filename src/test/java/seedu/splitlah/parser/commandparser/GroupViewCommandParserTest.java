225b96ba31794aacc1321765f766ba26ff9299d2
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-27 19:49:52 +0800

    Add GroupViewCommandParserTest

---
+++ src/test/java/seedu/splitlah/parser/commandparser/GroupViewCommandParserTest.java
@@ -0,0 +1,10 @@
+package seedu.splitlah.parser.commandparser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class GroupViewCommandParserTest {
+
+    private static final String COMMAND_TYPE = GroupViewCommandParser.COMMAND_TEXT;
+
+}
