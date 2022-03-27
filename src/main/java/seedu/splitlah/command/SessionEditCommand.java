04fde732b4b77f4974f7a735489e66be36e8d824
Roy Tang <roy_tql@hotmail.com>
2022-03-27 09:54:18 +0800

    Add attributes and constructor for SessionEditCommand class

--- src/main/java/seedu/splitlah/command/SessionEditCommand.java
+++ src/main/java/seedu/splitlah/command/SessionEditCommand.java
@@ -1,6 +1,14 @@
 package seedu.splitlah.command;
 
+import seedu.splitlah.data.Group;
 import seedu.splitlah.data.Manager;
+import seedu.splitlah.data.PersonList;
+import seedu.splitlah.data.Session;
+import seedu.splitlah.exceptions.InvalidDataException;
+import seedu.splitlah.ui.Message;
+
+import java.time.LocalDate;
+import java.util.logging.Level;
 
 /**
  * Represents a command which edits an existing Session object from a list of sessions managed by the Profile object.
@@ -8,6 +16,22 @@ import seedu.splitlah.data.Manager;
  * @author Roy
  */
 public class SessionEditCommand extends Command {
+
+    private static final String COMMAND_SUCCESS = "The session was edited successfully.";
+
+    private final int sessionId;
+    private final String sessionName;
+    private final String[] personNames;
+    private final LocalDate sessionDate;
+    private final int groupId;
+
+    public SessionEditCommand(int sessionId, String sessionName, String[] personNames, LocalDate date, int groupId) {
+        this.sessionId = sessionId;
+        this.sessionName = sessionName;
+        this.personNames = personNames;
+        this.sessionDate = date;
+        this.groupId = groupId;
+    }
 
     /**
      * Runs the command to edit an existing Session object from the list of sessions managed by a Manager Object.
