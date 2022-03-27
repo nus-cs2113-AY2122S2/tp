3b9f1e18989c7d25f42b03ad379e7532dfd203ff
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-27 20:37:58 +0800

    Add test regarding valid user input

--- src/test/java/seedu/splitlah/parser/commandparser/GroupViewCommandParserTest.java
+++ src/test/java/seedu/splitlah/parser/commandparser/GroupViewCommandParserTest.java
@@ -1,6 +1,9 @@
 package seedu.splitlah.parser.commandparser;
 
 import org.junit.jupiter.api.Test;
+import seedu.splitlah.command.Command;
+import seedu.splitlah.command.GroupViewCommand;
+import seedu.splitlah.data.Manager;
 import seedu.splitlah.exceptions.InvalidFormatException;
 import seedu.splitlah.parser.Parser;
 import seedu.splitlah.parser.ParserUtils;
@@ -58,6 +61,33 @@ class GroupViewCommandParserTest {
             String messageToTest = Message.ERROR_PARSER_MISSING_ARGUMENT + "/gid"
                     + "\n" + GroupViewCommandParser.COMMAND_FORMAT;
             assertEquals(messageToTest, e.getMessage());
+        }
+    }
+
+    /**
+     * Checks if a GroupViewCommand object is correctly returned when the command is correctly entered.
+     */
+    @Test
+    public void getCommand_validUserInput_ActivityCreateCommand() {
+        Manager manager = new Manager();
+
+        String groupInput = "group /create /n Group1 /pl Alice Bob Charlie";
+        Command createGroup = Parser.getCommand(groupInput);
+        createGroup.run(manager);
+
+        String validUserInput = "group /view /gid 1";
+        String validArguments = Parser.getRemainingArgument(validUserInput);
+        String errorMessage = ParserUtils.checkIfCommandIsValid(COMMAND_TYPE, validArguments);
+        if (!errorMessage.isEmpty()) {
+            fail();
+        }
+
+        GroupViewCommandParser groupViewCommandParser = new GroupViewCommandParser();
+        try {
+            Command command = groupViewCommandParser.getCommand(validArguments);
+            assertEquals(GroupViewCommand.class, command.getClass());
+        } catch (InvalidFormatException e) {
+            fail();
         }
     }
 
