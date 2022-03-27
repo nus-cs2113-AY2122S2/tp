952890f205fb5b57866364f91b8c07a570901845
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-27 19:26:45 +0800

    Update getCommand method in GroupViewCommandParser

--- src/main/java/seedu/splitlah/parser/commandparser/GroupViewCommandParser.java
+++ src/main/java/seedu/splitlah/parser/commandparser/GroupViewCommandParser.java
@@ -2,7 +2,9 @@ package seedu.splitlah.parser.commandparser;
 
 import seedu.splitlah.command.GroupViewCommand;
 import seedu.splitlah.exceptions.InvalidFormatException;
+import seedu.splitlah.parser.Parser;
 import seedu.splitlah.parser.ParserUtils;
+import seedu.splitlah.ui.Message;
 
 public class GroupViewCommandParser implements CommandParser<GroupViewCommand> {
 
@@ -13,9 +15,15 @@ public class GroupViewCommandParser implements CommandParser<GroupViewCommand> {
     public static final String[] COMMAND_DELIMITERS = {
         ParserUtils.GROUP_ID_DELIMITER
     };
-    
+
     @Override
     public GroupViewCommand getCommand(String commandArgs) throws InvalidFormatException {
-        return null;
+        try {
+            int groupId = Parser.parseGroupId(commandArgs);
+            assert groupId > 0 : Message.ASSERT_GROUPVIEW_GROUP_ID_NOT_INITIALIZED;
+            return new GroupViewCommand(groupId);
+        } catch (InvalidFormatException e) {
+            throw new InvalidFormatException(e.getMessage() + "\n" + COMMAND_FORMAT);
+        }
     }
 }
