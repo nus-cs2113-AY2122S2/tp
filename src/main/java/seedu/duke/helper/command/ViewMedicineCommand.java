1ef5cea6d14bc7c01989fd8d48748dd176462a58
Anvitha-r <anvithar1@gmail.com>
2022-03-28 05:34:05 +0800

    update exception to be thrown for view function for medicine given empty list

--- src/main/java/seedu/duke/helper/command/ViewMedicineCommand.java
+++ src/main/java/seedu/duke/helper/command/ViewMedicineCommand.java
@@ -1,6 +1,7 @@
 package seedu.duke.helper.command;
 
 import seedu.duke.assets.List;
+import seedu.duke.exception.HalpmiException;
 import seedu.duke.status.Status;
 
 public class ViewMedicineCommand extends Command {
@@ -9,7 +10,7 @@ public class ViewMedicineCommand extends Command {
         super(parameterArray);
     }
 
-    public Status execute(List medicineList) {
+    public Status execute(List medicineList) throws HalpmiException {
         if (parameterArray == null) {
             medicineList.view();
         } else {
