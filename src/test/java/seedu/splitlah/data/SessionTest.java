010301846f88625e0b95bfce4d270289116bc3bd
warrencxw <cxw.warren@gmail.com>
2022-03-27 10:55:05 +0800

    Add SessionTest class for JUnit testing of Session class

---
+++ src/test/java/seedu/splitlah/data/SessionTest.java
@@ -0,0 +1,17 @@
+package seedu.splitlah.data;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.splitlah.command.Command;
+import seedu.splitlah.exceptions.InvalidDataException;
+import seedu.splitlah.parser.Parser;
+import seedu.splitlah.ui.Message;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class SessionTest {
+
+}
