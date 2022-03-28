1f40c4105da4a9942fcf31fd2ded40abc70c40fb
alanlowzies <30099983+alanlowzies@users.noreply.github.com>
2022-03-28 23:32:27 +0800

    ParserTest.java: Remove unused import

--- src/test/java/werkit/ParserTest.java
+++ src/test/java/werkit/ParserTest.java
@@ -18,8 +18,6 @@ import org.junit.jupiter.api.Test;
 import storage.FileManager;
 import storage.LogHandler;
 
-import java.io.IOException;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
