fbd018104b31073e71e17692c2e97b834d5d6c6b
tjiarong <jia8991@gmail.com>
2022-03-28 20:20:03 +0800

    Added Javadoc description for isPermanent

--- src/main/java/seedu/planitarium/money/Income.java
+++ src/main/java/seedu/planitarium/money/Income.java
@@ -3,10 +3,12 @@
 package seedu.planitarium.money;
 
 public class Income extends Money {
+
     /**
      * Initialise a new income object.
      * @param description - Income's description
      * @param amount - Income's amount
+     * @param isPermanent - Income's recurrence
      */
     public Income(String description, double amount, boolean isPermanent) {
         super(description, amount, isPermanent);
