a53f31060e2d7c6e2a7c9789b6492724248605a4
Nnythingy <66124098+Nnythingy@users.noreply.github.com>
2022-03-28 22:12:47 +0800

    Update RegexTest.java

--- src/test/java/seedu/duke/RegexTest.java
+++ src/test/java/seedu/duke/RegexTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import util.exceptions.WrongCommandException;
 
 import java.util.HashMap;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RegexTest {
 
