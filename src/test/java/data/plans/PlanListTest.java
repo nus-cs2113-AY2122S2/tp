bda3b6a52e30e6e7c0741ac708ea57083d59784c
TianaiYan <e0661454@u.nus.edu>
2022-03-29 00:00:54 +0800

    Create JUnit test for Plan Details and Plan Delete

--- src/test/java/data/plans/PlanListTest.java
+++ src/test/java/data/plans/PlanListTest.java
@@ -7,6 +7,8 @@ import data.workouts.WorkoutList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import storage.LogHandler;
+import textcolors.TextColor;
+import werkit.UI;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -18,6 +20,7 @@ class PlanListTest {
     ExerciseList exerciseList;
     WorkoutList workoutList;
     PlanList planList;
+    UI ui;
 
     @BeforeEach
     void setUp() throws InvalidWorkoutException, InvalidExerciseException {
@@ -25,6 +28,7 @@ class PlanListTest {
         exerciseList = new ExerciseList();
         workoutList = new WorkoutList(exerciseList);
         planList = new PlanList(workoutList);
+        ui = new UI();
 
         exerciseList.addExerciseToList("push up");
         exerciseList.addExerciseToList("pull up");
@@ -40,7 +44,7 @@ class PlanListTest {
     }
 
     @Test
-    void listAllPlan_expectNoPlanPrints() throws InvalidPlanException {
+    void listAllPlan_expectNoPlanPrints() {
         String expectedOutput =
                 "Oops! You have not created any plans yet!"
                         + "\nTo create a new plan, enter 'plan /new <plan name> /workouts "
@@ -127,4 +131,84 @@ class PlanListTest {
             () -> planList.createAndAddPlan("Duplicate Sequence /workouts 1,2,3"));
     }
 
+    @Test
+    void listPlanDetails_validPlanNumber_expectThreeWorkouts() throws InvalidPlanException {
+        planList.createAndAddPlan("Plan 1 /workouts 1,2,3");
+        String expectedOutput =
+                "Here are the 3 workouts in [" + ui.getColorText(TextColor.COLOR_YELLOW, "Plan 1")
+                        + "].\n"
+                        + "1. push up (10 reps)\n"
+                        + "2. sit up (15 reps)\n"
+                        + "3. burpee (20 reps)\n";
+        expectedOutput = expectedOutput.replaceAll("\n", "").replaceAll("\r", "");
+        ByteArrayOutputStream consoleOutput = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(consoleOutput);
+        System.setOut(ps);
+        planList.listPlanDetails("1", ui);
+        System.out.flush();
+        String consoleOutputs = consoleOutput.toString().replaceAll("\n", "").replaceAll("\r", "");
+        assertEquals(expectedOutput, consoleOutputs);
+
+    }
+
+    @Test
+    void listPlanDetails_planNumberOutOfRange_expectInvalidPlanException() throws InvalidPlanException {
+        planList.createAndAddPlan("Plan 1 /workouts 1,5,4,3");
+        planList.createAndAddPlan("Plan 2 /workouts 1,5,4,3,1,1,1");
+        planList.createAndAddPlan("Plan 3 /workouts 1, 3, 4");
+
+        String planNumber = "5";
+        assertThrows(InvalidPlanException.class,
+                () -> planList.listPlanDetails(planNumber, ui));
+
+    }
+
+    @Test
+    void listPlanDetails_inputArgumentIsNotANumber_expectNumberFormatException() throws InvalidPlanException {
+        planList.createAndAddPlan("Plan 1 /workouts 1,5,4,3");
+        planList.createAndAddPlan("Plan 2 /workouts 1,5,4,3,1,1,1");
+        planList.createAndAddPlan("Plan 3 /workouts 1, 3, 4");
+
+        String invalidArgument = "3a";
+        assertThrows(NumberFormatException.class,
+                () -> planList.listPlanDetails(invalidArgument, ui));
+
+    }
+
+    @Test
+    void deletePlan_validIndexToDelete_expectDeletePlanMessage() throws InvalidPlanException {
+        planList.createAndAddPlan("Plan 1 /workouts 1,5,4,3");
+        planList.createAndAddPlan("Plan 2 /workouts 1,5,4,3,1,1,1");
+        planList.createAndAddPlan("Plan 3 /workouts 1, 3, 4");
+
+        int planNumberToDelete = 2;
+
+        assertEquals("Plan 2", planList.getPlansDisplayList().get(planNumberToDelete - 1).toString());
+        planList.deletePlan(Integer.toString(planNumberToDelete));
+        assertEquals("Plan 3", planList.getPlansDisplayList().get(planNumberToDelete - 1).toString());
+    }
+
+    @Test
+    void deletePlan_indexOutOfRange_expectInvalidPlanException() throws InvalidPlanException {
+        planList.createAndAddPlan("Plan 1 /workouts 1,5,4,3");
+        planList.createAndAddPlan("Plan 2 /workouts 1,5,4,3,1,1,1");
+        planList.createAndAddPlan("Plan 3 /workouts 1, 3, 4");
+
+        String planNumberToDelete = "5";
+        assertThrows(InvalidPlanException.class,
+                () -> planList.deletePlan(planNumberToDelete));
+
+    }
+
+    @Test
+    void deleteWorkout_inputArgumentIsNotANumber_expectNumberFormatException() throws InvalidPlanException {
+        planList.createAndAddPlan("Plan 1 /workouts 1,5,4,3");
+        planList.createAndAddPlan("Plan 2 /workouts 1,5,4,3,1,1,1");
+        planList.createAndAddPlan("Plan 3 /workouts 1, 3, 4");
+
+        String invalidArgument = "3a";
+        assertThrows(NumberFormatException.class,
+                () -> planList.deletePlan(invalidArgument));
+
+    }
 }
