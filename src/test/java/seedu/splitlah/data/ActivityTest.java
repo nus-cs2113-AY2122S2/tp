58a199da65315594f5bd90845a5d57fcd7e65310
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-28 16:21:01 +0800

    Add constants and setUp method in ActivityTest

--- src/test/java/seedu/splitlah/data/ActivityTest.java
+++ src/test/java/seedu/splitlah/data/ActivityTest.java
@@ -1,7 +1,45 @@
 package seedu.splitlah.data;
 
+import org.junit.jupiter.api.BeforeEach;
+import seedu.splitlah.command.Command;
+import seedu.splitlah.exceptions.InvalidDataException;
+import seedu.splitlah.parser.Parser;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class ActivityTest {
+
+
+    Manager manager = new Manager();
+    Session session;
+    Activity activity;
+
+    private static final int TEST_SESSION = 1;
+    private static final int TEST_ACTIVITY = 1;
+    private static final String CREATE_TEST_SESSION_INPUT =
+            "session /create /n Class outing /d 15-02-2022 /pl Alice Bob Charlie";
+    private static final String CREATE_TEST_ACTIVITY_INPUT_ONE =
+            "activity /create /sid 1 /n Lunch /p Alice /i Alice Bob Charlie /co 15";
+    private static final String CREATE_TEST_ACTIVITY_INPUT_TWO =
+            "activity /create /sid 1 /n Dinner /p Alice /i Alice Bob Charlie /co 30";
+
+    /**
+     * Creates a session and an activity that are stored and managed by the Manager object.
+     */
+    @BeforeEach
+    void setUp() {
+        Command createSessionCommand = Parser.getCommand(CREATE_TEST_SESSION_INPUT);
+        createSessionCommand.run(manager);
+        Command createActivityCommand = Parser.getCommand(CREATE_TEST_ACTIVITY_INPUT_ONE);
+        createActivityCommand.run(manager);
+
+        try {
+            session = manager.getProfile().getSession(TEST_SESSION);
+            activity = session.getActivity(TEST_ACTIVITY);
+        } catch (InvalidDataException exception) {
+            fail();
+        }
+    }
 
 }
