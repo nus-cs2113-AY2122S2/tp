23095814a69badfb5bbf4363d9286d3dc1699c1e
FaliciaOng <ongfali@gmail.com>
2022-03-29 21:54:01 +0800

    Update exceptions for housekeeper

--- src/main/java/seedu/duke/InvalidAvailabilityException.java
+++ src/main/java/seedu/duke/InvalidAvailabilityException.java
@@ -5,7 +5,7 @@ package seedu.duke;
  * This exception is thrown if the add housekeeper availability command has empty days available.
  */
 public class InvalidAvailabilityException extends HotelLiteManagerException {
-    private static final String ERROR_MESSAGE = "Error! Invalid Command -> Availability NAME @ 1,2";
+    private static final String ERROR_MESSAGE = "Error! Invalid Command -> Availability NAME / 1,2";
 
     @Override
     public String getErrorMessage() {
