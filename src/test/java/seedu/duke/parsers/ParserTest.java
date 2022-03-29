a68f99504623437112e2d5d41333e21841e8ee79
Changrui <e0589902@u.nus.edu>
2022-03-29 22:49:19 +0800

    Fix Coding standards

--- src/test/java/seedu/duke/parsers/ParserTest.java
+++ src/test/java/seedu/duke/parsers/ParserTest.java
@@ -1,27 +1,28 @@
 package seedu.duke.parsers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import seedu.duke.commands.Command;
 
 public class ParserTest extends Parser {
     public ParserTest() {
         // This can be replaced to any regex you want to test
-        commandFormat = "(\\s*(?<configurationGroupWord>[A-Z_]+)($|\\b=\\s*(?<newValue>.*)\\b))?";
+        commandFormat = "(\\s*(?<configurationGroupWord>[A-Z_]+)($|=\\\"\\b(?<newValue>.*)\\b\\\"))?";
         groupNames.add("configurationGroupWord");
         groupNames.add("newValue");
     }
 
     @Test
     public void checkRegex() {
-        final String testString = "";
+        final String testString = "COMPLETED_TASKS_SHOWN";
         try {
             parsedCommand = parseString(testString);
-            assertEquals(null, parsedCommand.get("configurationGroupWord"));
+            assertEquals("COMPLETED_TASKS_SHOWN", parsedCommand.get("configurationGroupWord"));
             assertEquals(null, parsedCommand.get("newValue"));
+            assertNull(parsedCommand.get("newValue"));
 
         } catch (Exception e) {
             fail();
