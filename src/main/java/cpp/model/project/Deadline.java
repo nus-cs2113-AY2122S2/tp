933ea34cbe6d46a7b587a39910914e78df972ecd
Franke Tang <franketang@gmail.com>
2022-03-30 16:21:19 +0800

    Create Deadline class
    Deadline currently understands two formats
    - yyyy/mm/dd
    - a given day of the week

---
+++ src/main/java/cpp/model/project/Deadline.java
@@ -0,0 +1,31 @@
+package cpp.model.project;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+import cpp.Constants;
+
+public class Deadline {
+
+    private LocalDate date;
+
+    public Deadline(String stringDate) {
+        if(Constants.DAYS_OF_THE_WEEK.contains(stringDate.toUpperCase())) {
+            DayOfWeek dateEnum = DayOfWeek.valueOf(stringDate.toUpperCase());
+            LocalDate today = LocalDate.now();
+            DayOfWeek todayEnum = today.getDayOfWeek();
+            int daysApart = dateEnum.getValue() - todayEnum.getValue();
+            daysApart = daysApart < 0 ? 7 - daysApart : daysApart;
+            today.plusDays(daysApart);
+            date = today;
+        } else {
+            try {
+                date = LocalDate.parse(stringDate);
+            } catch (DateTimeParseException e) {
+                System.out.println("Improper format. Please look the valid format for dates");
+            }
+        }
+    }
+
+}
