e9b3a24122a4b2a68b3dc5762b7fac29df15eef8
Changrui <e0589902@u.nus.edu>
2022-03-30 18:51:01 +0800

    Update TextUiTest
    Update TextUiTest

--- src/test/java/seedu/duke/ui/TextUiTest.java
+++ src/test/java/seedu/duke/ui/TextUiTest.java
@@ -3,10 +3,8 @@ package seedu.duke.ui;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
-import seedu.duke.data.TaskDuration;
 
 class TextUiTest {
-    TaskDuration taskDuration;
 
     @Test
     public void sampleTest() {
