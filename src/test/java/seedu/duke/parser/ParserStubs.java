c9f6c624a92974c450c579b442f1a863e4c68af0
Kang Hwee Thern <kanghweethern@gmail.com>
2022-03-30 21:03:52 +0800

    Add AddCommandParser JUnit tests
    Note that JUnit assertEquals() calls the Object's equal() method for
    comparison.
    
    Let's add equals() to AddCommand() and Item().
    
    This will also be done for future CommandParser tests.

--- src/test/java/seedu/duke/parser/ParserStubs.java
+++ src/test/java/seedu/duke/parser/ParserStubs.java
@@ -1,2 +1,8 @@
-package seedu.duke.parser;public class ParserStubs {
+package seedu.duke.parser;
+
+public class ParserStubs {
+    public static final String PAPERCUP_NAME = "Paper Cup";
+    public static final String PAPERCUP_QUANTITY = "25";
+    public static final String PAPERCUP_DESCRIPTION = "25ml cups";
 }
+
