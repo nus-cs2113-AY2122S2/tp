c1c229421dcc422d5f5edebf32fe2e15f727dfde
Zhou Qi <>
2022-03-30 23:02:11 +0800

    Edit EXPECTED.TXT

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -13,7 +13,7 @@ public class Duke {
                               + "| |_| | |_| |   <  __/\n"
                               + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
+        System.out.println("What is your name!");
 
         Scanner in = new Scanner(System.in);
         System.out.println("Hello " + in.nextLine());
