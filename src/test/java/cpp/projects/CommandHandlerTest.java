e404fea4ad1899b2ffc29882101e3bbe67aa9c67
johnmcmonigle <60244374+johnmcmonigle@users.noreply.github.com>
2022-03-30 23:22:21 +0800

    Fix Another JavaDoc issue

--- src/test/java/cpp/projects/CommandHandlerTest.java
+++ src/test/java/cpp/projects/CommandHandlerTest.java
@@ -7,7 +7,9 @@ import cpp.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class CommandHandlerTest {
     private static final String PROJECT1NAME = "CS2113tP";
