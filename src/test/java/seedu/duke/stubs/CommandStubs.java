5f26d35305f2f795b67485d6bb3e15c3f45a58b4
Kang Hwee Thern <kanghweethern@gmail.com>
2022-03-31 12:56:51 +0800

    Update Stubs with more Items and expected ItemLists

--- src/test/java/seedu/duke/stubs/CommandStubs.java
+++ src/test/java/seedu/duke/stubs/CommandStubs.java
@@ -10,20 +10,33 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static seedu.duke.stubs.ItemStubs.ITEM_MARKER;
-import static seedu.duke.stubs.ItemStubs.ITEM_PAPER_A4;
-import static seedu.duke.stubs.ItemStubs.ITEM_PAPER_A5;
+import static seedu.duke.stubs.ItemStubs.ITEM_PAPER_A4_10;
+import static seedu.duke.stubs.ItemStubs.ITEM_PAPER_A4_15;
+import static seedu.duke.stubs.ItemStubs.ITEM_PAPER_A5_10;
+import static seedu.duke.stubs.ItemStubs.ITEM_PENCIL;
 import static seedu.duke.stubs.ItemStubs.ITEM_WHITEBOARD;
 
 public class CommandStubs {
 
-    public static final ItemList LIST_TO_SEARCH = generateStubSearchList(ITEM_PAPER_A4, ITEM_PAPER_A5, ITEM_MARKER,
+    public static final ItemList SEARCH_LIST = generateItemList(ITEM_PAPER_A4_10, ITEM_PAPER_A5_10, ITEM_MARKER,
             ITEM_WHITEBOARD);
-    public static final Set<String> RESULT_SEARCH_PAPER_NAME = generateSearchResult(ITEM_PAPER_A4, ITEM_PAPER_A5);
-    public static final Set<String> RESULT_SEARCH_DRAW_DESCRIPTION = generateSearchResult(ITEM_MARKER, ITEM_WHITEBOARD);
-    public static final Set<String> RESULT_SEARCH_PAPER_NAME_A4_DESCRIPTION = generateSearchResult(ITEM_PAPER_A4);
-    public static final Set<String> RESULT_SEARCH_NONE = generateSearchResult();
+    public static final Set<String> SEARCH_RESULT_PAPER_NAME = generateSearchResult(ITEM_PAPER_A4_10, ITEM_PAPER_A5_10);
+    public static final Set<String> SEARCH_RESULT_DRAW_DESCRIPTION = generateSearchResult(ITEM_MARKER, ITEM_WHITEBOARD);
+    public static final Set<String> SEARCH_RESULT_PAPER_NAME_A4_DESCRIPTION = generateSearchResult(ITEM_PAPER_A4_10);
+    public static final Set<String> SEARCH_RESULT_NONE = generateSearchResult();
 
-    private static ItemList generateStubSearchList(Item... items) {
+    public static final ItemList EDIT_NAME_LIST_BEFORE = generateItemList(ITEM_PENCIL);
+    public static final ItemList EDIT_NAME_EXPECTED_LIST_AFTER = generateItemList(ITEM_MARKER);
+    public static final ItemList EDIT_ABSQUANTITY_LIST_BEFORE = generateItemList(ITEM_PAPER_A4_10);
+    public static final ItemList EDIT_ABSQUANTITY_EXPECTED_LIST_AFTER = generateItemList(ITEM_PAPER_A4_15);
+    public static final ItemList EDIT_RELQUANTITY_LIST_BEFORE = generateItemList(ITEM_PAPER_A4_15);
+    public static final ItemList EDIT_RELQUANTITY_EXPECTED_LIST_AFTER = generateItemList(ITEM_PAPER_A4_10);
+    public static final ItemList EDIT_DESC_LIST_BEFORE = generateItemList(ITEM_PAPER_A4_10);
+    public static final ItemList EDIT_DESC_EXPECTED_LIST_AFTER = generateItemList(ITEM_PAPER_A5_10);
+    public static final ItemList EDIT_ALL_LIST_BEFORE = generateItemList(ITEM_MARKER);
+    public static final ItemList EDIT_ALL_EXPECTED_LIST_AFTER = generateItemList(ITEM_WHITEBOARD);
+
+    private static ItemList generateItemList(Item... items) {
         ArrayList<Item> list = new ArrayList<>();
         Collections.addAll(list, items);
         return new ItemList(list);
