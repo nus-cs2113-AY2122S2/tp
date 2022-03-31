43c47412bc683b6537cb9a2b7954799fdd95cc05
quitejasper <jasper.chia9@gmail.com>
2022-03-31 14:44:25 +0800

    Update InputParser Class
    Added case for ListOverdueBorrowingsCommand

--- src/main/java/seedu/duke/parser/InputParser.java
+++ src/main/java/seedu/duke/parser/InputParser.java
@@ -9,6 +9,7 @@ import seedu.duke.commands.DeleteCommand;
 import seedu.duke.commands.ListCommand;
 import seedu.duke.commands.HelpCommand;
 import seedu.duke.commands.SearchCommand;
+import seedu.duke.commands.ListOverdueBorrowingsCommand;
 import seedu.duke.exceptions.InvMgrException;
 import seedu.duke.common.Messages;
 
@@ -54,6 +55,9 @@ public class InputParser {
 
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
+
+        case ListOverdueBorrowingsCommand.COMMAND_WORD:
+            return new ListOverdueBorrowingsParser().parse(arguments);
 
         case ExitCommand.COMMAND_WORD:
             return new ExitCommand();
