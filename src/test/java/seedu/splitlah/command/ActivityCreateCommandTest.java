da39b1673801ff186f66170e72b05b550f5ffbc4
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-03-31 14:28:55 +0800

    Update tests to remove redundancy

--- src/test/java/seedu/splitlah/command/ActivityCreateCommandTest.java
+++ src/test/java/seedu/splitlah/command/ActivityCreateCommandTest.java
@@ -33,25 +33,13 @@ class ActivityCreateCommandTest {
      *                              if the session unique identifier specified was not found.
      */
     @Test
-    public void run_hasNameDuplicatesInInvolvedList_activityListSizeRemainsOne() throws InvalidDataException {
-        String userInput = "activity /create /sid 1 /n Dinner /p Alice /i Alice Alice Charlie /co 30";
-        Command command = Parser.getCommand(userInput);
-        assertEquals(ActivityCreateCommand.class, command.getClass());
-        command.run(manager);
-        assertEquals(1, manager.getProfile().getSession(1).getActivityList().size());
-    }
-
-    /**
-     * Checks if activity unique identifier is not incremented if an activity fails
-     * to be created due to duplicate names in involved list.
-     */
-    @Test
-    public void run_hasNameDuplicatesInInvolvedList_activityIdNotIncremented() {
+    public void run_hasNameDuplicatesInInvolvedList_activityNotCreated() throws InvalidDataException {
         int currentActivityId = manager.getProfile().getActivityIdTracker();
         String userInput = "activity /create /sid 1 /n Dinner /p Alice /i Alice Alice Charlie /co 30";
         Command command = Parser.getCommand(userInput);
         assertEquals(ActivityCreateCommand.class, command.getClass());
         command.run(manager);
+        assertEquals(1, manager.getProfile().getSession(1).getActivityList().size());
         int testActivityId = manager.getProfile().getActivityIdTracker();
         assertEquals(currentActivityId, testActivityId);
     }
@@ -65,27 +53,16 @@ class ActivityCreateCommandTest {
      */
     @Test
     public void run_hasInvalidNameInInvolvedList_activityListSizeRemainsOne() throws InvalidDataException {
-        String userInput = "activity /create /sid 1 /n Dinner /p Alice /i Eve Mallory /co 30";
-        Command command = Parser.getCommand(userInput);
-        assertEquals(ActivityCreateCommand.class, command.getClass());
-        command.run(manager);
-        assertEquals(1, manager.getProfile().getSession(1).getActivityList().size());
-    }
-
-    /**
-     * Checks if activity unique identifier is not incremented if an activity fails
-     * to be created due to names in involved list not found in session.
-     */
-    @Test
-    public void run_hasInvalidNameInInvolvedList_activityIdNotIncremented() {
         int currentActivityId = manager.getProfile().getActivityIdTracker();
         String userInput = "activity /create /sid 1 /n Dinner /p Alice /i Eve Mallory /co 30";
         Command command = Parser.getCommand(userInput);
         assertEquals(ActivityCreateCommand.class, command.getClass());
         command.run(manager);
+        assertEquals(1, manager.getProfile().getSession(1).getActivityList().size());
         int testActivityId = manager.getProfile().getActivityIdTracker();
         assertEquals(currentActivityId, testActivityId);
     }
+
 
     /**
      * Checks if activity is created successfully and added into list of activities.
@@ -95,23 +72,12 @@ class ActivityCreateCommandTest {
      */
     @Test
     public void run_validCommand_activityListSizeBecomesTwo() throws InvalidDataException {
-        String userInput = "activity /create /sid 1 /n Dinner /p Alice /i Alice Bob Charlie /co 30";
-        Command command = Parser.getCommand(userInput);
-        assertEquals(ActivityCreateCommand.class, command.getClass());
-        command.run(manager);
-        assertEquals(2, manager.getProfile().getSession(1).getActivityList().size());
-    }
-
-    /**
-     * Checks if activity is created successfully and activity unique identifier is incremented.
-     */
-    @Test
-    public void run_validCommand_activityIdIncremented() {
         int currentActivityId = manager.getProfile().getActivityIdTracker();
         String userInput = "activity /create /sid 1 /n Dinner /p Alice /i Alice Bob Charlie /co 30";
         Command command = Parser.getCommand(userInput);
         assertEquals(ActivityCreateCommand.class, command.getClass());
         command.run(manager);
+        assertEquals(2, manager.getProfile().getSession(1).getActivityList().size());
         int testActivityId = manager.getProfile().getActivityIdTracker();
         assertEquals(currentActivityId + 1, testActivityId);
     }
