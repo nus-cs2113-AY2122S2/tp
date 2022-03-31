24cd45692c67a3c8a69d4aaf6a5ceb005b7e4561
chintaiann <chintaiann23@gmail.com>
2022-03-31 18:26:51 +0800

    v2.0
    1. implemented functionality of not allowing reservations to be added if exceeds maxParticipants
    
    2. error message when travelID not found during adding/removing reservations
    
    3. adding/deletion of reservations will correctly alter number of participants now

--- src/main/java/seedu/duke/command/ReservationCommand.java
+++ src/main/java/seedu/duke/command/ReservationCommand.java
@@ -13,11 +13,27 @@ public class ReservationCommand extends Command {
         this.newReservation = new Reservation(travelPackageID, name, number, pax);
     }
     public void execute(Packages packages) {
+            boolean foundID = false;
             for (int i =0;i<packages.getSize();i++){
                 if (packages.getPackage(i).getID() == newReservation.getPackageID()) {
-                    packages.getPackage(i).getReservationList().addReservation(this.newReservation);
+                    //handle participant overflow
+                    if (newReservation.getNumOfPax() + packages.getPackage(i).getNumParticipants() > packages.getPackage(i).getMaxParticipants()) {
+                        System.out.println("Too many participants, maximum amount of participants will be exceeded. Please try again.");
+                    }
+                    else {
+                        packages.getPackage(i).getReservationList().addReservation(this.newReservation);
+                        packages.getPackage(i).addParticipants(newReservation.getNumOfPax());
+                    }
+                    foundID = true;
+                    break;
+
                 }
             }
+            if (!foundID) {
+                System.out.println("Travel Package ID not found. Please try again.");
+        }
+
+
         }
     }
 
