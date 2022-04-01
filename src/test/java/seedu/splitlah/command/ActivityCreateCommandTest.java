b62395b9742a68a6249295ff6809577516e17769
ivanaitzliddat <ivanaitzliddat@yahoo.com.sg>
2022-04-01 21:27:12 +0800

    Add test case regarding valid command
    Checks if valid command with user input indicating cost list
    results in an activity successfully created.

--- src/test/java/seedu/splitlah/command/ActivityCreateCommandTest.java
+++ src/test/java/seedu/splitlah/command/ActivityCreateCommandTest.java
@@ -86,28 +86,50 @@ class ActivityCreateCommandTest {
      */
     @Test
     public void run_validCommand_activityListSizeBecomesTwo() throws InvalidDataException {
-        //Case 1: Split costs evenly among 3 persons
-        String userInput = "activity /create /sid 1 /n Dinner /p Alice /i Alice Bob Charlie /co 30";
-
-        //Checks that command returned is an ActivityCreateCommand object
-        Command command = Parser.getCommand(userInput);
-        assertEquals(ActivityCreateCommand.class, command.getClass());
-
-        //Checks that the number of activities stored increases by 1
-        int currentActivityId = manager.getProfile().getActivityIdTracker();
-        command.run(manager);
-        assertEquals(2, manager.getProfile().getSession(1).getActivityList().size());
-
-        //Checks that ActivityCost of all 3 persons are updated correctly
         Person alice = manager.getProfile().getSession(1).getPersonByName("Alice");
         Person bob = manager.getProfile().getSession(1).getPersonByName("Bob");
         Person charlie = manager.getProfile().getSession(1).getPersonByName("Charlie");
+
+        //Case 1: Split costs evenly among 3 persons
+        String userInputOne = "activity /create /sid 1 /n Dinner /p Alice /i Alice Bob Charlie /co 30";
+
+        //Checks that command returned is an ActivityCreateCommand object
+        Command commandOne = Parser.getCommand(userInputOne);
+        assertEquals(ActivityCreateCommand.class, commandOne.getClass());
+
+        //Checks that the number of activities stored increases by 1
+        int currentActivityOneId = manager.getProfile().getActivityIdTracker();
+        commandOne.run(manager);
+        assertEquals(2, manager.getProfile().getSession(1).getActivityList().size());
+
+        //Checks that ActivityCost of all 3 persons are updated correctly
         assertEquals(10, alice.getActivityCostOwed(2));
         assertEquals(10, bob.getActivityCostOwed(2));
         assertEquals(10, charlie.getActivityCostOwed(2));
 
         //Checks that the activityId is incremented
-        int testActivityId = manager.getProfile().getActivityIdTracker();
-        assertEquals(currentActivityId + 1, testActivityId);
+        int testActivityOneId = manager.getProfile().getActivityIdTracker();
+        assertEquals(currentActivityOneId + 1, testActivityOneId);
+
+        //Case 2: Individual costs provided
+        String userInputTwo = "activity /create /sid 1 /n Dinner /p Alice /i Alice Bob Charlie /cl 5 10 15";
+
+        //Checks that command returned is an ActivityCreateCommand object
+        Command commandTwo = Parser.getCommand(userInputTwo);
+        assertEquals(ActivityCreateCommand.class, commandTwo.getClass());
+
+        //Checks that the number of activities stored increases by 1
+        int currentActivityTwoId = manager.getProfile().getActivityIdTracker();
+        commandTwo.run(manager);
+        assertEquals(3, manager.getProfile().getSession(1).getActivityList().size());
+
+        //Checks that ActivityCost of all 3 persons are updated correctly
+        assertEquals(5, alice.getActivityCostOwed(3));
+        assertEquals(10, bob.getActivityCostOwed(3));
+        assertEquals(15, charlie.getActivityCostOwed(3));
+
+        //Checks that the activityId is incremented
+        int testActivityTwoId = manager.getProfile().getActivityIdTracker();
+        assertEquals(currentActivityTwoId + 1, testActivityTwoId);
     }
 }
