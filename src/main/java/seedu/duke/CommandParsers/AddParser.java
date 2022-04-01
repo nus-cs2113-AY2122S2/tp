492dabc102fbad1cd22a135b13288fce7f97280a
Nnythingy <ngjongrayedward8@gmail.com>
2022-04-02 03:03:30 +0800

    Update AddParser.java

--- src/main/java/seedu/duke/CommandParsers/AddParser.java
+++ src/main/java/seedu/duke/CommandParsers/AddParser.java
@@ -2,9 +2,7 @@ package seedu.duke.CommandParsers;
 
 import seedu.duke.Regex;
 import seedu.duke.Warehouse;
-import util.exceptions.InvalidFileException;
-import util.exceptions.InvalidObjectType;
-import util.exceptions.WrongCommandException;
+import util.exceptions.*;
 
 import java.util.HashMap;
 
@@ -25,26 +23,24 @@ public class AddParser extends CommandParser{
             String regexGood = "sku/(?<sku>.*) n/(?<name>.*) d/(?<desc>.*) up/(?<up>.*) ui/(?<ui>.*) wu/(?<isWholeUnit>.*) ba/(?<ba>.*)" +
                     " v/(?<v>.*) ip/(?<ip>.*) qty/(?<qty>.*)";
             HashMap<String,String> regexGoodMatch = new Regex(userInput, regexGood).getGroupValues();
-            warehouse.addGoodToInventory(regexGoodMatch.get("sku"),regexGoodMatch.get("name"),
-                    regexGoodMatch.get("desc"),
-                    Float.parseFloat(regexGoodMatch.get("up")), regexGoodMatch.get("ui"),
-                    Boolean.parseBoolean(regexGoodMatch.get("isWholeUnit")),
-                    Float.parseFloat(regexGoodMatch.get("ba")),
-                    Float.parseFloat(regexGoodMatch.get("v")),
-                    Boolean.parseBoolean(regexGoodMatch.get("ip")),
-                    Float.parseFloat(regexGoodMatch.get("qty")));
+            try {
+                warehouse.addGoodToInventory(regexGoodMatch.get("sku"),regexGoodMatch.get("qty"));
+            } catch (ItemDoesNotExistException e) {
+                // Can change to automatically add a unit good
+                System.out.println("Unit Good does not exist. Please add a Unit Good first");
+            }
         } else if (matches.get("flag").equals("ug")){
             String regexUnitGood = "sku/(?<sku>.*) n/(?<name>.*) d/(?<desc>.*) up/(?<up>.*) ui/(?<ui>.*) wu/(?<isWholeUnit>.*) ba/(?<ba>.*)" +
                     " v/(?<v>.*) ip/(?<ip>.*)";
             HashMap<String,String> regexUnitGoodMatch = new
                     Regex(userInput, regexUnitGood).getGroupValues();
-            warehouse.addUnitGoodToInventory(regexUnitGoodMatch.get("sku"),regexUnitGoodMatch.get("name"),
-                    regexUnitGoodMatch.get("desc"),
-                    Float.parseFloat(regexUnitGoodMatch.get("up")), regexUnitGoodMatch.get("ui"),
-                    Boolean.parseBoolean(regexUnitGoodMatch.get("isWholeUnit")),
-                    Float.parseFloat(regexUnitGoodMatch.get("ba")),
-                    Float.parseFloat(regexUnitGoodMatch.get("v")),
-                    Boolean.parseBoolean(regexUnitGoodMatch.get("ip")));
+            try {
+                warehouse.addUnitGoodToInventory(regexUnitGoodMatch.get("sku"),regexUnitGoodMatch.get("name"),
+                        regexUnitGoodMatch.get("desc"), regexUnitGoodMatch.get("capacity"));
+            } catch (UnitTestException e) {
+                System.out.println("Capacity Added is not either Small, Medium, Large. "
+                        + "Default set to Medium");
+            }
         } else if (matches.get("flag").equals("bg")){
             // batch goods
             String regexBatchGoods = "fp/(?<filepath>.*)";
