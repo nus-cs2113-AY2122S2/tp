e4ecbaa44930122d4d58ff5fcdc8681d31919c19
siewyangzhi <syangzhi54@gmail.com>
2022-04-02 17:45:41 +0800

    Update code to fix gradle checkstyle error

--- src/main/java/seedu/duke/exceptions/InvalidItemNameException.java
+++ src/main/java/seedu/duke/exceptions/InvalidItemNameException.java
@@ -1,8 +1,8 @@
 package seedu.duke.exceptions;
 
 public class InvalidItemNameException extends HotelLiteManagerException {
-    private static final String ERROR_MESSAGE = "Error! The item name is invalid. Item Names can only contain " +
-            "alphabetical characters, numbers, blank spaces or apostrophes.";
+    private static final String ERROR_MESSAGE = "Error! The item name is invalid. Item Names can only contain "
+            + "alphabetical characters, numbers, blank spaces or apostrophes.";
 
     @Override
     public String getErrorMessage() {
--- src/main/java/seedu/duke/itemlists/Item.java
+++ src/main/java/seedu/duke/itemlists/Item.java
@@ -1,17 +1,19 @@
 package seedu.duke.itemlists;
 
 import seedu.duke.exceptions.InvalidItemNameException;
+
 /**
  * Represents an item that is stored within the inventory. An item object consists of the item's name as well as its
  * pax.
  */
+
 public class Item {
     private String name;
     private int pax;
 
     public Item(String name, int pax) throws InvalidItemNameException {
         boolean isItemNameFormatValid = checkIfItemNameFormatValid(name);
-        if(isItemNameFormatValid == false){
+        if (isItemNameFormatValid == false) {
             throw new InvalidItemNameException();
         }
         setName(name);
