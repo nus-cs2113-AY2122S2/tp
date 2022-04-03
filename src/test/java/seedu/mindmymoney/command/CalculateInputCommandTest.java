f52b5a51d6d66dfb4081c86b5f6dc844a9825e85
limjierui <jieruilim@u.nus.edu>
2022-04-04 00:39:57 +0800

    Fix date issues.
    Add more Junit test for Add and Update Command

--- src/test/java/seedu/mindmymoney/command/CalculateInputCommandTest.java
+++ src/test/java/seedu/mindmymoney/command/CalculateInputCommandTest.java
@@ -70,7 +70,7 @@ class CalculateInputCommandTest {
         
         String inputString = "/pm cash /c Personal /d Nike Shoes /a 300 /t 30/03/2022";
         new AddCommand(inputString, user).executeCommand();
-        inputString = "/pm cash /c Food /d Coke /a 20 /t 30/04/2022";
+        inputString = "/pm cash /c Food /d Coke /a 20 /t 01/04/2022";
         new AddCommand(inputString, user).executeCommand();
 
         setUp();
@@ -101,7 +101,7 @@ class CalculateInputCommandTest {
         User user = new User(expenditureTestList, creditCardTestList, incomeList);
         String inputString = "/pm cash /c Personal /d Nike Shoes /a 300 /t 30/03/2022";
         new AddCommand(inputString, user).executeCommand();
-        inputString = "/pm cash /c Food /d Coke /a 20 /t 30/04/2021";
+        inputString = "/pm cash /c Food /d Coke /a 20 /t 01/04/2021";
         new AddCommand(inputString, user).executeCommand();
 
         setUp();
