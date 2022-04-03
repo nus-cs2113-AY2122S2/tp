fdeaedcf5d7fe3ca2269b0fe9ecc444d0d3f480d
siewyangzhi <syangzhi54@gmail.com>
2022-04-04 00:16:07 +0800

    Update code to implement the item name constraint and add it in the UG
    Fixes #316

--- src/main/java/seedu/duke/itemlists/Item.java
+++ src/main/java/seedu/duke/itemlists/Item.java
@@ -10,6 +10,7 @@ import seedu.duke.exceptions.InvalidItemNameException;
 public class Item {
     private String name;
     private int pax;
+    private String updatedName;
 
     public Item(String name, int pax) throws InvalidItemNameException {
         boolean isItemNameFormatValid = checkIfItemNameFormatValid(name);
@@ -20,7 +21,25 @@ public class Item {
         setPax(pax);
     }
 
-    public Item(String name) {
+    public Item(String name, String updatedName) throws InvalidItemNameException {
+        boolean isItemNameFormatValid = checkIfItemNameFormatValid(name);
+        if (isItemNameFormatValid == false) {
+            throw new InvalidItemNameException();
+        }
+
+        boolean isUpdatedItemNameFormatValid = checkIfItemNameFormatValid(updatedName);
+        if (isUpdatedItemNameFormatValid == false) {
+            throw new InvalidItemNameException();
+        }
+        setName(name);
+        setUpdatedName(updatedName);
+    }
+
+    public Item(String name) throws InvalidItemNameException {
+        boolean isItemNameFormatValid = checkIfItemNameFormatValid(name);
+        if (isItemNameFormatValid == false) {
+            throw new InvalidItemNameException();
+        }
         setName(name);
     }
 
@@ -46,5 +65,13 @@ public class Item {
             isItemNameFormatValid = true;
         }
         return isItemNameFormatValid;
+    }
+
+    public String getUpdatedName() {
+        return updatedName;
+    }
+
+    public void setUpdatedName(String updatedName) {
+        this.updatedName = updatedName;
     }
 }
