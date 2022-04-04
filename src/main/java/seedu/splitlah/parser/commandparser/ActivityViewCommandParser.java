3b596e596c142f52d411b314127f0aa9eec52cb9
RenTianle <lele.rer@outlook.com>
2022-04-04 15:35:00 +0800

    Edit javadoc

--- src/main/java/seedu/splitlah/parser/commandparser/ActivityViewCommandParser.java
+++ src/main/java/seedu/splitlah/parser/commandparser/ActivityViewCommandParser.java
@@ -7,7 +7,7 @@ import seedu.splitlah.parser.ParserUtils;
 import seedu.splitlah.ui.Message;
 
 /**
- * Represents a command parser that views an Activity object.
+ * Represents a command parser that is able to parse user arguments into an ActivityViewCommand object.
  *
  * @author Tianle
  */
