dee7e2fbba11b5c2aadc036dde9256f843979278
SeanHo <seanhowenbin@gmail.com>
2022-04-05 23:09:42 +0800

    add,calc,bye tests

--- src/test/java/seedu/mindmymoney/MindMyMoneyTest.java
+++ src/test/java/seedu/mindmymoney/MindMyMoneyTest.java
@@ -1,27 +1,21 @@
 package seedu.mindmymoney;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import seedu.mindmymoney.command.AddCommand;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.PrintStream;
 import java.util.NoSuchElementException;
 
 class MindMyMoneyTest {
 
     /**
      * Asserts if MMM is able to run and await for new line.
+     * If a NoSuchElementException occurs, MM is trying to read a newline but unable to, hence ran successfully.
      */
     @Test
     public void mindMyMoney_runAndInputNewLine_expectedException() {
         assertThrows(NoSuchElementException.class,
             () -> new MindMyMoney().run());
+
     }
 }
