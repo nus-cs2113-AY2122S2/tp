f5a6e2f1e145e20ddd25bf1774a9dee7a3da807d
Joshua <joshuajr13@gmail.com>
2022-04-06 04:15:03 +0800

    Update MealReservationParser.java to remove magic string literals

--- src/main/java/arcs/parser/MealReservationParser.java
+++ src/main/java/arcs/parser/MealReservationParser.java
@@ -6,6 +6,15 @@ import arcs.commands.mealreservation.AddMealReservationCommand;
 import java.util.HashMap;
 
 public class MealReservationParser {
+
+    private static final String DRINKS_COMMAND_WORD = "drinks";
+    private static final String APPETIZER_COMMAND_WORD = "appetizer";
+    private static final String MAIN_COMMAND_WORD = "main";
+    private static final String SIDE_COMMAND_WORD = "side";
+    private static final String DESSERT_COMMAND_WORD = "dessert";
+    private static final String IC_COMMAND_WORD = "ic";
+    private static final String FLIGHT_ID_COMMAND_WORD = "fid";
+
     public static Command prepareMealReservationCommand(String argumentLine) {
         if (argumentLine == null || argumentLine.isEmpty()) {
             return new AddMealReservationCommand(null, null, null);
@@ -26,25 +35,25 @@ public class MealReservationParser {
             String field = argSplit[0].trim().toLowerCase();
             String value = argSplit[1].trim();
             switch (field) {
-            case "fid":
+            case FLIGHT_ID_COMMAND_WORD:
                 flightId = value;
                 break;
-            case "ic":
+            case IC_COMMAND_WORD:
                 customerIc = value;
                 break;
-            case "drinks":
+            case DRINKS_COMMAND_WORD:
                 menuItemsToReserve.put("DRINKS", value.replace("_", " "));
                 break;
-            case "appetizer":
+            case APPETIZER_COMMAND_WORD:
                 menuItemsToReserve.put("APPETIZER", value.replace("_", " "));
                 break;
-            case "main":
+            case MAIN_COMMAND_WORD:
                 menuItemsToReserve.put("MAIN", value.replace("_", " "));
                 break;
-            case "side":
+            case SIDE_COMMAND_WORD:
                 menuItemsToReserve.put("SIDE", value.replace("_", " "));
                 break;
-            case "dessert":
+            case DESSERT_COMMAND_WORD:
                 menuItemsToReserve.put("DESSERT", value.replace("_", " "));
                 break;
             default:
@@ -54,3 +63,4 @@ public class MealReservationParser {
         return new AddMealReservationCommand(customerIc,flightId,menuItemsToReserve);
     }
 }
+
