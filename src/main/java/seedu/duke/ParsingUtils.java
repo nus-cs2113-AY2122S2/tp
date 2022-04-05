e0f27e4b572a787deb89305ffe54c6c6c625ea9b
zhenster <bobicusj@gmail.com>
2022-04-05 14:38:52 +0800

    Fix saved files to include only valid inputs

--- src/main/java/seedu/duke/ParsingUtils.java
+++ src/main/java/seedu/duke/ParsingUtils.java
@@ -1,12 +1,21 @@
 package seedu.duke;
 
+/**
+ * Contains common parsing helper methods.
+ */
+
 public class ParsingUtils {
     private static final String DELIMITER = "/";
 
-    public int countSlashes(String userInput) {
+    /**
+     * Counts the number of delimiters in a string.
+     * @param string The given string.
+     * @return The number of slashes in the string.
+     */
+    public int countSlashes(String string) {
         int slashCount = 0;
-        for (int i = 0; i < userInput.length(); i++) {
-            String curChar = Character.toString(userInput.charAt(i));
+        for (int i = 0; i < string.length(); i++) {
+            String curChar = Character.toString(string.charAt(i));
             if (curChar.equals(DELIMITER)) {
                 slashCount += 1;
             }
@@ -14,8 +23,14 @@ public class ParsingUtils {
         return slashCount;
     }
 
-    public boolean isAlpha(String name) {
-        String nameWithoutSpaces = name.replaceAll("\\s+", "");
-        return nameWithoutSpaces.matches("[a-zA-Z]+");
+    /**
+     * Checks if a string contains only alphabetical characters.
+     * Useful for checking the validity of names.
+     * @param string The given string.
+     * @return A boolean (true if the string contains only alphabetical characters, false otherwise).
+     */
+    public boolean isAlpha(String string) {
+        String stringWithoutSpaces = string.replaceAll("\\s+", "");
+        return stringWithoutSpaces.matches("[a-zA-Z]+");
     }
 }
