c7b313995588bce7309f9d1b6d10a0fa2ba7e838
Sean Ho <69450377+SeanHoWB@users.noreply.github.com>
2022-04-05 16:39:59 +0800

    Update AddCommand.java

--- src/main/java/seedu/mindmymoney/command/AddCommand.java
+++ src/main/java/seedu/mindmymoney/command/AddCommand.java
@@ -159,7 +159,7 @@ public class AddCommand extends Command {
 
         System.out.println("Successfully added: \n\n"
                 + "Description: " + description + "\n"
-                + "Amount: $" + String.format("%.2f", amountInt) + "\n"
+                + "Amount: $" + String.format("%.2f", amountFloat) + "\n"
                 + "Category: " + category + "\n"
                 + "Payment method: " + paymentMethod + "\n"
                 + "Date: " + time + "\n\n"
