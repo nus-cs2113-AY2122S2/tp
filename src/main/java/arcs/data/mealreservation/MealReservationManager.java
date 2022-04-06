c047ba80af92cae2090b6b5d4fe35be2b557e040
Joshua <joshuajr13@gmail.com>
2022-04-06 19:47:56 +0800

    Update MealReservationManager.java to include delete meal reservation functionality

--- src/main/java/arcs/data/mealreservation/MealReservationManager.java
+++ src/main/java/arcs/data/mealreservation/MealReservationManager.java
@@ -69,4 +69,19 @@ public class MealReservationManager {
         return "";
     }
 
+    public boolean deleteSpecificCustomerMealReservation(String ic, String fid) {
+        for (MealReservation mealReservation : mealReservations) {
+            if (ic.equals(mealReservation.getCustomer().getIc())
+                    && fid.equals(mealReservation.getRoute().getFlightID())) {
+                if (!(mealReservation instanceof MealReservation)) {
+                    return false;
+                } else {
+                    mealReservations.remove(mealReservation);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
