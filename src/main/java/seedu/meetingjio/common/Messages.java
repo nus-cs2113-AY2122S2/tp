71473dd0b661de8ff3b757d6a121e87af8286df3
Yan Jie <60136330+yanjie1017@users.noreply.github.com>
2022-04-07 03:30:58 +0800

    Fix bugs in I/O redirection test

--- src/main/java/seedu/meetingjio/common/Messages.java
+++ src/main/java/seedu/meetingjio/common/Messages.java
@@ -20,7 +20,7 @@ public class Messages {
     public static final String MESSAGE_WELCOME = "Welcome to MeetingJio!";
     public static final String MESSAGE_QUESTION_NAME = "How do I address you?";
     public static final String MESSAGE_HINT = "If you are unfamiliar with MeetingJio, "
-            + "you can view the list of commands available using the command 'help'.";
+            + "you can view the list of commands available using the command 'help'";
     public static final String MESSAGE_DIVIDER = "________________________________________________________"
             + "__________________________________________________";
     public static final String MESSAGE_GOODBYE = "See you again!";
