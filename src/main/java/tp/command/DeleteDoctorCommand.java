62ea803182570d5bbae7bfe2185792cbd8e0b706
DolphXty <63856054+DolphXty@users.noreply.github.com>
2022-04-06 21:58:35 +0800

    commit

--- src/main/java/tp/command/DeleteDoctorCommand.java
+++ src/main/java/tp/command/DeleteDoctorCommand.java
@@ -1,14 +1,9 @@
 package tp.command;
 
-import tp.AppointmentList;
-import tp.DoctorList;
-import tp.PatientList;
-import tp.Ui;
+import tp.*;
 import tp.person.Doctor;
-import tp.DoctorStorage;
-import tp.PatientStorage;
-import tp.IHospitalException;
-import tp.AppointmentStorage;
+
+import java.util.Scanner;
 
 public class DeleteDoctorCommand extends Command {
     private final int index;
@@ -16,20 +11,23 @@ public class DeleteDoctorCommand extends Command {
     public DeleteDoctorCommand(int index) {
         this.index = index;
     }
-
+    Scanner scanner=new Scanner(System.in);
     @Override
     public String execute(DoctorList doctorList, PatientList patientList,
                           AppointmentList appointmentList, Ui ui, DoctorStorage doctorStorage,
                           PatientStorage patientStorage,
                           AppointmentStorage appointmentStorage) throws IHospitalException {
         Doctor curr = doctorList.deleteDoctor(index);
-        if (curr == null){
-            return String.format("The index " + index + " is not valid in the doctor list");
+        while (curr == null){
+            System.out.println("The doctor you request to delete does not exist.");
+            System.out.println("Please reenter a valid doctor index or enter 0 to change command.");
+            int com=scanner.nextInt();
+            if (com==0)
+                return("Exiting delect doctor command.");
+            curr=doctorList.deleteDoctor(com);
         }
-        else {
             return String.format(boundary + "Noted. I've removed this doctor:" + "\n" + curr
                     + "\n" + "Now you have " + doctorList.getSize()
                     + " doctors in the system." + System.lineSeparator() + boundary);
-        }
     }
 }
