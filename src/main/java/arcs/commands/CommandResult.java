bd956ddb0c77ff32ce52cf28ce028b5b5bfe1a2f
Joshua <joshuajr13@gmail.com>
2022-04-06 19:22:52 +0800

    Update CommandResult.java to include find meal reservation feature

--- src/main/java/arcs/commands/CommandResult.java
+++ src/main/java/arcs/commands/CommandResult.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class CommandResult {
     private String feedbackToUser;
     private ArrayList<String> commandInfo = null;
+    private String commandInfoString;
 
     public CommandResult(String feedbackToUser) {
         this.feedbackToUser = feedbackToUser;
@@ -15,11 +16,20 @@ public class CommandResult {
         this.commandInfo = commandInfo;
     }
 
+    public CommandResult(String feedbackToUser, String commandInfoString) {
+        this.feedbackToUser = feedbackToUser;
+        this.commandInfoString = commandInfoString;
+    }
+
     public String getFeedbackToUser() {
         return feedbackToUser;
     }
 
     public ArrayList<String> getCommandInfo() {
         return commandInfo;
+    }
+
+    public String getCommandInfoString() {
+        return commandInfoString;
     }
 }
