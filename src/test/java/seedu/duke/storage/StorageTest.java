2dd9d21b892b6f1c65ae8cba641b1227375f64cb
Kang Hwee Thern <kanghweethern@gmail.com>
2022-04-07 12:10:59 +0800

    Remove unused HashSet import

--- src/test/java/seedu/duke/storage/StorageTest.java
+++ src/test/java/seedu/duke/storage/StorageTest.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 public class StorageTest {
