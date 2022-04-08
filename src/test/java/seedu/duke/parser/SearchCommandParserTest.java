5e130b5d737c05a25e0236da77c0dcb2e6d2eb2f
Kang Hwee Thern <kanghweethern@gmail.com>
2022-04-09 05:56:32 +0800

    Update Search & Edit CommandParserTest
    Previously, Search & EditCommandParser could take in arguments with 0-length String as values.
    
    A new commit f2870dd is pushed such that arguments with 0-length String values are treated as empty.
    
    Let's
    - test the above implementation

--- src/test/java/seedu/duke/parser/SearchCommandParserTest.java
+++ src/test/java/seedu/duke/parser/SearchCommandParserTest.java
@@ -11,6 +11,16 @@ public class SearchCommandParserTest {
     private SearchCommandParser parser = new SearchCommandParser();
 
     @Test
+    void parse_compulsoryFieldsWithoutValue_throwException() {
+        String testInputFormat = " %s %s %s";
+        String testInput = String.format(testInputFormat,
+                1,
+                CliSyntax.PREFIX_NAME.getPrefix(),
+                CliSyntax.PREFIX_DESCRIPTION.getPrefix());
+        assertParseFailure(parser, testInput, Messages.INVALID_SYNTAX);
+    }
+
+    @Test
     void parse_missingAllCompulsoryFields_throwException() {
         // No name and description
         String testInputFormat = " %s";
@@ -27,12 +37,28 @@ public class SearchCommandParserTest {
                 CliSyntax.PREFIX_NAME.getPrefix(), ParserStubs.PAPERCUP_NAME);
         assertParseSuccess(parser, testInputNameOnly, ParserStubs.SEARCHCOMMAND_NAMEONLY);
 
+        // Name with missing description value
+        String testInputNameMissingDescFormat = " %s %s%s %s";
+        String testInputNameMissingDesc = String.format(testInputNameMissingDescFormat,
+                1,
+                CliSyntax.PREFIX_NAME.getPrefix(), ParserStubs.PAPERCUP_NAME,
+                CliSyntax.PREFIX_DESCRIPTION.getPrefix());
+        assertParseSuccess(parser, testInputNameMissingDesc, ParserStubs.SEARCHCOMMAND_NAMEONLY);
+
         // Description only
-        String testInputDescriptionOnlyFormat = " %s %s %s";
+        String testInputDescriptionOnlyFormat = " %s %s%s";
         String testInputDescriptionOnly = String.format(testInputDescriptionOnlyFormat,
                 1,
                 CliSyntax.PREFIX_DESCRIPTION.getPrefix(), ParserStubs.PAPERCUP_DESCRIPTION);
         assertParseSuccess(parser, testInputDescriptionOnly, ParserStubs.SEARCHCOMMAND_DESCRIPTIONONLY);
+
+        // Description with missing name value
+        String testInputDescriptionMissingNameFormat = " %s %s%s %s";
+        String testInputDescriptionMissingName = String.format(testInputDescriptionMissingNameFormat,
+                1,
+                CliSyntax.PREFIX_DESCRIPTION.getPrefix(), ParserStubs.PAPERCUP_DESCRIPTION,
+                CliSyntax.PREFIX_NAME.getPrefix());
+        assertParseSuccess(parser, testInputDescriptionMissingName, ParserStubs.SEARCHCOMMAND_DESCRIPTIONONLY);
 
         // Both name and description
         String testInputNameAndDescriptionFormat = " %s %s%s %s%s";
