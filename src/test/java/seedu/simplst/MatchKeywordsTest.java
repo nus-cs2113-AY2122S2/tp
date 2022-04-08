aa780577d3cb26f086238cb22390f02dfa984f2a
Shyunyin <shyunyin321@gmail.com>
2022-04-09 05:51:22 +0800

    FIx MatchKeywordstest

--- src/test/java/seedu/simplst/MatchKeywordsTest.java
+++ src/test/java/seedu/simplst/MatchKeywordsTest.java
@@ -12,7 +12,7 @@ class MatchKeywordsTest {
     @Test
     void getGroupValues() throws MissingFlagException, EmptyFieldException {
         String regex = "(oid/(?<oid>\\d*) sku/(?<sku>.*) r/(?<recv>.*) addr/(?<addr>.*)";
-        String test1 = "oid/12 sku/156 r/receiver a/address";
+        String test1 = "o/ oid/12 sku/156 r/receiver a/address";
         MatchKeywords matchKeywords1 = new MatchKeywords(test1, regex);
         HashMap<String, String> matches = matchKeywords1.getGroupValues();
 
