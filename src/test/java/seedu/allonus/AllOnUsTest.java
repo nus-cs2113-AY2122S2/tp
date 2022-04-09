0d8cd010ddd3ffcca5a9d0391a937d60ffe84c01
OzairHasan <e0407597@u.nus.edu>
2022-04-10 02:19:53 +0800

    Resolve test errors

--- src/test/java/seedu/allonus/AllOnUsTest.java
+++ src/test/java/seedu/allonus/AllOnUsTest.java
@@ -2,11 +2,10 @@
 
 package seedu.allonus;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+
 
 class AllOnUsTest {
 
@@ -18,17 +17,17 @@ class AllOnUsTest {
         assertEquals(false, AllOnUs.isExitCommand(""));
 
         // test for navigation to Contacts Manager
-        assertEquals(true, AllOnUs.isContactsManagerCommand("goto m/Contacts_Manager"));
+        assertEquals(true, AllOnUs.isContactsManagerCommand("goto Contacts_Manager"));
         assertEquals(false, AllOnUs.isContactsManagerCommand("wrong"));
         assertEquals(false, AllOnUs.isContactsManagerCommand(""));
 
         // test for navigation to Study Manager
-        assertEquals(true, AllOnUs.isStudyManagerCommand("goto m/Study_Manager"));
+        assertEquals(true, AllOnUs.isStudyManagerCommand("goto Study_Manager"));
         assertEquals(false, AllOnUs.isStudyManagerCommand("wrong"));
         assertEquals(false, AllOnUs.isStudyManagerCommand(""));
 
         // test for navigation to Expense Tracker
-        assertEquals(true, AllOnUs.isExpenseTrackerCommand("goto m/Expense_Tracker"));
+        assertEquals(true, AllOnUs.isExpenseTrackerCommand("goto Expense_Tracker"));
         assertEquals(false, AllOnUs.isExpenseTrackerCommand("wrong"));
         assertEquals(false, AllOnUs.isExpenseTrackerCommand(""));
 
