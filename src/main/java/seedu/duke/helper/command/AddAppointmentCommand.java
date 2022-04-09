0564bc668d2a89abd9a216a5519373ac85633b3b
shxr3f <e0540641@u.nus.edu>
2022-04-09 16:29:15 +0800

    Adding Documentation for Command classes part 1.

--- src/main/java/seedu/duke/helper/command/AddAppointmentCommand.java
+++ src/main/java/seedu/duke/helper/command/AddAppointmentCommand.java
@@ -4,11 +4,26 @@ import seedu.duke.assets.List;
 import seedu.duke.exception.DuplicateEntryException;
 import seedu.duke.status.Status;
 
+/*
+ * Class that extends Abstract Command Class.
+ */
 public class AddAppointmentCommand extends Command {
+
+    /*
+     * Constructor method that calls constructor of super class
+     * @ param parameterArray an array of Strings - additional parameters given by user
+     */
     public AddAppointmentCommand(String[] parameterArray) {
         super(parameterArray);
     }
 
+    /*
+     * Method that takes in an AppointmentList that needs to be acted on.
+     * Uses parameterArray as inputs to add a new Appointment to the List.
+     * @ param list a List object - should be an instance of AppointmentList
+     * @ return Status.ADD_APPOINTMENT_SUCCESS if changes made to list was successful
+     * @ throws DuplicateEntryException if similar entry already exist.
+     */
     @Override
     public Status execute(List appointmentList) throws DuplicateEntryException {
         appointmentList.add(parameterArray);
--- src/main/java/seedu/duke/helper/command/ViewAppointmentCommand.java
+++ src/main/java/seedu/duke/helper/command/ViewAppointmentCommand.java
@@ -4,11 +4,26 @@ import seedu.duke.assets.List;
 import seedu.duke.exception.HalpmiException;
 import seedu.duke.status.Status;
 
+/*
+ * Class that extends Abstract Command Class.
+ */
 public class ViewAppointmentCommand extends Command {
+
+    /*
+     * Constructor method that calls constructor of super class
+     * @ param parameterArray an array of Strings - additional parameters given by user
+     */
     public ViewAppointmentCommand(String[] parameterArray) {
         super(parameterArray);
     }
 
+    /*
+     * Method that takes in an AppointmentList that needs to be acted on.
+     * parameterArray should be NULL. Prints list of existing Appointments.
+     * @ param list a List object - should be an instance of AppointmentList
+     * @ return Status.VIEW_SUCCESS if list was printed.
+     * @ throws HalpmiException if issues with parameterArray.
+     */
     public Status execute(List appointmentList) throws HalpmiException {
         if (parameterArray == null) {
             appointmentList.view();
