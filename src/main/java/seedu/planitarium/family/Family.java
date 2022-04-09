a859215b7b488767c10014d05cdb16b6c162fb6a
Kr0sfyre <teanweijun@gmail.com>
2022-04-09 14:10:47 +0800

    Fix missing semicolons

--- src/main/java/seedu/planitarium/family/Family.java
+++ src/main/java/seedu/planitarium/family/Family.java
@@ -185,9 +185,9 @@ public class Family {
         Double sum = 0.0;
         for (int i = Constants.SINGULAR; i <= Constants.NUM_GROUPS; i++) {
             PersonList personList = getList(i);
-            Double income = personList.getTotalIncome()
+            Double income = personList.getTotalIncome();
             String incomeString = UI.formatValue(income);
-            Double expenditure = personList.getTotalExpenditure()
+            Double expenditure = personList.getTotalExpenditure();
             String expenditureString = UI.formatValue(expenditure);
             Double remain = personList.getRemain();
             sum += remain;
