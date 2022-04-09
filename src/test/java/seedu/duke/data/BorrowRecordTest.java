30d9af83de8f01a22b444f5ad2b4da223864fedd
Lester Sim <lester.sim@u.nus.edu>
2022-04-09 16:46:52 +0800

    Change BorrowRecordTest test method names

--- src/test/java/seedu/duke/data/BorrowRecordTest.java
+++ src/test/java/seedu/duke/data/BorrowRecordTest.java
@@ -12,22 +12,23 @@ import static seedu.duke.stubs.BorrowRecordStubs.PRESENTRECORD_A_STARTDATE;
 
 public class BorrowRecordTest {
     @Test
-    public void conflict_borrowRecord() {
+    public void borrowRecord_sameDate_isConflict() {
         assertEquals(true, PRESENTRECORD_A.isConflict(PRESENTRECORD_A));
     }
 
     @Test
-    public void conflict_sameDate_borrowRecord() {
+    public void borrowRecord_overlapDate_isConflict() {
         assertEquals(true, PRESENTRECORD_A.isConflict(FUTURERECORD_A));
     }
 
     @Test
-    public void noConflict_borrowRecord() {
+    public void borrowRecord_diffDate_noConflict() {
         assertEquals(false, PRESENTRECORD_A.isConflict(PASTRECORD_A));
     }
 
+    // Test if string output for a BorrowRecord matches expected string output
     @Test
-    public void string_borrowRecord() {
+    public void borrowRecord_stringOutput() {
         String expectedOutput = String.format("Name of Borrower: %s",
                 PRESENTRECORD_A_NAME) + System.lineSeparator();
         expectedOutput += String.format("Borrow Duration: %s to %s",
