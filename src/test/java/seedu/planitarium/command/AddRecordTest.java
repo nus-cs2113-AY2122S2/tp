54b5fd796a5ca320a36318ce0b763848a617afdc
hlwang56 <e0550418@u.nus.edu>
2022-04-10 20:35:37 +0800

    Update JUnit

--- src/test/java/seedu/planitarium/command/AddRecordTest.java
+++ src/test/java/seedu/planitarium/command/AddRecordTest.java
@@ -1,4 +1,60 @@
 package seedu.planitarium.command;
 
+import org.junit.jupiter.api.Test;
+import seedu.planitarium.commands.AddPersonCommand;
+import seedu.planitarium.commands.AddRecordCommand;
+import seedu.planitarium.exceptions.PlanITariumException;
+import seedu.planitarium.family.Family;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class AddRecordTest {
+    Family family;
+    AddRecordCommand addRec;
+    protected static final String INVALID_UID_MSG =
+            "Unknown error is detected from 'Invalid category index `-1`', please check again.";
+    protected static final String INVALID_IMONEY_MSG =
+            "Unknown error is detected from 'Empty string after `/i`', please check again.";
+    protected static final String INVALID_IMONEY_MSG2 =
+            "Unknown error is detected from 'Invalid money value `-999.99`', please check again.";
+
+    private void initialize() throws PlanITariumException {
+        family = new Family();
+        AddPersonCommand add = new AddPersonCommand(CommandsForTesting.ADDPERSON, family);
+        add.execute();
+    }
+
+    @Test
+    void addRec_invalidUid_fail() {
+        try{
+            initialize();
+            addRec = new AddRecordCommand(CommandsForTesting.ADDINCOME2, family);
+        } catch (PlanITariumException e) {
+            assertEquals(e.toString(), INVALID_UID_MSG);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void addIncome_invalidMoney_fail() {
+        try {
+            initialize();
+            addRec = new AddRecordCommand(CommandsForTesting.ADDINCOME3, family);
+        } catch (PlanITariumException e) {
+            assertEquals(e.toString(), INVALID_IMONEY_MSG2);
+        } catch (Exception e) {
+            fail();
+        }
+
+        try {
+            initialize();
+            addRec = new AddRecordCommand(CommandsForTesting.ADDINCOME4, family);
+        } catch (PlanITariumException e) {
+            assertEquals(e.toString(), INVALID_IMONEY_MSG);
+        } catch (Exception e) {
+            fail();
+        }
+    }
 }
--- src/test/java/seedu/planitarium/command/CommandsForTesting.java
+++ src/test/java/seedu/planitarium/command/CommandsForTesting.java
@@ -4,25 +4,37 @@ public class CommandsForTesting {
     public static final String INVALIDCMD = "invalidcmd";
 
     public static final String ADDPERSON = "add /n bob /g 1";
-    public static final String ADDPERSON1 = "add /n alice /g 0";
-    public static final String ADDPERSON2 = "add /n carol /g 2";
-    public static final String ADDPERSON3 = "add /n dave /g 3";
     public static final String ADDPERSON4 = "add /n /g 3";
     public static final String ADDPERSON5 = "add /n alice /g 4";
     public static final String ADDPERSON6 = "add /n alice /g";
 
     public static final String DELETEPERSON = "delete /u 1 /g 1";
+
     public static final String ADDINCOME = "addin /u 1 /g 1 /i 999.99 /d salary /p f ";
-    public static final String DELETEINCOME = "deletein /u 1 /g 1 /r 1";
-    public static final String EDITINCOME = "editin /u 1 /g 1 /r 1 /i 2000 /d income /p t ";
+    public static final String ADDINCOME2 = "addin /u 2 /g 1 /i 999.99 /d salary /p f ";
+    public static final String ADDINCOME3 = "addin /u 1 /g 1 /i  /d salary /p f ";
+    public static final String ADDINCOME4 = "addin /u 1 /g 1 /i -999.99 /d salary /p f ";
+
     public static final String ADDEXPEND = "addout /u 1 /g 1 /e 1999.99 /d spend /c 1 /p f ";
+    public static final String ADDEXPEND2 = "addout /u 1 /g 1 /e -1999.99 /d spend /c 1 /p f ";
+
+    public static final String DELETEINCOME = "deletein /u 1 /g 1 /r 1";
     public static final String DELETEEXPEND = "deleteout /u 1 /g 1 /r 1";
+
+    public static final String EDITINCOME = "editin /u 1 /g 1 /r 1 /i 2000 /d income /p t ";
     public static final String EDITEXPEND = "editout /u 1 /g 1 /r 1 /e 999.99 /d spent /c 3 /p f ";
-    public static final String OVERVIEW = "overview";
+
     public static final String LISTBYGROUP = "list /g 1";
+
     public static final String LISTCAT = "listcat";
+
     public static final String FIND = "find /d s /c 1";
+    public static final String FIND1 = "find";
+
     public static final String EXIT = "bye";
+
+    public static final String OVERVIEW = "overview";
+
     public static final String HELP = "help";
 
 }
