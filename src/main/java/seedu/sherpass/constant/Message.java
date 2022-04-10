be1017bb9d84e73a0cc664a4c23ab20f023ff53f
BradenTeo <58351701+BradenTeo@users.noreply.github.com>
2022-04-10 20:00:27 +0800

    Reformat help messages

--- src/main/java/seedu/sherpass/constant/Message.java
+++ src/main/java/seedu/sherpass/constant/Message.java
@@ -5,6 +5,20 @@ public class Message {
     public static final String WHITESPACE = " ";
     public static final String TAB_INDENT = "\t";
     public static final String NEWLINE = "\n";
+    public static final String PARTITION_LINE = "______________________________"
+            + "______________________________";
+    public static final String LS = System.lineSeparator();
+    public static final String LOGO = "  ____  _\n"
+            + " / ___|| |__   ___ _ __ "
+            + "_ __   __ _ ___ ___\n"
+            + " \\___ \\| '_ \\ / _ \\ '__| "
+            + "'_ \\ / _` / __/ __|\n"
+            + "  ___) | | | |  __/ |  "
+            + "| |_) | (_| \\__ \\__ \\\n"
+            + " |____/|_| |_|\\___|_|  "
+            + "| .__/ \\__,_|___/___/\n"
+            + "                       "
+            + "|_|";
     public static final String WELCOME_MESSAGE_ONE = "Welcome to\n";
     public static final String WELCOME_MESSAGE_TWO = "Here is your schedule for today:";
     public static final String WELCOME_MESSAGE_STUDY = "Gotcha! Entering study mode...\n"
@@ -23,24 +37,37 @@ public class Message {
 
     private static final String HELP_MESSAGE = "For more information, please enter the 'help' command.";
     public static final String HELP_MESSAGE_SPECIFIC_COMMAND = "\n\nFor more information on "
-            + "the command you wish to execute,\nenter 'help <command>' e.g. help add";
+            + "the command you wish to execute,\nenter 'help COMMAND_WORD' e.g. help add";
     public static final String HELP_MESSAGE_QUICK_START_COMMAND = "Command input quick start guide:\n"
-            + "1) Show: show today/week/<date>/all/todo\n"
-            + "2) Mark: mark <list_index>\n"
-            + "3) Unmark: unmark <list_index>\n"
-            + "4) Add: add <task_description> /by <task_due_date> /do_on <date_to_work_on_task>\n"
-            + "5) Delete: delete <list_index>\n"
-            + "6) Find: find <keyword>\n"
-            + "7) Clear: clear all/expired/done\n"
-            + "8) Bye: bye";
+            + " 1) Show tasks:    show today/week/DATE/all/todo\n"
+            + " 2) Mark a task:   mark TASK_NUMBER\n"
+            + " 3) Unmark a task: unmark TASK_NUMBER\n"
+            + " 4) Add task: add TASK_DESCRIPTION /do DATE /start START_TIME\n"
+            + "              /end END_TIME /repeat FREQUENCY\n"
+            + "              or\n"
+            + "              add TASK_DESCRIPTION /do DATE /start START_TIME\n"
+            + "              /end END_TIME [/bydate DEADLINE /bytime END_TIME]\n"
+            + " 5) Edit task: edit TASK_NUMBER [TASK_DESCRIPTION] [/do DATE]\n"
+            + "               [/start START_TIME] [/end END_TIME]\n"
+            + "               [/bydate DUE DATE /bytime DUE_TIME]\n"
+            + "               or\n"
+            + "               edit TASK_NUMBER [TASK_DESCRIPTION] [/do DATE]\n"
+            + "               [/start START_TIME] [/end END_TIME]\n"
+            + "               [/bydate DUE DATE /bytime DUE_TIME] /repeat\n"
+            + " 6) Delete a task:    delete TASK_NUMBER\n"
+            + " 7) Clear tasks:      clear all/expired/done\n"
+            + " 8) Enter study mode: study\n"
+            + " 9) Help:             help [COMMAND_WORD/quick start]\n"
+            + "10) Exit:             bye";
     public static final String HELP_MESSAGE_STUDY = "Timer command input quick start guide:\n"
-            + "1) Start default timer: start <mode_number>\n"
-            + "2) Start custom timer: start /custom <timer_duration>\n"
-            + "3) Stop timer: stop\n"
-            + "4) Pause timer: pause\n"
-            + "5) Resume timer: resume\n"
-            + "6) Exit study mode: leave\n"
-            + "7) Mark task as done: mark <task_index>";
+            + "1) Start default timer: start MODE_NUMBER\n"
+            + "2) Start custom timer:  start /custom TIMER_DURATION\n"
+            + "3) Stop timer:          stop\n"
+            + "4) Pause timer:         pause\n"
+            + "5) Resume timer:        resume\n"
+            + "6) Show tasks:          show today/week/DATE/all/todo\n"
+            + "7) Mark task as done:   mark TASK_NUMBER\n"
+            + "8) Exit study mode:     leave";
 
     public static final String ERROR_PREFIX = "Command error: ";
     public static final String EDIT_TASK_RESULT_MESSAGE = "Okay! I've edited this task:";
@@ -79,7 +106,7 @@ public class Message {
 
     public static final String ERROR_INVALID_INPUT_MESSAGE = "Please key in an appropriate command.\n"
             + HELP_MESSAGE;
-    public static final String ERROR_INVALID_STUDY_INPUT_MESSAGE = "Please key in an appropriate command.\n"
+    public static final String ERROR_INVALID_STUDY_INPUT_MESSAGE = "Please key in an appropriate command.\n\n"
             + HELP_MESSAGE_STUDY;
     public static final String ERROR_INVALID_DELETE_INDEX_MESSAGE = "It seems that you've given\n"
             + "an invalid index to delete the task.";
@@ -98,7 +125,7 @@ public class Message {
     public static final String ERROR_INVALID_TIMER_INPUT_MESSAGE = "Oops! Your timer input "
             + "does not seem to be correct.\n\n"
             + "Please select one of the three default modes with\n"
-            + "\t'start <mode_number>'\n\n"
+            + "\t'start MODE_NUMBER'\n\n"
             + "or choose your own custom timer with\n"
-            + "\t'start /custom <timer_duration>'";
+            + "\t'start /custom TIMER_DURATION'";
 }
