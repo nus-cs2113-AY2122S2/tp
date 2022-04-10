7025a6f3ac33f321462c3a8469c94ed0642f18c6
quitejasper <jasper.chia9@gmail.com>
2022-04-10 16:02:03 +0800

    Update ListFutureBorrowingsCommand.java
    Resolves #160

--- src/main/java/seedu/duke/commands/ListFutureBorrowingsCommand.java
+++ src/main/java/seedu/duke/commands/ListFutureBorrowingsCommand.java
@@ -6,7 +6,7 @@ import seedu.duke.data.Item;
 import seedu.duke.data.ItemList;
 import seedu.duke.ui.Ui;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class ListFutureBorrowingsCommand extends Command {
@@ -21,38 +21,20 @@ public class ListFutureBorrowingsCommand extends Command {
     public void execute(ItemList itemList, Ui ui) {
         if (name.isPresent()) {
             ui.showMessages("Here is a list of future borrowings for " + name.get() + ": ");
-            int borrowIndex = 0;
-            for (int i = 0; i < itemList.getSize(); i++) {
-                Item borrowedItem = itemList.getItem(i);
-                ArrayList<BorrowRecord> borrowRecords = borrowedItem.getBorrowRecords();
-
-                for (BorrowRecord record : borrowRecords) {
-                    boolean isFutureBorrowing = record.getBorrowStatus() == BorrowStatus.FUTURE;
-                    boolean matchesName = record.getBorrowerName().equals(name.get());
-                    if (isFutureBorrowing && matchesName) {
-                        borrowIndex++;
-                        ui.showMessages(borrowIndex + ") Name of Item: " + borrowedItem.getName(),
-                                "Name of Borrower: " + record.getBorrowerName(),
-                                "Borrow Duration: " + record.getBorrowDuration() + "\n");
-                    }
-                }
-            }
         } else {
             ui.showMessages("Here is a list of future borrowings: ");
-            for (int i = 0; i < itemList.getSize(); i++) {
-                Item borrowedItem = itemList.getItem(i);
-                ArrayList<BorrowRecord> borrowRecords = borrowedItem.getBorrowRecords();
+        }
 
-                for (BorrowRecord record : borrowRecords) {
-                    if (record.getBorrowStatus() == BorrowStatus.FUTURE) {
-                        ui.showMessages("Name of Item: " + borrowedItem.getName(),
-                                "Name of Borrower: " + record.getBorrowerName(),
-                                "Borrow Duration: " + record.getBorrowDuration() + "\n");
-                    }
-                }
+        for (int i = 0; i < itemList.getSize(); i++) {
+            Item borrowedItem = itemList.getItem(i);
+
+            List<BorrowRecord> filteredRecords = borrowedItem.filterRecords(name, BorrowStatus.FUTURE);
+
+            for (BorrowRecord record : filteredRecords) {
+                ui.showMessages("Name of Item: " + borrowedItem.getName(),
+                        record.toString());
             }
         }
     }
-
 
 }
