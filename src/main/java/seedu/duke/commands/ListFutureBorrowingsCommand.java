8ae6ce58b2289b070de012c10f5400563eac3012
quitejasper <jasper.chia9@gmail.com>
2022-04-10 21:41:20 +0800

    Update ListFutureBorrowingsCommand.java
    `Item` already has an inbuilt function to retrieve a list of strings
    that represent the BorrowRecords.
    
    Let's:
    * Leverage on this function in retrieving our borrowrecords (with the
    name of the item)
    * Use flatmap to collate all borrowrecords so as to determine whether
    there are any borrowings at all
    * Modify remaining code to utilise the list of records created, rather
    than creating another list for the sake of printing

--- src/main/java/seedu/duke/commands/ListFutureBorrowingsCommand.java
+++ src/main/java/seedu/duke/commands/ListFutureBorrowingsCommand.java
@@ -1,6 +1,5 @@
 package seedu.duke.commands;
 
-import seedu.duke.data.BorrowRecord;
 import seedu.duke.data.BorrowStatus;
 import seedu.duke.data.Item;
 import seedu.duke.data.ItemList;
@@ -28,18 +27,12 @@ public class ListFutureBorrowingsCommand extends Command {
      */
     public void execute(ItemList itemList, Ui ui) {
         List<Item> items = itemList.getItemArrayList();
-        List<BorrowRecord> futureRecords =
+        List<String> futureRecords =
                 items.stream()
-                        // for every item, get the arraylist of borrowrecords
-                        .map(Item::getBorrowRecords)
+                        // for every item, get the list of borrowrecords in the form of strings
+                        .map(item -> item.filterRecords(name, BorrowStatus.FUTURE))
                         // combine all the arraylists into 1 single stream of borrowrecords (regardless of item)
                         .flatMap(Collection::stream)
-                        // filter records based on borrowstatus
-                        .filter(record -> record.getBorrowStatus() == BorrowStatus.FUTURE)
-                        // filter by borrower name if there is any during command input
-                        // if there is no borrower specified in command input, compare the borrowername with itself
-                        // (sure to return)
-                        .filter(record -> record.getBorrowerName().equals(name.orElse(record.getBorrowerName())))
                         // convert this stream into a list (that is futureRecords)
                         .collect(Collectors.toList());
 
@@ -58,15 +51,8 @@ public class ListFutureBorrowingsCommand extends Command {
             ui.showMessages("Here is a list of future borrowings: ");
         }
 
-        for (int i = 0; i < itemList.getSize(); i++) {
-            Item borrowedItem = itemList.getItem(i);
-
-            List<BorrowRecord> filteredRecords = borrowedItem.filterRecords(name, BorrowStatus.FUTURE);
-
-            for (BorrowRecord record : filteredRecords) {
-                ui.showMessages("Name of Item: " + borrowedItem.getName(),
-                        record.toString());
-            }
+        for (String record: futureRecords) {
+            ui.showMessages(record);
         }
     }
 
