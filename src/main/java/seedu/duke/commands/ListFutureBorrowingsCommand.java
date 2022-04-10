a1f989584ac20c613122aa22a6872df98138dcae
quitejasper <jasper.chia9@gmail.com>
2022-04-10 18:20:39 +0800

    Update ListFutureBorrowingsCommand.java
    Resolves #151
    
    Programme was initially unable to detect when there are no relevant
    records in the future (and for the particular borrower). This does not
    align with the User Guide where we should be able to.
    
    Let's
    * Use Java Streams to create a compiled list of relevant borrow records,
    and use that list to check if we should return a message saying that
    there are no records.

--- src/main/java/seedu/duke/commands/ListFutureBorrowingsCommand.java
+++ src/main/java/seedu/duke/commands/ListFutureBorrowingsCommand.java
@@ -6,8 +6,10 @@ import seedu.duke.data.Item;
 import seedu.duke.data.ItemList;
 import seedu.duke.ui.Ui;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class ListFutureBorrowingsCommand extends Command {
     public static final String COMMAND_WORD = "listfb";
@@ -18,7 +20,38 @@ public class ListFutureBorrowingsCommand extends Command {
         this.name = name;
     }
 
+    /**
+     * This method first checks if there are any borrow records at all (for the person if it is specified in input).
+     * If there are, the method will loop through the items to print out the items and their respective borrow records.
+     * @param itemList ItemList containing all items in the current inventory
+     * @param ui User Interface for reading inputs and/or printing outputs
+     */
     public void execute(ItemList itemList, Ui ui) {
+        List<Item> items = itemList.getItemArrayList();
+        List<BorrowRecord> futureRecords =
+                items.stream()
+                        // for every item, get the arraylist of borrowrecords
+                        .map(Item::getBorrowRecords)
+                        // combine all the arraylists into 1 single stream of borrowrecords (regardless of item)
+                        .flatMap(Collection::stream)
+                        // filter records based on borrowstatus
+                        .filter(record -> record.getBorrowStatus() == BorrowStatus.FUTURE)
+                        // filter by borrower name if there is any during command input
+                        // if there is no borrower specified in command input, compare the borrowername with itself
+                        // (sure to return)
+                        .filter(record -> record.getBorrowerName().equals(name.orElse(record.getBorrowerName())))
+                        // convert this stream into a list (that is futureRecords)
+                        .collect(Collectors.toList());
+
+        if (futureRecords.size() == 0) {
+            if (name.isPresent()) {
+                ui.showMessages("There are no future borrowings for " + name.get() + ".");
+            } else {
+                ui.showMessages("There are no future borrowings.");
+            }
+            return;
+        }
+
         if (name.isPresent()) {
             ui.showMessages("Here is a list of future borrowings for " + name.get() + ": ");
         } else {
