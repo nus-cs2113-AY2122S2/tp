f5046bb9bef042f0d15b1f3cd4482f7b70de037a
JL <tha.junlim@gmail.com>
2022-04-10 17:10:58 +0800

    Add javadoc for execute method in Command class

--- src/main/java/seedu/sherpass/command/Command.java
+++ src/main/java/seedu/sherpass/command/Command.java
@@ -7,5 +7,14 @@ import seedu.sherpass.task.TaskList;
 
 public abstract class Command {
 
+    /**
+     * Executes the command. In this case, the execute method of the Command class
+     * will not be executed as it is an abstract method, and will require other subclasses
+     * to override this existing method for it to work. 
+     *
+     * @param tasks Array representation of tasks. Not used.
+     * @param ui User Interface. Not used.
+     * @param storage Stores changes made in a save file. Not used.
+     */
     public abstract void execute(TaskList tasks, Ui ui, Storage storage);
 }
