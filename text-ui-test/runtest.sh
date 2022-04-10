b40aa92bbd2232db8eac7f1abbe71b5d35d9a8df
matheril <sauravvm237@gmail.com>
2022-04-10 12:47:07 +0800

    Fix textui for unix

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -10,7 +10,6 @@ cd text-ui-test
 
 java  -jar $(find ../build/libs -mindepth 1 -print -quit) < input.txt > ACTUAL.TXT
 
-cp EXPECTED.TXT EXPECTED-UNIX.TXT
 dos2unix EXPECTED-UNIX.TXT ACTUAL.TXT
 diff EXPECTED-UNIX.TXT ACTUAL.TXT
 if [ $? -eq 0 ]
