bf92173b2829e89372cbd29cc966a2f2854743c6
quitejasper <jasper.chia9@gmail.com>
2022-04-10 21:46:28 +0800

    Update ListCurrentBorrowingsCommand.java
    Modified execute method in accordance to ListFutureBorrowingsCommand.
    
    Context:
    `Item` already has an inbuilt function to retrieve a list of strings
    that represent the BorrowRecords.
    
    Let's:
    * Leverage on this function in retrieving our borrowrecords (with the
    name of the item)
    * Use flatmap to collate all borrowrecords so as to determine whether
    there are any borrowings at all
    * Modify remaining code to utilise the list of records created, rather
    than creating another list for the sake of printing

--- src/main/java/seedu/duke/commands/ListCurrentBorrowingsCommand.java
+++ src/main/java/seedu/duke/commands/ListCurrentBorrowingsCommand.java
@@ -1,13 +1,14 @@
 package seedu.duke.commands;
 
-import seedu.duke.data.BorrowRecord;
 import seedu.duke.data.BorrowStatus;
 import seedu.duke.data.Item;
 import seedu.duke.data.ItemList;
 import seedu.duke.ui.Ui;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class ListCurrentBorrowingsCommand extends Command {
     public static final String COMMAND_WORD = "listcb";
@@ -18,21 +19,40 @@ public class ListCurrentBorrowingsCommand extends Command {
         this.name = name;
     }
 
+    /**
+     * This method first checks if there are any borrow records at all (for the person if it is specified in input).
+     * If there are, the method will loop through the items to print out the items and their respective borrow records.
+     * @param itemList ItemList containing all items in the current inventory
+     * @param ui User Interface for reading inputs and/or printing outputs
+     */
     public void execute(ItemList itemList, Ui ui) {
+        List<Item> items = itemList.getItemArrayList();
+        List<String> currentRecords =
+                items.stream()
+                        // for every item, get the list of borrowrecords in the form of strings
+                        .map(item -> item.filterRecords(name, BorrowStatus.PRESENT))
+                        // combine all the arraylists into 1 single stream of borrowrecords (regardless of item)
+                        .flatMap(Collection::stream)
+                        // convert this stream into a list (that is currentRecords)
+                        .collect(Collectors.toList());
+
+        if (currentRecords.size() == 0) {
+            if (name.isPresent()) {
+                ui.showMessages("There are no current borrowings for " + name.get() + ".");
+            } else {
+                ui.showMessages("There are no current borrowings.");
+            }
+            return;
+        }
         if (name.isPresent()) {
             ui.showMessages("Here is a list of current borrowings for " + name.get() + ": ");
         } else {
             ui.showMessages("Here is a list of current borrowings: ");
         }
 
-        for (int i = 0; i < itemList.getSize(); i++) {
-            Item borrowedItem = itemList.getItem(i);
-            // Return a list of records filtered for name (optional) and status
-            List<String> filteredRecords = borrowedItem.filterRecords(name, BorrowStatus.PRESENT);
-
-            for (String record : filteredRecords) {
-                ui.showMessages(record);
-            }
+        for (String record: currentRecords) {
+            ui.showMessages(record);
         }
+
     }
 }
