fff46fd050af6a8774dbe9e11578091e48aac477
Shyunyin <shyunyin321@gmail.com>
2022-04-10 22:38:25 +0800

    Finish orderline test

--- src/test/java/seedu/simplst/OrderlineTest.java
+++ src/test/java/seedu/simplst/OrderlineTest.java
@@ -1,10 +1,32 @@
 package seedu.simplst;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//whatIsBeingTested_descriptionOfTestInputs_expectedOutcome
+
 public class OrderlineTest {
 
-    public void setupOrderline() {
+    @Test
+    public void fulfilOrder_allQuantityFulfilled_isDone() {
         Order testOrder = new Order(1, "John Doe", "123 Maple ave");
         UnitGood testUnitGood = new UnitGood("WC1", "Wooden Chairs", "German Oak", "small");
         Good testGood = new Good(testUnitGood, 50);
+        Orderline testOrderline = new Orderline(testGood, 1, 20);
+
+        testOrderline.setQuantityFulfilled(20);
+        assertEquals(20, testOrderline.getQuantityFulfilled());
+        assertEquals(true, testOrderline.getCheckedOff());
+    }
+
+    @Test
+    public void fulfilOrder_someQuantityFulfilled_isNotDone() {
+        Order testOrder = new Order(2, "John Day", "321 Maple ave");
+        UnitGood testUnitGood = new UnitGood("WC2", "Wooden Table", "German Oak", "small");
+        Good testGood = new Good(testUnitGood, 50);
+        Orderline testOrderline = new Orderline(testGood, 2, 20);
+
+        testOrderline.setQuantityFulfilled(10);
+        assertEquals(false, testOrderline.getCheckedOff());
     }
 }
--- src/test/java/seedu/simplst/UnitGoodTest.java
+++ src/test/java/seedu/simplst/UnitGoodTest.java
@@ -3,7 +3,6 @@ package seedu.simplst;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//whatIsBeingTested_descriptionOfTestInputs_expectedOutcome
 public class UnitGoodTest {
 
     @Test
