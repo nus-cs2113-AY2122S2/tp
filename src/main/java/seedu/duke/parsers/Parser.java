ae861fb699a76f3204576c3bac183383e4671e5a
Yzkkk <yangzikun0511@gmail.com>
2022-04-10 12:52:50 +0800

    minor edits

--- src/main/java/seedu/duke/parsers/Parser.java
+++ src/main/java/seedu/duke/parsers/Parser.java
@@ -21,6 +21,7 @@ import seedu.duke.util.NumberConstants;
  * Represents a Parser that parse a {@code Command}.
  */
 public abstract class Parser {
+    //@@author Yzkkk
     protected static final String EXIT_COMMAND_WORD = StringConstants.EXIT_COMMAND_WORD;
     protected static final String ADD_COMMAND_WORD = StringConstants.ADD_COMMAND_WORD;
     protected static final String DELETE_COMMAND_WORD = StringConstants.DELETE_COMMAND_WORD;
@@ -128,6 +129,24 @@ public abstract class Parser {
     }
 
     //@@author Yzkkk
+    /**
+     * Checks for strings that are parsed into groups based on commandFormat, but are essentially invalid.
+     * @throws InvalidFlagException If the flag inputted is invalid
+     * @throws InvalidModuleGradeException If the module grade inputted is invalid
+     * @throws InvalidTagOperationException If the tag operation inputted is invalid
+     */
+    private void checkForInvalidStrings() throws InvalidFlagException,
+            InvalidModuleGradeException, InvalidTagOperationException {
+        checksForInvalidModFlag();
+        checksForInvalidTaskName();
+        checksForInvalidTaskDescriptionFlag();
+        checksForInvalidModDescriptionFlag();
+        checksForInvalidTimeFlag();
+        checksForInvalidTagCommand();
+        checksForInvalidModuleGrade();
+    }
+
+    //@@author Yzkkk
     private void checksForInvalidModuleGrade() throws InvalidModuleGradeException {
         if (groupNames.contains(INVALID_MODULE_GRADE)) {
             String invalidInput = parsedCommand.get(INVALID_MODULE_GRADE);
@@ -205,23 +224,5 @@ public abstract class Parser {
                 throw new ExcessArgumentException(invalidInput);
             }
         }
-    }
-    
-    //@@author Yzkkk
-    /**
-     * Checks for strings that are parsed into groups based on commandFormat, but are essentially invalid.
-     * @throws InvalidFlagException If the flag inputted is invalid
-     * @throws InvalidModuleGradeException If the module grade inputted is invalid
-     * @throws InvalidTagOperationException If the tag operation inputted is invalid
-     */
-    private void checkForInvalidStrings() throws InvalidFlagException,
-            InvalidModuleGradeException, InvalidTagOperationException {
-        checksForInvalidModFlag();
-        checksForInvalidTaskName();
-        checksForInvalidTaskDescriptionFlag();
-        checksForInvalidModDescriptionFlag();
-        checksForInvalidTimeFlag();
-        checksForInvalidTagCommand();
-        checksForInvalidModuleGrade();
     }
 }
