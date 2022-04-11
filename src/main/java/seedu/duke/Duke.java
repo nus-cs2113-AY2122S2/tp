3915f143d155a80fa28f7b87713c94fdba562061
zhenster <bobicusj@gmail.com>
2022-04-11 12:00:47 +0800

    Refactor write to file for housekeeper performance

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -5,11 +5,7 @@ import java.util.logging.LogManager;
 
 import seedu.duke.command.Command;
 import seedu.duke.command.customercommands.AddSatisfactionCommand;
-import seedu.duke.command.housekeepercommands.AddAvailabilityCommand;
-import seedu.duke.command.housekeepercommands.AddHousekeeperCommand;
-import seedu.duke.command.housekeepercommands.AgeIncreaseCommand;
-import seedu.duke.command.housekeepercommands.ResetAvailabilityCommand;
-import seedu.duke.command.housekeepercommands.DeleteHousekeeperCommand;
+import seedu.duke.command.housekeepercommands.*;
 import seedu.duke.command.itemcommands.AddItemCommand;
 import seedu.duke.command.itemcommands.DeleteItemCommand;
 import seedu.duke.command.itemcommands.UpdateItemNameCommand;
@@ -28,7 +24,7 @@ public class Duke {
             writeItemListsToFile(command, listContainer);
         } else if (command instanceof AddAvailabilityCommand || command instanceof AddHousekeeperCommand || command
                 instanceof AgeIncreaseCommand || command instanceof ResetAvailabilityCommand || command
-                instanceof DeleteHousekeeperCommand) {
+                instanceof DeleteHousekeeperCommand || command instanceof AddHousekeeperPerformanceCommand) {
             writeHousekeeperListsToFile(command, listContainer);
         } else if (command instanceof AddSatisfactionCommand) {
             writeSatisfactionListsToFile(command, listContainer);
@@ -51,6 +47,10 @@ public class Duke {
         } else if (command instanceof ResetAvailabilityCommand) {
             ResetAvailabilityCommand resetAvailabilityCommand = (ResetAvailabilityCommand) command;
             resetAvailabilityCommand.writeHousekeeperToFile(listContainer);
+        } else if (command instanceof AddHousekeeperPerformanceCommand) {
+            AddHousekeeperPerformanceCommand addHousekeeperPerformanceCommand
+                    = (AddHousekeeperPerformanceCommand) command;
+            addHousekeeperPerformanceCommand.writeHousekeeperPerformanceListToFile(listContainer);
         }
     }
 
