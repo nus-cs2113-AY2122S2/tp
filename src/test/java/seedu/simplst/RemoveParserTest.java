9d6f4e99526abc5c4d01382b1575fb54d28f5a19
Shyunyin <shyunyin321@gmail.com>
2022-04-11 15:43:41 +0800

    Update TotalParserTest

--- src/test/java/seedu/simplst/RemoveParserTest.java
+++ src/test/java/seedu/simplst/RemoveParserTest.java
@@ -12,7 +12,8 @@ public class RemoveParserTest {
     Warehouse warehouse = new Warehouse(1000);
     //assertNull after removing for goods and ug respectively
     @Test
-    public void removeUnitGood_addAndRemoveUG_notNull() throws MissingFlagException, EmptyFieldException, WrongCommandException, InvalidFileException, InvalidObjectType {
+    public void removeUnitGood_addAndRemoveUG_notNull() throws MissingFlagException, EmptyFieldException,
+            WrongCommandException, InvalidFileException, InvalidObjectType {
         String userInputOne = "add ug/ sku/WC1 n/Wooden Chair d/German oak size/Medium";
         AddParser addParser = new AddParser(warehouse, userInputOne);
         addParser.initExtractParams();
