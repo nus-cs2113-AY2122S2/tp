73f167458a5953e8cddacfb91f427e5fd8c2dd52
zhenster <bobicusj@gmail.com>
2022-04-11 09:53:37 +0800

    More AddSatisfactionCommandTests

--- src/test/java/seedu/duke/AddSatisfactionCommandTest.java
+++ src/test/java/seedu/duke/AddSatisfactionCommandTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import seedu.duke.command.Command;
 import seedu.duke.command.customercommands.AddSatisfactionCommand;
-import seedu.duke.command.itemcommands.AddItemCommand;
-import seedu.duke.exceptions.*;
-import seedu.duke.itemlists.Item;
-import seedu.duke.itemlists.ItemList;
+import seedu.duke.exceptions.EmptySatisfactionValueException;
+import seedu.duke.exceptions.EmptySatisfactionCustomerException;
+import seedu.duke.exceptions.InvalidSatisfactionValueException;
+import seedu.duke.exceptions.InvalidSatisfactionCustomerNameException;
+import seedu.duke.exceptions.InvalidCommandException;
+import seedu.duke.exceptions.HotelLiteManagerException;
+import seedu.duke.exceptions.DuplicateCommandException;
 import seedu.duke.satisfactionlists.Satisfaction;
 import seedu.duke.satisfactionlists.SatisfactionList;
 
@@ -19,61 +21,53 @@ import java.io.IOException;
 
 public class AddSatisfactionCommandTest {
     private ListContainer listContainer;
-    private Ui ui = new Ui();
-    private static final int INDEX_OF_FRED = 2;
-    private static final int INDEX_OF_ANGELA = 2;
-    private static final int INDEX_OF_STEVE = 2;
+    private Ui ui;
+    private static final int INDEX_OF_FRED = 0;
+    private static final int INDEX_OF_ANGELA = 0;
+    private static final int INDEX_OF_STEVE = 0;
 
     @BeforeEach
-    //Sets up the pretesting environment by creating a SatisfactionList with 2 Satisfaction objects.
+    //Sets up the pretesting environment by creating an empty SatisfactionList
     public void pretestingSetUp() throws HotelLiteManagerException, IOException {
         listContainer = new ListContainer();
         ui = new Ui();
         SatisfactionList satisfactionList = listContainer.getSatisfactionList();
-        Satisfaction testSatisfactionBob = new Satisfaction("Bob", 3);
-        satisfactionList.addSatisfaction(testSatisfactionBob);
-        Satisfaction testSatisfactionJoe = new Satisfaction("Joe", 4);
-        satisfactionList.addSatisfaction(testSatisfactionJoe);
+        satisfactionList.clearSatisfactionList();
+
     }
 
     @Test
     public void execute_NonEmptyCustomerNameAndRating_success() throws HotelLiteManagerException, IOException {
-        AddSatisfactionCommand addSatisfactionCommand= new AddSatisfactionCommand("Fred / 5");
+        AddSatisfactionCommand addSatisfactionCommand = new AddSatisfactionCommand("Fred / 3");
         addSatisfactionCommand.execute(listContainer, ui);
         SatisfactionList satisfactionList = listContainer.getSatisfactionList();
         Satisfaction satisfaction = satisfactionList.getSatisfaction(INDEX_OF_FRED);
-        assertEquals("Fred", satisfaction.getCustomerName());
-        assertEquals(5, satisfaction.getSatisfactionValue());
+        assertEquals("fred", satisfaction.getCustomerName());
+        assertEquals(3, satisfaction.getSatisfactionValue());
+        satisfactionList.clearSatisfactionList();
     }
 
     @Test
-    public void execute_satisfactionValueWithTheUpperBoundaryValue_success() throws HotelLiteManagerException, IOException {
+    public void execute_satisfactionValueWithUpperBoundaryValue_success() throws HotelLiteManagerException, IOException {
         AddSatisfactionCommand addSatisfactionCommand = new AddSatisfactionCommand("Angela / 5");
         addSatisfactionCommand.execute(listContainer, ui);
         SatisfactionList satisfactionList = listContainer.getSatisfactionList();
         Satisfaction satisfaction = satisfactionList.getSatisfaction(INDEX_OF_ANGELA);
         assertEquals("angela", satisfaction.getCustomerName());
         assertEquals(5, satisfaction.getSatisfactionValue());
+        satisfactionList.clearSatisfactionList();
     }
 
-
-//    @Test
-//    public void commandParser_addCommandWithNonEmptySatisfactionCustomerAndSatisfactionValue_success()
-//            throws Exception {
-//        CommandParser parser = new CommandParser();
-//
-//        Command command1 = parser.parse("add satisfaction Bob Jones / 3");
-//        AddSatisfactionCommand addSatisfactionCommand1 = (AddSatisfactionCommand) command1;
-//        Satisfaction satisfaction1 = addSatisfactionCommand1.getSatisfaction();
-//        assertEquals("bob jones", satisfaction1.getCustomerName());
-//        assertEquals(3, satisfaction1.getSatisfactionValue());
-//
-//        Command command2 = parser.parse("add satisfaction Fred / 4");
-//        AddSatisfactionCommand addSatisfactionCommand2 = (AddSatisfactionCommand) command2;
-//        Satisfaction satisfaction2 = addSatisfactionCommand2.getSatisfaction();
-//        assertEquals("fred", satisfaction2.getCustomerName());
-//        assertEquals(4, satisfaction2.getSatisfactionValue());
-//    }
+    @Test
+    public void execute_satisfactionValueWithLowerBoundaryValue_success() throws HotelLiteManagerException, IOException {
+        AddSatisfactionCommand addSatisfactionCommand = new AddSatisfactionCommand("Steve / 5");
+        addSatisfactionCommand.execute(listContainer, ui);
+        SatisfactionList satisfactionList = listContainer.getSatisfactionList();
+        Satisfaction satisfaction = satisfactionList.getSatisfaction(INDEX_OF_STEVE);
+        assertEquals("steve", satisfaction.getCustomerName());
+        assertEquals(5, satisfaction.getSatisfactionValue());
+        satisfactionList.clearSatisfactionList();
+    }
 
     @Test
     public void commandParser_addCommandWithEmptySatisfactionCustomer_exceptionThrown() {
@@ -104,6 +98,25 @@ public class AddSatisfactionCommandTest {
         assertThrows(InvalidSatisfactionValueException.class, ()
                 -> new CommandParser().parse("add satisfaction Joe / 6"));
     }
+
+    @Test
+    public void commandParser_addCommandWithTooManySlashes_exceptionThrown() {
+        assertThrows(InvalidCommandException.class, ()
+            -> new CommandParser().parse("add satisfaction Joe / 3 / 3"));
+    }
+
+    @Test
+    public void commandParser_addCommandWithDuplicateCommands_exceptionThrown() {
+        assertThrows(DuplicateCommandException.class, ()
+                -> new CommandParser().parse("add satisfaction add satisfaction Joe / 3"));
+    }
+
+    @Test
+    public void commandParser_addCommandWithInvalidCustomerName_exceptionThrown() {
+        assertThrows(InvalidSatisfactionCustomerNameException.class, ()
+                -> new CommandParser().parse("add satisfaction Joe 3213 / 3"));
+    }
+
 
 
 
