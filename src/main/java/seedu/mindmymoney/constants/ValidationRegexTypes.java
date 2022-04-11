b120b8bd9cc1e9e0a9d746b44215f00365d77f84
SeanHo <seanhowenbin@gmail.com>
2022-04-11 22:34:54 +0800

    fix regex

--- src/main/java/seedu/mindmymoney/constants/ValidationRegexTypes.java
+++ src/main/java/seedu/mindmymoney/constants/ValidationRegexTypes.java
@@ -4,7 +4,8 @@ package seedu.mindmymoney.constants;
  * Container for validation regex types used in commands.
  */
 public class ValidationRegexTypes {
-    public static final String VALIDATION_REGEX_D = "^([012][1-9]|3[01])/([0][1-9]|1[012])/([0-9][0-9][0-9][0-9])$";
+    public static final String VALIDATION_REGEX_D =
+        "^([0][1-9]|[12][0-9]|3[01])/([0][1-9]|1[012])/([0-9][0-9][0-9][0-9])$";
     public static final String VALIDATION_REGEX_M = "^([0][1-9]|1[012])/([0-9][0-9][0-9][0-9])$";
     public static final String VALIDATION_REGEX_Y = "^([0-9][0-9][0-9][0-9])$";
 }
