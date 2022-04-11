56f9d2a9b2004c4dc42655ad653266cf396ce802
SeanHo <seanhowenbin@gmail.com>
2022-04-11 16:03:44 +0800

    format fixes

--- src/main/java/seedu/mindmymoney/command/UpdateCommand.java
+++ src/main/java/seedu/mindmymoney/command/UpdateCommand.java
@@ -160,7 +160,8 @@ public class UpdateCommand extends Command {
                 newAmountAsFloat, inputTime);
             expenditureList.set(indexToUpdate, newExpenditure);
             System.out.println("Successfully set expenditure " + indexAsString + " to:\n"
-                + "$" + String.format("%.2f", newExpenditure.getAmount()) + " was spent on " + newExpenditure.getDescription()
+                + "$" + String.format("%.2f", newExpenditure.getAmount()) + " was spent on "
+                + newExpenditure.getDescription()
                 + "(" + newExpenditure.getCategory() + ") " + "using " + newExpenditure.getPaymentMethod()
                 + " [" + newExpenditure.getTime() + "]");
         } catch (ArrayIndexOutOfBoundsException e) {
