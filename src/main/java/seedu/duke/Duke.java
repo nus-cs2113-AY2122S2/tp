3aad7802ed7a1b60c5340e6c5e1217d070b1fcbe
zhenster <bobicusj@gmail.com>
2022-04-11 10:28:58 +0800

    Refactor writeToFile for satisfactionList

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.logging.LogManager;
 
 import seedu.duke.command.Command;
+import seedu.duke.command.customercommands.AddSatisfactionCommand;
 import seedu.duke.command.housekeepercommands.AddAvailabilityCommand;
 import seedu.duke.command.housekeepercommands.AddHousekeeperCommand;
 import seedu.duke.command.housekeepercommands.AgeIncreaseCommand;
@@ -29,6 +30,8 @@ public class Duke {
                 instanceof AgeIncreaseCommand || command instanceof ResetAvailabilityCommand || command
                 instanceof DeleteHousekeeperCommand) {
             writeHousekeeperListsToFile(command, listContainer);
+        } else if (command instanceof AddSatisfactionCommand) {
+            writeSatisfactionListsToFile(command, listContainer);
         }
     }
 
@@ -65,6 +68,13 @@ public class Duke {
         } else if (command instanceof DeleteItemCommand) {
             DeleteItemCommand deleteItemNameCommand = (DeleteItemCommand) command;
             deleteItemNameCommand.writeItemListToFile(listContainer);
+        }
+    }
+
+    private void writeSatisfactionListsToFile(Command command, ListContainer listContainer) throws IOException {
+        if (command instanceof AddSatisfactionCommand) {
+            AddSatisfactionCommand addSatisfactionCommand = (AddSatisfactionCommand) command;
+            addSatisfactionCommand.writeSatisfactionListToFile(listContainer);
         }
     }
 
